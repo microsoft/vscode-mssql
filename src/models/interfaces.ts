bd509ff7a78b6b0b089c79d088db8e84826aa5da
Anthony Dresser <andresse@microsoft.com>
2016-07-22 14:47:21 -0700

    fix jslint problems and added config

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -3,8 +3,7 @@ import vscode = require('vscode');
 import Constants = require('./constants');
 
 // interfaces
-export enum ContentType
-{
+export enum ContentType {
     Root = 0,
     Messages = 1,
     ResultsetsMeta = 2,
@@ -18,50 +17,44 @@ Constants.gOutputContentTypeColumns, Constants.gOutputContentTypeRows];
 // mssql.config wrapped into an interface for us to use more easily
 // Provided by the user when creating a new database connection
 // See this for more info: http://pekim.github.io/tedious/api-connection.html
-export interface IConnectionCredentials
-{
-	server: string;
-	database: string;
-	user: string;
-	password: string;
-	connectionTimeout: number;
-	requestTimeout: number;
-    options: { encrypt: boolean, appName: string }
+export interface IConnectionCredentials {
+    server: string;
+    database: string;
+    user: string;
+    password: string;
+    connectionTimeout: number;
+    requestTimeout: number;
+    options: { encrypt: boolean, appName: string };
 };
 
 export interface IConnectionCredentialsQuickPickItem extends vscode.QuickPickItem {
-	connectionCreds: IConnectionCredentials;
-}
+    connectionCreds: IConnectionCredentials;
+};
 
 // Obtained from an active connection to show in the status bar
-export interface IConnectionProperties
-{
-	serverVersion: string;
-	currentUser: string;
-	currentDatabase: string;
+export interface IConnectionProperties {
+    serverVersion: string;
+    currentUser: string;
+    currentDatabase: string;
 };
 
-export interface IBackgridColumnMetadata
-{
-	name: string,
-    label: string,
-    cell: string
+export interface IBackgridColumnMetadata {
+    name: string;
+    label: string;
+    cell: string;
 }
 
-export interface ISqlResultsetMeta
-{
-    columnsUri: string,
-    rowsUri: string
+export interface ISqlResultsetMeta {
+    columnsUri: string;
+    rowsUri: string;
 };
 
-export interface ISqlMessage
-{
-    messageText: string
+export interface ISqlMessage {
+    messageText: string;
 };
 
-export interface ISqlResultset
-{
-    columns: any[],
-    rows: any[],
-    executionPlanXml: string
+export interface ISqlResultset {
+    columns: any[];
+    rows: any[];
+    executionPlanXml: string;
 };
--- src/models/sqlOutputContentProvider.ts
+++ src/models/sqlOutputContentProvider.ts
@@ -10,14 +10,13 @@ class QueryResultSet {
     public messages: string[] = [];
     public resultsets: Interfaces.ISqlResultset[] = [];
 
-    constructor(messages : string[], resultsets : Interfaces.ISqlResultset[]){
+    constructor(messages: string[], resultsets: Interfaces.ISqlResultset[]) {
         this.messages = messages;
         this.resultsets = resultsets;
     }
 }
 
-export class SqlOutputContentProvider implements vscode.TextDocumentContentProvider
-{
+export class SqlOutputContentProvider implements vscode.TextDocumentContentProvider {
     private _queryResultsMap: Map<string, QueryResultSet> = new Map<string, QueryResultSet>();
     public static providerName = 'tsqloutput';
     public static providerUri = vscode.Uri.parse('tsqloutput://');
@@ -28,101 +27,93 @@ export class SqlOutputContentProvider implements vscode.TextDocumentContentProvi
         return this._onDidChange.event;
     }
 
-    public onContentUpdated() {
+    public onContentUpdated(): void {
         Utils.logDebug(Constants.gMsgContentProviderOnContentUpdated);
         this._onDidChange.fire(SqlOutputContentProvider.providerUri);
     }
 
-    constructor(context: vscode.ExtensionContext)
-    {
+    constructor(context: vscode.ExtensionContext) {
         const self = this;
 
         // create local express server
         this._service = new LocalWebService(context.extensionPath);
 
         // add http handler for '/'
-        this._service.addHandler(Interfaces.ContentType.Root, function(req, res) {
+        this._service.addHandler(Interfaces.ContentType.Root, function(req, res): void {
             Utils.logDebug(Constants.gMsgContentProviderOnRootEndpoint);
-            let uri : string = req.query.uri;
-            res.render(path.join(LocalWebService.staticContentPath, Constants.gMsgContentProviderSqlOutputHtml), {uri:uri});
+            let uri: string = req.query.uri;
+            res.render(path.join(LocalWebService.staticContentPath, Constants.gMsgContentProviderSqlOutputHtml), {uri: uri});
         });
 
         // add http handler for '/resultsetsMeta' - return metadata about columns & rows in multiple resultsets
-        this._service.addHandler(Interfaces.ContentType.ResultsetsMeta, function(req, res) {
+        this._service.addHandler(Interfaces.ContentType.ResultsetsMeta, function(req, res): void {
 
             Utils.logDebug(Constants.gMsgContentProviderOnResultsEndpoint);
             let resultsetsMeta: Interfaces.ISqlResultsetMeta[] = [];
-            let uri : string = req.query.uri;
-            for (var index = 0; index < self._queryResultsMap.get(uri).resultsets.length; index ++)
-            {
+            let uri: string = req.query.uri;
+            for (let index = 0; index < self._queryResultsMap.get(uri).resultsets.length; index ++) {
                 resultsetsMeta.push( <Interfaces.ISqlResultsetMeta> {
-                    columnsUri: "/" + Constants.gOutputContentTypeColumns + "?id=" + index.toString(),
-                    rowsUri: "/" + Constants.gOutputContentTypeRows + "?id=" + index.toString()
+                    columnsUri: '/' + Constants.gOutputContentTypeColumns + '?id=' + index.toString(),
+                    rowsUri: '/' + Constants.gOutputContentTypeRows + '?id=' + index.toString()
                 });
             }
             let json = JSON.stringify(resultsetsMeta);
-            //Utils.logDebug(json);
+            // Utils.logDebug(json);
             res.send(json);
         });
 
         // add http handler for '/messages' - return all messages as a JSON string
-        this._service.addHandler(Interfaces.ContentType.Messages, function(req, res) {
+        this._service.addHandler(Interfaces.ContentType.Messages, function(req, res): void {
             Utils.logDebug(Constants.gMsgContentProviderOnMessagesEndpoint);
-            let uri : string = req.query.uri;
+            let uri: string = req.query.uri;
             let json = JSON.stringify(self._queryResultsMap.get(uri).messages);
-            //Utils.logDebug(json);
+            // Utils.logDebug(json);
             res.send(json);
         });
 
         // add http handler for '/columns' - return column metadata as a JSON string
-        this._service.addHandler(Interfaces.ContentType.Columns, function(req, res) {
-            var id = req.query.id;
+        this._service.addHandler(Interfaces.ContentType.Columns, function(req, res): void {
+            let id = req.query.id;
             Utils.logDebug(Constants.gMsgContentProviderOnColumnsEndpoint + id);
-            let uri : string = req.query.uri;
+            let uri: string = req.query.uri;
             let columnMetadata = self._queryResultsMap.get(uri).resultsets[id].columns;
             let json = JSON.stringify(columnMetadata);
-            //Utils.logDebug(json);
+            // Utils.logDebug(json);
             res.send(json);
         });
 
         // add http handler for '/rows' - return rows end-point for a specific resultset
-        this._service.addHandler(Interfaces.ContentType.Rows, function(req, res) {
-            var id = req.query.id;
+        this._service.addHandler(Interfaces.ContentType.Rows, function(req, res): void {
+            let id = req.query.id;
             Utils.logDebug(Constants.gMsgContentProviderOnRowsEndpoint + id);
-            let uri : string = req.query.uri;
+            let uri: string = req.query.uri;
             let json = JSON.stringify(self._queryResultsMap.get(uri).resultsets[id].rows);
-            //Utils.logDebug(json);
+            // Utils.logDebug(json);
             res.send(json);
         });
 
         // start express server on localhost and listen on a random port
-        try
-        {
+        try {
             this._service.start();
-        }
-        catch (error)
-        {
+        } catch (error) {
             Utils.showErrorMsg(error);
             throw(error);
         }
     }
 
-    private clear(uri:string)
-    {
+    private clear(uri: string): void {
         Utils.logDebug(Constants.gMsgContentProviderOnClear);
         this._queryResultsMap.delete(uri);
     }
 
-    public show(uri : string, title : string)
-    {
-        vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.Two, "SQL Query Results: " + title);
+    public show(uri: string, title: string): void {
+        vscode.commands.executeCommand('vscode.previewHtml', uri, vscode.ViewColumn.Two, 'SQL Query Results: ' + title);
     }
 
-    public updateContent(messages, resultsets)
-    {
+    public updateContent(messages, resultsets): string {
         Utils.logDebug(Constants.gMsgContentProviderOnUpdateContent);
-        let title : string = Utils.getActiveTextEditor().document.fileName;
-        let uri : string = SqlOutputContentProvider.providerUri + title;
+        let title: string = Utils.getActiveTextEditor().document.fileName;
+        let uri: string = SqlOutputContentProvider.providerUri + title;
         this.clear(uri);
         this.show(uri, title);
         this._queryResultsMap.set(uri, new QueryResultSet(messages, resultsets));
@@ -131,8 +122,7 @@ export class SqlOutputContentProvider implements vscode.TextDocumentContentProvi
     }
 
     // Called by VS Code exactly once to load html content in the preview window
-    public provideTextDocumentContent(uri: vscode.Uri): string
-    {
+    public provideTextDocumentContent(uri: vscode.Uri): string {
         Utils.logDebug(Constants.gMsgContentProviderProvideContent + uri.toString());
 
         // return dummy html content that redirects to 'http://localhost:<port>' after the page loads
