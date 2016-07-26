e11340442fbff43143e93854e266df0f93ae5b1a
Mitchell Sternke <misternk@microsoft.com>
2016-07-26 10:23:24 -0700

    Initial commit of telemetry-enabled code

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -8,7 +8,11 @@ import { SqlOutputContentProvider } from '../models/sqlOutputContentProvider';
 import StatusView from '../views/statusView';
 import ConnectionManager from './connectionManager';
 import QueryRunner from './queryRunner';
+<<<<<<< 189032e01776d19bdb7d178d87f1986402f679f6
 import SqlToolsServerClient from '../languageservice/serviceclient';
+=======
+import Telemetry from '../models/telemetry';
+>>>>>>> Initial commit of telemetry-enabled code
 
 export default class MainController implements vscode.Disposable {
     private _context: vscode.ExtensionContext;
@@ -41,6 +45,8 @@ export default class MainController implements vscode.Disposable {
     public activate(): void {
         const self = this;
 
+        let activationTimer = new Utils.Timer();
+
         // register VS Code commands
         this.registerCommand(Constants.cmdConnect);
         this._event.on(Constants.cmdConnect, () => { self.onNewConnection(); });
@@ -62,6 +68,13 @@ export default class MainController implements vscode.Disposable {
 
         // initialize language service client
         SqlToolsServerClient.getInstance().initialize(this._context);
+
+        activationTimer.end();
+
+        // telemetry for activation
+        Telemetry.sendTelemetryEvent(this._context, 'ExtensionActivated', {},
+            { activationTime: activationTimer.getDuration() }
+        );
 
         Utils.logDebug(Constants.extensionActivated);
     }
---
+++ typings/getmac.d.ts
@@ -0,0 +1,3 @@
+declare module "getmac" {
+    export function getMac(callback: (err: Error, result: string) => void): void;
+}
