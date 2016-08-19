136cd2c1b94ff3d244cc30fe9a4cb43a31cfdf5c
Anthony Dresser <andresse@microsoft.com>
2016-08-18 18:13:01 -0700

    adds vscode wrapper

---
+++ src/controllers/vscodeWrapper.ts
@@ -0,0 +1,28 @@
+import vscode = require('vscode');
+
+export default class VscodeWrapper {
+    public activeTextEditor(): vscode.TextEditor {
+        return undefined;
+    }
+
+    public createOutputChannel(channelName: string): vscode.OutputChannel {
+        return undefined;
+    }
+
+    public getConfiguration(extensionName: string): vscode.WorkspaceConfiguration {
+        return undefined;
+    }
+
+    public showInformationMessage(msg: string): Thenable<string> {
+        return undefined;
+    }
+
+    public showWarningMessage(msg: string): Thenable<string> {
+        return undefined;
+    }
+
+    public showErrorMsg(msg: string): Thenable<string> {
+        return undefined;
+    }
+
+}
