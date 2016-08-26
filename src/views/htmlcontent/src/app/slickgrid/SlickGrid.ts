63a6aa3d412d49aeb8bdfde132cf65a42e998f16
Anthony Dresser <andresse@microsoft.com>
2016-08-25 18:30:58 -0700

    removed duplicate scroll bars

--- src/views/htmlcontent/src/app/slickgrid/SlickGrid.ts
+++ src/views/htmlcontent/src/app/slickgrid/SlickGrid.ts
@@ -302,6 +302,7 @@ export class SlickGrid implements OnChanges, OnInit, OnDestroy {
 
     onResize(): void {
         if (this._grid !== undefined) {
+            let element = this._el.nativeElement.getElementsByClassName('grid')[0];
             // this will make sure the grid header and body to be re-rendered
             this._grid.resizeCanvas();
         }
