3fd2c4849a5578f17fb49c2f1731aa658eb08d9c
Anthony Dresser <andresse@microsoft.com>
2016-09-06 13:23:32 -0700

    fixed a line too long error in lint

--- src/models/sqlOutputContentProvider.ts
+++ src/models/sqlOutputContentProvider.ts
@@ -123,8 +123,6 @@ export class SqlOutputContentProvider implements vscode.TextDocumentContentProvi
         return `
                 <html>
                     <head>
-                        <script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
-
                     </head>
                     <body></body>
                     <script type="text/javascript">
