35cd4a881b811a77a28f8d44acd447bd22d97252
Anthony Dresser <anthonydresserdev@gmail.com>
2016-09-15 11:51:35 -0700

    Feature/copy paste (#83)
    * initial copy paste code
    
    * initial copy-paste pipe
    
    * added copy paste functionality
    
    * updated package.json
    
    * added tests; removed console logs
    
    * removed unnecessary code

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -22,6 +22,7 @@ export const outputContentTypeResultsetMeta = 'resultsetsMeta';
 export const outputContentTypeColumns = 'columns';
 export const outputContentTypeRows = 'rows';
 export const outputContentTypeSaveResults = 'saveResults';
+export const outputContentTypeCopy = 'copyResults';
 export const outputServiceLocalhost = 'http://localhost:';
 export const msgContentProviderSqlOutputHtml = 'sqlOutput.ejs';
 
