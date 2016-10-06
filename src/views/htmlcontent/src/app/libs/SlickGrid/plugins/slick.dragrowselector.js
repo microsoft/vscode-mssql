9d701d8646ce1fca7b8024f9e3a751cfe0e3bce2
Sharon Ravindran <shravind@microsoft.com>
2016-10-06 10:31:11 -0700

    Remove stopImmediatePropagation

--- src/views/htmlcontent/src/app/libs/SlickGrid/plugins/slick.dragrowselector.js
+++ src/views/htmlcontent/src/app/libs/SlickGrid/plugins/slick.dragrowselector.js
@@ -208,14 +208,12 @@
                 if(cell.cell !== 0) {
                     _ranges = [new Slick.Range(cell.row, cell.cell-1, cell.row, cell.cell-1)];
                     setSelectedRanges(_ranges);
-                    _grid.setActiveCell(cell.row, cell.cell);
-                    // e.stopImmediatePropagation();
+                    _grid.setActiveCell(cell.row, cell.cell);;
                     return true;
                 } else {
                     _ranges = [new Slick.Range(cell.row, 0, cell.row, _grid.getColumns().length - 1)]
                     setSelectedRanges(_ranges);
                     _grid.setActiveCell(cell.row, 1);
-                    // e.stopImmediatePropagation();
                     return true;
                 }
             }
@@ -245,7 +243,6 @@
             }
 
             setSelectedRanges(_ranges);
-            // e.stopImmediatePropagation();
 
             return true;
         }
