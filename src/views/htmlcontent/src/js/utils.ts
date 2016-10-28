e065901edb8b0d75da074d86374817819f59798c
Anthony Dresser <anthonydresserdev@gmail.com>
2016-10-28 12:19:20 -0700

    added select all event to context menu; added events for save as and … (#219)
    * added select all event to context menu; added events for save as and select all
    
    * added shortcuts to context menu when present
    
    * fixed build errors
    
    * fixed select all

--- src/views/htmlcontent/src/js/utils.ts
+++ src/views/htmlcontent/src/js/utils.ts
@@ -1,3 +1,6 @@
+const shortcuts = require('./shortcuts.json!');
+const displayCodes = require('./displayCodes.json!');
+
 export function formatString(str: string, ...args: any[]): string {
     // This is based on code originally from https://github.com/Microsoft/vscode/blob/master/src/vs/nls.js
     // License: https://github.com/Microsoft/vscode/blob/master/LICENSE.txt
@@ -11,4 +14,44 @@ export function formatString(str: string, ...args: any[]): string {
         });
     }
     return result;
+}
+
+/**
+ * determines the platform away shortcut string for an event for display purposes
+ * @param eventString The exact event string of the keycode you require (e.g event.toggleMessagePane)
+ */
+export function stringCodeFor(eventString: string): string {
+    // iterate through all the known shortcuts
+    for (let shortcut in shortcuts) {
+        if (shortcuts.hasOwnProperty(shortcut)) {
+            // if it matches the requested event
+            if (shortcuts[shortcut] === eventString && shortcut !== 'undefined') {
+                let keyString = shortcut;
+                let platString = window.navigator.platform;
+
+                // find the current platform
+                if (platString.match(/win/i)) {
+                    // iterate through the display replacement that are defined
+                    for (let key in displayCodes['windows']) {
+                        if (displayCodes['windows'].hasOwnProperty(key)) {
+                            keyString = keyString.replace(key, displayCodes['windows'][key]);
+                        }
+                    }
+                } else if (platString.match(/linux/i)) {
+                    for (let key in displayCodes['linux']) {
+                        if (displayCodes['linux'].hasOwnProperty(key)) {
+                            keyString = keyString.replace(key, displayCodes['linux'][key]);
+                        }
+                    }
+                } else if (platString.match(/mac/i)) {
+                    for (let key in displayCodes['mac']) {
+                        if (displayCodes['mac'].hasOwnProperty(key)) {
+                            keyString = keyString.replace(key, displayCodes['mac'][key]);
+                        }
+                    }
+                }
+                return keyString;
+            }
+        }
+    }
 }
