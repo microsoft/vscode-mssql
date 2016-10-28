e26f4baa14de5c32e07c1158a5485305190251fa
Sharon Ravindran <shravind@microsoft.com>
2016-10-28 16:06:33 -0700

    handle error callback during query execution

--- src/controllers/QueryRunner.ts
+++ src/controllers/QueryRunner.ts
@@ -148,6 +148,22 @@ export default class QueryRunner {
     public handleResult(result: QueryExecuteCompleteNotificationResult): void {
         this._vscodeWrapper.logToOutputChannel(Utils.formatString(Constants.msgFinishedExecute, this._uri));
         this._isExecuting = false;
+        if (result.message) {
+            // Error occured during execution
+            this._statusView.executedQuery(this.uri);
+            this.batchSets = [{
+                hasError: true,
+                id: 0,
+                selection: undefined,
+                messages: [{ time: undefined, message: result.message }],
+                resultSetSummaries: [],
+                executionElapsed: undefined,
+                executionEnd: undefined,
+                executionStart: undefined
+            }];
+            this.dataResolveReject.resolve(this.batchSets);
+            return;
+        }
         this.batchSets = result.batchSummaries;
 
         this.batchSets.map((batch) => {
