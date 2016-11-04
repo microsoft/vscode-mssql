2b87b9fc5726551ab439980306d0bf6cb903ad68
Anthony Dresser <anthonydresserdev@gmail.com>
2016-11-04 11:13:25 -0700

    Defect/column unique (#256)
    * added id as a field and made it unique to fix selection
    
    * changed id to be optional;

--- src/views/htmlcontent/src/js/slickgrid/EngineAPI.ts
+++ src/views/htmlcontent/src/js/slickgrid/EngineAPI.ts
@@ -9,7 +9,8 @@ export enum FieldType {
 }
 
 export interface IColumnDefinition {
-    id: string;
+    id?: string;
+    name: string;
     type: FieldType;
     asyncPostRender?: (cellRef: string, row: number, dataContext: JSON, colDef: any) => void;
     formatter?: (row: number, cell: any, value: any, columnDef: any, dataContext: any) => string;
