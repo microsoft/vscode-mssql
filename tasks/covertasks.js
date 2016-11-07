971a60046c53ef6472a6e93d555773beccd6406c
Kevin Cunnane <kcunnane@microsoft.com>
2016-11-07 12:51:38 -0800

    Remove typings.json and commented gulp task code

--- tasks/covertasks.js
+++ tasks/covertasks.js
@@ -1,36 +1,5 @@
 var gulp = require('gulp');
-var cproc = require('child_process');
 var jeditor = require("gulp-json-editor");
-var os = require('os');
-
-// Sets a global environment variable
-function setGlobalEnv(name, value) {
-    var setCommand;
-    var getCommand;
-    if (os.platform() === 'win32') {
-        setCommand = `SETX ${name} ${value}`;
-        getCommand = `SET ${name}`;
-    } else if (os.platform() === 'darwin') {
-        setCommand =  `launchctl setenv ${name} ${value}`;
-        getCommand =  `launchctl getenv ${name}`;
-    } else {
-        setCommand =  `export ${name}=${value}`;
-        getCommand =  `export -n ${name}`;
-    }
-    console.log(setCommand);
-    cproc.execSync(setCommand);
-    var valueAfterSet = cproc.execSync(getCommand);
-    console.log(`actual value: ${valueAfterSet}`);
-}
-
-// gulp.task('cover:enable', (done) => {
-//     setGlobalEnv('SQLTOOLSCOVER', 'true');
-//     done();
-// });
-// gulp.task('cover:disable', (done) => {
-//     setGlobalEnv('SQLTOOLSCOVER', 'false');
-//     done();
-// });
 
 gulp.task('cover:enable', () => {
     return gulp.src("./coverconfig.json")
