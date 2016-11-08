87c85ce76a8309c7ae780fcd5df116accbb707e1
kcunnane <kcunnane@KCUNNANEMB>
2016-11-07 17:14:27 -0800

    Support Cobertura reports in coverage - Added Cobertura reporting and moved logic to the coverconfig.json so it can easily be changed on demand - Clean up the coverage folder on each coverage run, and no longer create a new json file for each run

--- tasks/covertasks.js
+++ tasks/covertasks.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var del = require('del');
 var jeditor = require("gulp-json-editor");
 
-gulp.task('cover:enable', () => {
+gulp.task('cover:clean', function (done) {
+    return del('coverage', done);
+});
+
+gulp.task('cover:enableconfig',() => {
     return gulp.src("./coverconfig.json")
     .pipe(jeditor(function(json) {
         json.enabled = true;
@@ -9,6 +14,9 @@ gulp.task('cover:enable', () => {
     }))
     .pipe(gulp.dest("./out", {'overwrite':true}));
 });
+
+gulp.task('cover:enable', gulp.series('cover:clean', 'cover:enableconfig'));
+
 gulp.task('cover:disable', () => {
     return gulp.src("./coverconfig.json")
     .pipe(jeditor(function(json) {
