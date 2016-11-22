193f27621d61f389f107d3bc49ae92f585fe18af
kcunnane <kcunnane@KCUNNANEMB>
2016-11-21 19:44:58 -0800

    Final tests to cover all inputs to copyResults

--- test/queryRunner.test.ts
+++ test/queryRunner.test.ts
@@ -205,16 +205,20 @@ suite('Query Runner tests', () => {
         });
     }
 
-    test('Correctly copy pastes a selection', () => {
+    suite('Copy Tests', () => {
+        // ------ Common inputs and setup for copy tests  -------
         const TAB = '\t';
         const CLRF = '\r\n';
-        const finalString = '1' + TAB + '2' + TAB + CLRF +
+        const finalStringNoHeader = '1' + TAB + '2' + TAB + CLRF +
                             '3' + TAB + '4' + TAB + CLRF +
                             '5' + TAB + '6' + TAB + CLRF +
                             '7' + TAB + '8' + TAB + CLRF +
                             '9' + TAB + '10' + TAB + CLRF;
-        let testuri = 'test';
-        let testresult = {
+
+        const finalStringWithHeader = 'Col1' + TAB + 'Col2' + TAB + CLRF + finalStringNoHeader;
+
+        const testuri = 'test';
+        const testresult = {
             message: '',
             resultSubset: {
                 rowCount: 5,
@@ -227,54 +231,9 @@ suite('Query Runner tests', () => {
                 ]
             }
         };
+
         let testRange: ISlickRange[] = [{fromCell: 0, fromRow: 0, toCell: 1, toRow: 4}];
-        testSqlToolsServerClient.setup(x => x.sendRequest(TypeMoq.It.isAny(),
-                                                          TypeMoq.It.isAny())).callback(() => {
-                                                              // testing
-                                                          }).returns(() => { return Promise.resolve(testresult); });
-        testStatusView.setup(x => x.executingQuery(TypeMoq.It.isAnyString()));
-        let configResult: {[key: string]: any} = {};
-        configResult[Constants.copyIncludeHeaders] = false;
-        setupWorkspaceConfig(configResult);
 
-        let queryRunner = new QueryRunner(
-            testuri,
-            testuri,
-            testStatusView.object,
-            testSqlToolsServerClient.object,
-            testQueryNotificationHandler.object,
-            testVscodeWrapper.object
-        );
-        queryRunner.uri = testuri;
-        return queryRunner.copyResults(testRange, 0, 0).then(() => {
-            let pasteContents = ncp.paste();
-            assert.equal(pasteContents, finalString);
-        });
-    });
-
-    test('Copies selection with column headers as expected', () => {
-        const TAB = '\t';
-        const CLRF = '\r\n';
-        const finalString = 'Col1' + TAB + 'Col2' + TAB + CLRF +
-                            '1' + TAB + '2' + TAB + CLRF +
-                            '3' + TAB + '4' + TAB + CLRF +
-                            '5' + TAB + '6' + TAB + CLRF +
-                            '7' + TAB + '8' + TAB + CLRF +
-                            '9' + TAB + '10' + TAB + CLRF;
-        let testuri = 'test';
-        let testresult = {
-            message: '',
-            resultSubset: {
-                rowCount: 5,
-                rows: [
-                    ['1', '2'],
-                    ['3', '4'],
-                    ['5', '6'],
-                    ['7', '8'],
-                    ['9', '10']
-                ]
-            }
-        };
         let result: QueryExecuteCompleteNotificationResult = {
             ownerUri: testuri,
             message: undefined,
@@ -297,35 +256,117 @@ suite('Query Runner tests', () => {
             }]
         };
 
-        let testRange: ISlickRange[] = [{fromCell: 0, fromRow: 0, toCell: 1, toRow: 4}];
-        testSqlToolsServerClient.setup(x => x.sendRequest(TypeMoq.It.isAny(),
-                                                          TypeMoq.It.isAny())).callback(() => {
-                                                              // testing
-                                                          }).returns(() => { return Promise.resolve(testresult); });
-        testStatusView.setup(x => x.executingQuery(TypeMoq.It.isAnyString()));
-        testStatusView.setup(x => x.executedQuery(TypeMoq.It.isAnyString()));
-        testVscodeWrapper.setup( x => x.logToOutputChannel(TypeMoq.It.isAnyString()));
-        let configResult: {[key: string]: any} = {};
-        configResult[Constants.copyIncludeHeaders] = true;
-        setupWorkspaceConfig(configResult);
+        setup(() => {
+            testSqlToolsServerClient.setup(x => x.sendRequest(TypeMoq.It.isAny(),
+                                                            TypeMoq.It.isAny())).callback(() => {
+                                                                // testing
+                                                            }).returns(() => { return Promise.resolve(testresult); });
+            testStatusView.setup(x => x.executingQuery(TypeMoq.It.isAnyString()));
+            testStatusView.setup(x => x.executedQuery(TypeMoq.It.isAnyString()));
+            testVscodeWrapper.setup( x => x.logToOutputChannel(TypeMoq.It.isAnyString()));
+        });
 
-        let queryRunner = new QueryRunner(
-            testuri,
-            testuri,
-            testStatusView.object,
-            testSqlToolsServerClient.object,
-            testQueryNotificationHandler.object,
-            testVscodeWrapper.object
-        );
-        queryRunner.uri = testuri;
-        queryRunner.dataResolveReject = {resolve: () => {
-            // Needed to handle the result callback
-        }};
-        // Call handleResult to ensure column header info is seeded
-        queryRunner.handleResult(result);
-        return queryRunner.copyResults(testRange, 0, 0).then(() => {
-            let pasteContents = ncp.paste();
-            assert.equal(pasteContents, finalString);
+        // ------ Copy tests  -------
+        test('Correctly copy pastes a selection', () => {
+            let configResult: {[key: string]: any} = {};
+            configResult[Constants.copyIncludeHeaders] = false;
+            setupWorkspaceConfig(configResult);
+
+            let queryRunner = new QueryRunner(
+                testuri,
+                testuri,
+                testStatusView.object,
+                testSqlToolsServerClient.object,
+                testQueryNotificationHandler.object,
+                testVscodeWrapper.object
+            );
+            queryRunner.uri = testuri;
+            return queryRunner.copyResults(testRange, 0, 0).then(() => {
+                let pasteContents = ncp.paste();
+                assert.equal(pasteContents, finalStringNoHeader);
+            });
+        });
+
+        test('Copies selection with column headers set in user config', () => {
+            // Set column headers in the user config settings
+            let configResult: {[key: string]: any} = {};
+            configResult[Constants.copyIncludeHeaders] = true;
+            setupWorkspaceConfig(configResult);
+
+            let queryRunner = new QueryRunner(
+                testuri,
+                testuri,
+                testStatusView.object,
+                testSqlToolsServerClient.object,
+                testQueryNotificationHandler.object,
+                testVscodeWrapper.object
+            );
+            queryRunner.uri = testuri;
+            queryRunner.dataResolveReject = {resolve: () => {
+                // Needed to handle the result callback
+            }};
+            // Call handleResult to ensure column header info is seeded
+            queryRunner.handleResult(result);
+            return queryRunner.copyResults(testRange, 0, 0).then(() => {
+                let pasteContents = ncp.paste();
+                assert.equal(pasteContents, finalStringWithHeader);
+            });
+        });
+
+        test('Copies selection with headers when true passed as parameter', () => {
+            // Do not set column config in user settings
+            let configResult: {[key: string]: any} = {};
+            configResult[Constants.copyIncludeHeaders] = false;
+            setupWorkspaceConfig(configResult);
+
+            let queryRunner = new QueryRunner(
+                testuri,
+                testuri,
+                testStatusView.object,
+                testSqlToolsServerClient.object,
+                testQueryNotificationHandler.object,
+                testVscodeWrapper.object
+            );
+            queryRunner.uri = testuri;
+            queryRunner.dataResolveReject = {resolve: () => {
+                // Needed to handle the result callback
+            }};
+            // Call handleResult to ensure column header info is seeded
+            queryRunner.handleResult(result);
+
+            // call copyResults with additional parameter indicating to include headers
+            return queryRunner.copyResults(testRange, 0, 0, true).then(() => {
+                let pasteContents = ncp.paste();
+                assert.equal(pasteContents, finalStringWithHeader);
+            });
+        });
+
+        test('Copies selection without headers when false passed as parameter', () => {
+            // Set column config in user settings
+            let configResult: {[key: string]: any} = {};
+            configResult[Constants.copyIncludeHeaders] = true;
+            setupWorkspaceConfig(configResult);
+
+            let queryRunner = new QueryRunner(
+                testuri,
+                testuri,
+                testStatusView.object,
+                testSqlToolsServerClient.object,
+                testQueryNotificationHandler.object,
+                testVscodeWrapper.object
+            );
+            queryRunner.uri = testuri;
+            queryRunner.dataResolveReject = {resolve: () => {
+                // Needed to handle the result callback
+            }};
+            // Call handleResult to ensure column header info is seeded
+            queryRunner.handleResult(result);
+
+            // call copyResults with additional parameter indicating to not include headers
+            return queryRunner.copyResults(testRange, 0, 0, false).then(() => {
+                let pasteContents = ncp.paste();
+                assert.equal(pasteContents, finalStringNoHeader);
+            });
         });
     });
 });
