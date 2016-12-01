252e1631c2feb588cbd0950d6a23fe835f9bd5d2
Anthony Dresser <anthonydresserdev@gmail.com>
2016-12-01 11:48:33 -0800

    Default font (#430)
    * added default font
    
    * updated default font
    
    * added default font size for results grid
    
    * removed literal string

--- src/models/SqlOutputContentProvider.ts
+++ src/models/SqlOutputContentProvider.ts
@@ -78,9 +78,14 @@ export class SqlOutputContentProvider implements vscode.TextDocumentContentProvi
             } catch (e) {
                 prod = false;
             }
+            let mssqlConfig = self._vscodeWrapper.getConfiguration(Constants.extensionName);
             let editorConfig = self._vscodeWrapper.getConfiguration('editor');
-            let fontfamily = editorConfig.get<string>('fontFamily').split('\'').join('').split('"').join('');
-            let fontsize = editorConfig.get<number>('fontSize') + 'px';
+            let extensionFontFamily = mssqlConfig.get<string>(Constants.extConfigResultFontFamily).split('\'').join('').split('"').join('');
+            let extensionFontSize = mssqlConfig.get<number>(Constants.extConfigResultFontSize);
+            let fontfamily = extensionFontFamily ?
+                             extensionFontFamily :
+                             editorConfig.get<string>('fontFamily').split('\'').join('').split('"').join('');
+            let fontsize = extensionFontSize ? extensionFontSize + 'px' : editorConfig.get<number>('fontSize') + 'px';
             let fontweight = editorConfig.get<string>('fontWeight');
             res.render(path.join(LocalWebService.staticContentPath, Constants.msgContentProviderSqlOutputHtml),
                 {
