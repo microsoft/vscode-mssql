637d1db5c232ea5a31c9fb182f494755216e4744
Anthony Dresser <andresse@microsoft.com>
2016-12-02 20:13:12 -0800

    test

--- tasks/buildServer.js
+++ tasks/buildServer.js
@@ -7,12 +7,6 @@ gulp.task('appveyor:uploadTestResults', () => {
     var jobId = process.env.APPVEYOR_JOB_ID;
     var options = {
         server: 'https://ci.appveyor.com/api/testresults/xunit/' + jobId,
-        data: {
-            fileName: function(file) {
-                console.log('file: ' + file.path);
-                return path.relative(__dirname, file.path)
-            }
-        },
         callback: function (err, data, res) {
             if (err) {
                 console.log('error:' + err.toString());
