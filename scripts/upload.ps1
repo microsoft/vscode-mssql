37728cb275c3bb9334ede54539857efc8db7c393
Anthony Dresser <andresse@microsoft.com>
2016-12-02 22:26:03 -0800

    changing

--- scripts/upload.ps1
+++ scripts/upload.ps1
@@ -1,2 +1,2 @@
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'test-reports\*').path)
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path 'test-reports\*'))
