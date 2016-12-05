37eefb1582320413300d59a3c3c9b076c74671b5
Mitchell Sternke <misternk@microsoft.com>
2016-12-05 12:43:27 -0800

    Testing fix

--- test/initialization.test.ts
+++ test/initialization.test.ts
@@ -1,4 +1,5 @@
 import assert = require('assert');
+import os = require('os');
 import vscode = require('vscode');
 
 import * as Extension from '../src/extension';
@@ -26,10 +27,17 @@ suite('Initialization Tests', () => {
         Telemetry.disable();
     });
 
-    test('Connection manager is initialized properly', (done) => {
-        // Trigger extension activation
-        let uri = vscode.Uri.parse('untitled:%5Ctest.sql');
-        vscode.workspace.openTextDocument(uri);
+    test('Connection manager is initialized properly', function(done): void { // Note: this can't be an arrow function (=>), otherwise this.timeout() breaks
+        this.timeout(10000); // Service installation usually takes a bit longer than the default 2000ms on a fresh install
+
+        // Trigger extension activation by opening an empty SQL file
+        let fileUri: vscode.Uri;
+        if (os.platform() === 'win32') {
+            fileUri = vscode.Uri.parse('untitled:%5Ctest.sql');
+        } else {
+            fileUri = vscode.Uri.parse('untitled:%2Ftest.sql');
+        }
+        vscode.workspace.openTextDocument(fileUri);
 
         // Wait for the extension to activate
         ensureExtensionIsActive().then(() => {
