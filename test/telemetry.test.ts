d55a35dcdd0915547206145db81e631827752b9d
kcunnane <kcunnane@KCUNNANEMB>
2016-12-05 21:08:31 -0800

    Filter error paths to only include source files. - minimize path sent via telemetry as we only want the path after the /out/ section of the path

--- test/telemetry.test.ts
+++ test/telemetry.test.ts
@@ -1,5 +1,6 @@
 import assert = require('assert');
 import vscode = require('vscode');
+import Telemetry from '../src/models/telemetry';
 
 suite('Telemetry Tests', () => {
     test('Correct version of applicationInsights is installed', () => {
@@ -25,5 +26,18 @@ suite('Telemetry Tests', () => {
             versionOk = true;
         }
         assert.ok(versionOk, 'Version of applicationInsights must be greater than or equal to 0.15.19. Detected version was ' + versionString);
+    });
+
+    test('Path before /out/ is stripped', () => {
+        let errorString = '/User/myuser/vscode/extensions/ms-mssql.mssql-0.1.5/out/src/controller/mainController.js:216.45';
+        let expectedErrorString = 'src/controller/mainController.js:216.45';
+        let actualErrorString = Telemetry.FilterErrorPath(errorString);
+        assert.equal(actualErrorString, expectedErrorString);
+    });
+
+    test('Path without /out/ is retained', () => {
+        let errorString = '/User/should/never/happen/src/controller/mainController.js:216.45';
+        let actualErrorString = Telemetry.FilterErrorPath(errorString);
+        assert.equal(actualErrorString, errorString);
     });
 });
