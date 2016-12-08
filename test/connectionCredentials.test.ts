e0a9387d5c89f9bb25b3bce40fb62c9b1f203d15
Raymond Martin <rcmartin@smu.edu>
2016-12-08 11:37:57 -0800

    fixing up initial test for connectionCredentials

--- test/connectionCredentials.test.ts
+++ test/connectionCredentials.test.ts
@@ -11,27 +11,25 @@ import * as interfaces from '../src/models/interfaces';
 import { CredentialStore } from '../src/credentialstore/credentialstore';
 import { ConnectionProfile } from '../src/models/connectionProfile';
 import { ConnectionStore } from '../src/models/connectionStore';
-import { ConnectionConfig } from '../src/connectionconfig/connectionconfig';
 import { ConnectionCredentials } from '../src/models/ConnectionCredentials';
 import { IPrompter, IQuestion} from '../src/prompts/question';
 import { TestPrompter } from './stubs';
+import { IConnectionProfile } from '../src/models/interfaces';
 import VscodeWrapper from '../src/controllers/vscodeWrapper';
 
 import assert = require('assert');
 
 suite('ConnectionCredentials Tests', () => {
-    let defaultNamedProfile: interfaces.IConnectionProfile;
+    let defaultProfile: interfaces.IConnectionProfile;
     let prompter: TypeMoq.Mock<IPrompter>;
     let context: TypeMoq.Mock<vscode.ExtensionContext>;
-    let globalstate: TypeMoq.Mock<vscode.Memento>;
     let credentialStore: TypeMoq.Mock<CredentialStore>;
     let vscodeWrapper: TypeMoq.Mock<VscodeWrapper>;
-    let connectionConfig: TypeMoq.Mock<ConnectionConfig>;
     let connectionStore: TypeMoq.Mock<ConnectionStore>;
 
     setup(() => {
-        defaultNamedProfile = Object.assign(new ConnectionProfile(), {
-            profileName: 'defaultNamedProfile',
+        defaultProfile = Object.assign(new ConnectionProfile(), {
+            profileName: 'defaultProfile',
             server: 'namedServer',
             database: 'bcd',
             authenticationType: utils.authTypeToString(interfaces.AuthenticationTypes.SqlLogin),
@@ -42,11 +40,8 @@ suite('ConnectionCredentials Tests', () => {
 
         prompter = TypeMoq.Mock.ofType(TestPrompter);
         context = TypeMoq.Mock.ofType(stubs.TestExtensionContext);
-        globalstate = TypeMoq.Mock.ofType(stubs.TestMemento);
-        context.object.globalState = globalstate.object;
         credentialStore = TypeMoq.Mock.ofType(CredentialStore);
         vscodeWrapper = TypeMoq.Mock.ofType(VscodeWrapper);
-        connectionConfig = TypeMoq.Mock.ofType(ConnectionConfig);
         connectionStore = TypeMoq.Mock.ofType(ConnectionStore);
 
         // setup default behavior for vscodeWrapper
@@ -62,25 +57,24 @@ suite('ConnectionCredentials Tests', () => {
     });
 
     test('ensureRequiredPropertiesSet should remove password from plain text and save password to Credential Store', done => {
-        // Setup Profile Information
-        let profile = Object.assign(new ConnectionProfile(), defaultNamedProfile, {
-            profileName: 'otherServer-bcd-cde',
-            server: 'otherServer',
-            savePassword: true
+        // Setup Profile Information to have savePassword on and filled in password
+        let profile = Object.assign(new ConnectionProfile(), defaultProfile, {
+            savePassword: true,
+            password: 'hasPassword'
         });
 
+        // Setup input paramaters
         let isProfile: boolean = true;
         let isPasswordRequired: boolean = false;
         let wasPasswordEmptyInConfigFile: boolean = false;
         let answers = {};
 
-        connectionConfig.setup(x => x.addConnection(TypeMoq.It.isAny()));
-        connectionStore.setup(x => x.removeProfile(TypeMoq.It.isAny()));
-        connectionStore.setup(x => x.saveProfile(TypeMoq.It.isAny()));
-        globalstate.setup(x => x.update(TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => Promise.resolve());
+        // Mocking functions
+        connectionStore.setup(x => x.removeProfile(TypeMoq.It.isAny())).returns((profile1: IConnectionProfile) => (Promise.resolve(true)));
+        connectionStore.setup(x => x.saveProfile(TypeMoq.It.isAny())).returns((profile1: IConnectionProfile) => (Promise.resolve(profile1)));
         prompter.setup(x => x.prompt(TypeMoq.It.isAny())).returns((questions: IQuestion[]) => Promise.resolve(answers));
 
-
+        // Call function to test
         ConnectionCredentials.ensureRequiredPropertiesSet(
             profile,
             isProfile,
@@ -90,13 +84,13 @@ suite('ConnectionCredentials Tests', () => {
             connectionStore.object)
             .then( success => {
                 assert.ok(success);
+                // If function is called then password is removed
                 connectionStore.verify(x => x.removeProfile(TypeMoq.It.isAny()), TypeMoq.Times.once());
-                connectionStore.verify(x => x.saveProfile(TypeMoq.It.isAny()), TypeMoq.Times.once());
-                connectionConfig.verify(x => x.addConnection(TypeMoq.It.isAny()), TypeMoq.Times.once());
-                // done();
-            });
 
-        done();
+                // If function is called then password is stored in CredentialStore
+                connectionStore.verify(x => x.saveProfile(TypeMoq.It.isAny()), TypeMoq.Times.once());
+                done();
+            });
     });
 
 });
