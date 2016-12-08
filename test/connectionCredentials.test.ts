ff6f64981355170f04289b9f6b8cec6272cfe981
Raymond Martin <rcmartin@smu.edu>
2016-12-08 11:56:59 -0800

    outlining all test cases for password saving

--- test/connectionCredentials.test.ts
+++ test/connectionCredentials.test.ts
@@ -33,8 +33,7 @@ suite('ConnectionCredentials Tests', () => {
             server: 'namedServer',
             database: 'bcd',
             authenticationType: utils.authTypeToString(interfaces.AuthenticationTypes.SqlLogin),
-            user: 'cde',
-            password: 'asdf!@#$'
+            user: 'cde'
         });
 
 
@@ -56,6 +55,7 @@ suite('ConnectionCredentials Tests', () => {
         });
     });
 
+    // Connect with savePassword true and filled password and ensure password is saved and removed from plain text
     test('ensureRequiredPropertiesSet should remove password from plain text and save password to Credential Store', done => {
         // Setup Profile Information to have savePassword on and filled in password
         let profile = Object.assign(new ConnectionProfile(), defaultProfile, {
@@ -93,4 +93,22 @@ suite('ConnectionCredentials Tests', () => {
             });
     });
 
+    // Connect with savePassword true and filled password then connect again with a blank password and
+    // ensure that previous password persists
+    test('ensureRequiredPropertiesSet should keep Credential Store password', done => {
+    });
+
+    // Connect with savePassword true and filled password then connect again with a different password and
+    // ensure that previous password is overwritten
+    test('ensureRequiredPropertiesSet should update Credential Store Password if it has changed', done => {
+    });
+
+    // Connect with savePassword true and blank password then connect again with blank password
+    // ensure that previous password is entered at connect time persists
+    test('ensureRequiredPropertiesSet should request password and save it', done => {
+    });
+
+    // Connect with savePassword false and ensure password is never saved
+    test('ensureRequiredPropertiesSet should not save password', done => {
+    });
 });
