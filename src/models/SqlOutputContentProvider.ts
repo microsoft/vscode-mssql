0d7c26a9580699a39d288d911a16a8d5b36a9bb0
Kevin Cunnane <kcunnane@microsoft.com>
2017-02-02 17:25:53 -0800

    Fix Results Panel not Refreshing Automatically (#670)
    - Fixes #669
    - Ensures that TextDocumentContentProvider.provideTextDocumentContent always provides an updated string when being refreshed. In VSCode 1.9.0, it's vital to do this as there is a diff on the provided content and the document is only refreshed if this is different
    - Updated release version

--- src/models/SqlOutputContentProvider.ts
+++ src/models/SqlOutputContentProvider.ts
@@ -412,12 +412,16 @@ export class SqlOutputContentProvider implements vscode.TextDocumentContentProvi
         // URI needs to be encoded as a component for proper inclusion in a url
         let encodedUri = encodeURIComponent(uri.toString());
 
-        // return dummy html content that redirects to 'http://localhost:<port>' after the page loads
+        // Fix for issue #669 "Results Panel not Refreshing Automatically" - always include a unique time
+        // so that the content returned is different. Otherwise VSCode will not refresh the document since it
+        // thinks that there is nothing to be updated.
+        let timeNow = new Date().getTime();
         return `
         <html>
         <head>
             <script type="text/javascript">
                 window.onload = function(event) {
+                    console.log('reloaded results window at time ${timeNow}ms');
                     var doc = document.documentElement;
                     var styles = window.getComputedStyle(doc);
                     var backgroundcolor = styles.getPropertyValue('--background-color');
