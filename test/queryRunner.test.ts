90288bbebb879f87a32e393f8d2855103dd91806
Raymond Martin <rcmartin@smu.edu>
2017-02-07 13:35:38 -0800

    fixing tests

--- test/queryRunner.test.ts
+++ test/queryRunner.test.ts
@@ -478,13 +478,13 @@ suite('Query Runner tests', () => {
         // ------ Common inputs and setup for copy tests  -------
         const TAB = '\t';
         const CLRF = '\r\n';
-        const finalStringNoHeader = '1' + TAB + '2' + TAB + CLRF +
-                            '3' + TAB + '4' + TAB + CLRF +
-                            '5' + TAB + '6' + TAB + CLRF +
-                            '7' + TAB + '8' + TAB + CLRF +
-                            '9' + TAB + '10' + TAB + CLRF;
+        const finalStringNoHeader = '1' + TAB + '2' + CLRF +
+                            '3' + TAB + '4' + CLRF +
+                            '5' + TAB + '6' + CLRF +
+                            '7' + TAB + '8' + CLRF +
+                            '9' + TAB + '10' + CLRF;
 
-        const finalStringWithHeader = 'Col1' + TAB + 'Col2' + TAB + CLRF + finalStringNoHeader;
+        const finalStringWithHeader = 'Col1' + TAB + 'Col2' + CLRF + finalStringNoHeader;
 
         const testuri = 'test';
         const testresult = {
