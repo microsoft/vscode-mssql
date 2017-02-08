b86f9ac1209554a0d38e4f93bc698694ea50e3fa
Raymond Martin <rcmartin@smu.edu>
2017-02-08 13:37:21 -0800

    spelling correction

--- test/localization.test.ts
+++ test/localization.test.ts
@@ -5,18 +5,18 @@ import assert = require('assert');
 
 suite('Localization Tests', () => {
 
-    test('Default Localiztion Test' , done => {
+    test('Default Localization Test' , done => {
         assert.equal(LocalizedConstants.testLocalizationConstant, 'test_en');
         done();
     });
 
-    test('EN Localiztion Test' , done => {
+    test('EN Localization Test' , done => {
         LocalizedConstants.loadLocalizedConstants('en');
         assert.equal(LocalizedConstants.testLocalizationConstant, 'test_en');
         done();
     });
 
-    test('ES Localiztion Test' , done => {
+    test('ES Localization Test' , done => {
         LocalizedConstants.loadLocalizedConstants('es');
         assert.equal(LocalizedConstants.testLocalizationConstant, 'test_es');
         done();
