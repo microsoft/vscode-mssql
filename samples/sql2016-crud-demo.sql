7d27e271f7b95807f599cc6c1b3cb34ea6a15287
praveenpi <rpraveenpai065@gmail.com>
2018-10-31 19:05:17 +0530

    updated sql2016-crud-demo

--- samples/sql2016-crud-demo.sql
+++ samples/sql2016-crud-demo.sql
@@ -31,7 +31,9 @@ CREATE TABLE [dbo].[Customers]
  [CustId] [int] NOT NULL,
  [FirstName] [nvarchar](50) NOT NULL,
  [LastName] [nvarchar](50) NOT NULL,
+ [DateOfBirth][date] NOT NULL,
  [Email] [nvarchar](50) NOT NULL,
+
 
  PRIMARY KEY CLUSTERED ([CustId] ASC) ON [PRIMARY]
 );
@@ -42,19 +44,19 @@ SELECT * FROM Customers;
 GO
 
 -- Insert sample data into table
-INSERT INTO [dbo].[Customers]([CustId],[FirstName],[LastName],[Email])
+INSERT INTO [dbo].[Customers]([CustId],[FirstName],[LastName],[DateOfBirth],[Email])
 VALUES
-(1, 'Amitabh', 'Bachchan', 'angry_young_man@gmail.com'),
-(2, 'Abhishek', 'Bachchan', 'abhishek@abhishekbachchan.org'),
-(3, 'Aishwarya', 'Rai', 'ash@gmail.com'),
-(4, 'Aamir', 'Khan', 'aamir@khan.com')
+(1, 'Amitabh', 'Bachchan', '1942-07-10','angry_young_man@gmail.com'),
+(2, 'Abhishek', 'Bachchan','1976-05-06','abhishek@abhishekbachchan.org'),
+(3, 'Aishwarya', 'Rai','1973-11-06', 'ash@gmail.com'),
+(4, 'Aamir', 'Khan','1965-04-28', 'aamir@khan.com')
 GO
 
 -- Dump records (we should have 4 records now)
 SELECT * FROM Customers;
 GO
 
--- Update 2 records
+-- Update 2 records(Sets email as bachans@gmailcom to all those who have lastname as Bachan)
 UPDATE Customers
 SET Email = 'bachchans@gmail.com'
 WHERE LastName = 'Bachchan'
@@ -64,9 +66,9 @@ GO
 SELECT * FROM Customers;
 GO
 
--- Delete a record
+-- Delete a record(Customer with DateOfBirth 1965-04-28 i.e, Aamir Khan, will be deleted)
 DELETE FROM Customers
-WHERE CustId = 4
+WHERE DateOfBirth = '1965-04-28'
 GO
 
 -- Dump records (note that we only have 3 records now)
