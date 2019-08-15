4705f7bb89dae95ab3c3b9312581a1a829641541
abist <adbist@microsoft.com>
2019-08-15 11:42:53 -0700

    added copyright headers

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { Event } from 'vscode';
 import { ISlickRange, IResultsConfig, ISelectionData, ResultSetSubset } from './models/interfaces';
 
--- src/views/htmlcontent/src/js/components/contextmenu.component.ts
+++ src/views/htmlcontent/src/js/components/contextmenu.component.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+
 import { Component, Output, EventEmitter, Inject, forwardRef, OnInit } from '@angular/core';
 import {ISlickRange} from 'angular2-slickgrid';
 
