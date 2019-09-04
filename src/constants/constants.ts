946f1e15223d02b3bc3239097c3c804b0bb603c0
abist <adbist@microsoft.com>
2019-09-04 11:00:06 -0700

    added webviewpanel controller tests

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 // Collection of Non-localizable Constants
 export const languageId = 'sql';
 export const extensionName = 'mssql';
