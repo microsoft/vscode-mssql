0b889faa68723fe97415a2bf71f5482494e52280
abist <adbist@microsoft.com>
2019-09-13 10:05:38 -0700

    fix the test reports location

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,6 @@ export function activate(context: vscode.ExtensionContext): Promise<boolean> {
     if (applyLocalization) {
         LocalizedConstants.loadLocalizedConstants(vscode.env.language);
     }
-
     return controller.activate();
 }
 
