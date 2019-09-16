11df0a4655945fc998106f4e2eb611dd30079313
abist <adbist@microsoft.com>
2019-09-16 15:44:26 -0700

    added use statement for OE query context menu actions

--- src/controllers/untitledSqlDocumentService.ts
+++ src/controllers/untitledSqlDocumentService.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-
+import * as vscode from 'vscode';
 import VscodeWrapper from './vscodeWrapper';
 import { Uri } from 'vscode';
 
@@ -24,8 +24,14 @@ export default class UntitledSqlDocumentService {
 
                 // Open an untitled document. So the  file doesn't have to exist in disk
                 let doc = await this.vscodeWrapper.openMsSqlTextDocument(content);
+
                 // Show the new untitled document in the editor's first tab and change the focus to it.
                 await this.vscodeWrapper.showTextDocument(doc, 1, false);
+                const editor = vscode.window.activeTextEditor;
+                const position = editor.selection.active;
+                let newPosition = position.with(position.line + 1, 0);
+                let newSelection = new vscode.Selection(newPosition, newPosition);
+                editor.selection = newSelection;
                 resolve(doc.uri);
             } catch (error) {
                 reject(error);
