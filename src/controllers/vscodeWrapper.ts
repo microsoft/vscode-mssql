d5684bbb95dae64d7912d63a5bff2ceee7cd1f96
abist <adbist@microsoft.com>
2019-10-01 15:03:43 -0700

    remove extra line from comment

--- src/controllers/vscodeWrapper.ts
+++ src/controllers/vscodeWrapper.ts
@@ -318,7 +318,6 @@ export default class VscodeWrapper {
 
     /**
      * Called when workspace settings are changed
-     *
      */
     public get onDidChangeConfiguration(): vscode.Event<vscode.ConfigurationChangeEvent> {
         return vscode.workspace.onDidChangeConfiguration;
