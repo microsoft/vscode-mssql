84695d5cc1a9282cd35f0446fb30f50be64cf2e6
Aditya Bist <adbist@microsoft.com>
2020-02-26 15:14:33 -0800

    Revert "Close results only if it exists (#1597)"
    This reverts commit 7c4aff0d4d9280067338855d3e6d54961f0e5b83.

--- src/models/sqlOutputContentProvider.ts
+++ src/models/sqlOutputContentProvider.ts
@@ -284,17 +284,17 @@ export class SqlOutputContentProvider {
      */
     public onDidCloseTextDocument(doc: vscode.TextDocument): void {
         for (let [key, value] of this._queryResultsMap.entries()) {
-            // closes text document related to a results window we are holding
+            // closed text document related to a results window we are holding
             if (doc.uri.toString(true) === value.queryRunner.uri) {
                 value.flaggedForDeletion = true;
             }
 
-            // "closes" a results window we are holding
+            // "closed" a results window we are holding
             if (doc.uri.toString(true) === key) {
                 value.timeout = this.setRunnerDeletionTimeout(key);
-                this.closeResultsTab(doc.uri.toString());
             }
         }
+        this.closeResultsTab(doc.uri.toString());
     }
 
     /**
@@ -411,6 +411,7 @@ export class SqlOutputContentProvider {
         let controller = this._panels.get(uri);
         controller.dispose();
         this._panels.delete(uri);
+        this._queryResultsMap.delete(uri);
     }
 
     // PRIVATE HELPERS /////////////////////////////////////////////////////
