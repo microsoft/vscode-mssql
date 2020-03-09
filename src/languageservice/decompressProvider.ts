22b98e18ef22efa84758e65e9676d1c972d6722b
abist <adbist@microsoft.com>
2020-03-09 16:09:54 -0700

    add tar for unix

--- src/languageservice/decompressProvider.ts
+++ src/languageservice/decompressProvider.ts
@@ -7,11 +7,23 @@
 
 import {IDecompressProvider, IPackage} from './interfaces';
 import  {ILogger} from '../models/interfaces';
-const decompress = require('decompress-zip');
+import { PlatformInformation } from '../models/platform';
+
+const DecompressZip = require('decompress-zip');
+const DecompressTar = require('tar');
 
 export default class DecompressProvider implements IDecompressProvider {
-    public decompress(pkg: IPackage, logger: ILogger): Promise<void> {
-        const unzipper = new decompress(pkg.tmpFile.name);
+
+    private _isWindows: boolean;
+
+    constructor() {
+        PlatformInformation.getCurrent().then((platform: PlatformInformation) => {
+            this._isWindows = platform.isWindows();
+        });
+    }
+
+    private decompressZip(pkg: IPackage, logger: ILogger): Promise<void> {
+        const unzipper = new DecompressZip(pkg.tmpFile.name);
         return new Promise<void>((resolve, reject) => {
             let totalFiles = 0;
             unzipper.on('progress', async (index, fileCount) => {
@@ -27,5 +39,17 @@ export default class DecompressProvider implements IDecompressProvider {
             });
             unzipper.extract({ path: pkg.installPath });
         });
+    }
+
+    private decompressTar(pkg: IPackage, logger: ILogger): Promise<void> {
+        return undefined;
+    }
+
+    public decompress(pkg: IPackage, logger: ILogger): Promise<void> {
+        if (this._isWindows) {
+            return this.decompressZip(pkg, logger);
+        } else {
+            return this.decompressTar(pkg, logger);
+        }
     }
 }
