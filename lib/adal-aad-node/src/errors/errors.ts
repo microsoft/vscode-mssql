d96af2e58e497e84fd231cef8f2e576af4882cb5
Amir Omidi <amomidi@microsoft.com>
2020-07-28 12:26:40 -0700

    Add getAccountSecurityToken

--- lib/adal-aad-node/src/errors/errors.ts
+++ lib/adal-aad-node/src/errors/errors.ts
@@ -1,14 +1,19 @@
-import { ErrorLookup } from "../models/error";
+import { ErrorLookup, Error1Context } from "../models/error";
 
 type ErrorMapping = {
     [errorCode: number]: string;
 };
 
 const simpleErrorMapping: ErrorMapping = {
+    2: 'Something failed with the authentication, or your tokens have been deleted from the system. Please try adding your account to Azure Data Studio again.',
 };
 
 export class DefaultErrorLookup implements ErrorLookup {
     getSimpleError(errorCode: number): string {
         return simpleErrorMapping[errorCode];
+    }
+
+    getError1(errorCode: number, context: Error1Context): string {
+        return `Specified tenant with ID "${context.tenantId}" not found.`;
     }
 }
