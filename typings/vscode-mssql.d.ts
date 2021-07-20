e8cb505b6619416c7e29058b2b87a7ccb9240fe9
chgagnon <chgagnon@microsoft.com>
2021-07-20 16:01:04 -0700

    Allow ignoreFocusOut when prompting for connection

--- typings/vscode-mssql.d.ts
+++ typings/vscode-mssql.d.ts
@@ -27,8 +27,9 @@ declare module 'vscode-mssql' {
         readonly dacFx: IDacFxService;
         /**
          * Prompts the user to select an existing connection or create a new one, and then returns the result
+         * @param ignoreFocusOut Whether the quickpick prompt ignores focus out (default false)
          */
-        promptForConnection(): Promise<IConnectionInfo | undefined>
+        promptForConnection(ignoreFocusOut?: boolean): Promise<IConnectionInfo | undefined>;
     }
 
     /**
