b8fae184b309da8fa3830747c73496f974b07c82
Charles Gagnon <chgagnon@microsoft.com>
2021-07-21 15:35:16 -0700

    Export Schema Compare service API (#17017)

---
+++ src/models/contracts/schemaCompare/schemaCompareContracts.ts
@@ -0,0 +1,11 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { RequestType } from 'vscode-languageclient';
+import * as mssql from 'vscode-mssql';
+
+export namespace SchemaCompareGetDefaultOptionsRequest {
+    export const type = new RequestType<mssql.SchemaCompareGetOptionsParams, mssql.SchemaCompareOptionsResult, void, void>('schemaCompare/getDefaultOptions');
+}
