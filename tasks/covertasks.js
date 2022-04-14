cdbf29e1dc80ef0ca3c92535590d570fb5e750c0
Aditya Bist <adbist@microsoft.com>
2022-04-14 13:44:36 -0700

    change tasks

--- tasks/covertasks.js
+++ tasks/covertasks.js
@@ -53,4 +53,4 @@ gulp.task('cover:combine-html', () => {
 });
 
 // for running on the jenkins build system
-gulp.task('cover', gulp.series('cover:clean', 'cover:enableconfig', 'test', 'cover:combine-json'));
+gulp.task('test:cover', gulp.series('cover:clean', 'cover:enableconfig', 'test'));
