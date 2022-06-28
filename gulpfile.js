207672fe3a2972e01558f4e5857595ebd9e8601d
Charles Gagnon <chgagnon@microsoft.com>
2022-06-28 11:54:53 -0700

    remove unused done

--- gulpfile.js
+++ gulpfile.js
@@ -237,7 +237,7 @@ gulp.task('ext:localization', gulp.series('ext:localization:generate-eng-package
 
 gulp.task('ext:build', gulp.series('ext:localization', 'ext:copy', 'ext:clean-library-ts-files', 'ext:compile', 'ext:compile-view')); // removed lint before copy
 
-gulp.task('ext:test', async (done) => {
+gulp.task('ext:test', async () => {
 	let workspace = process.env['WORKSPACE'];
 	if (!workspace) {
 		workspace = process.cwd();
