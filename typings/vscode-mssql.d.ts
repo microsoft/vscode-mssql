4a4c7c6b8a0e7595787b6a57343b23ff0a4ccedb
ssreerama <ssreerama@microsoft.com>
2022-07-06 07:20:39 -0700

    Added new interface and comments

--- typings/vscode-mssql.d.ts
+++ typings/vscode-mssql.d.ts
@@ -506,10 +506,22 @@ declare module 'vscode-mssql' {
 		description: string;
 		displayName: string;
 	}
+
+	/*
+	* Interface containing Deployment options from <DacFx>\Source\DeploymentApi\DacDeployOptions.cs
+	*/
 	export interface DeploymentOptions {
 		doNotDropObjectTypes: DacDeployOptionPropertyObject;
 		excludeObjectTypes: DacDeployOptionPropertyObject;
 		booleanOptionsDictionary: { [key: string]: DacDeployOptionPropertyBoolean };
+	}
+
+	/*
+	* Interface containing option value and option name
+	*/
+	export interface IOptionWithValue {
+		optionName: string;
+		checked: boolean;
 	}
 
 	/**
