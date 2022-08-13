73ed2fef75d826e6eda38c147d41428bf06cbe1f
Lewis Sanchez <lewissanchez@microsoft.com>
2022-08-12 17:58:36 -0700

    Revert "Adds ts files to coverage report using remapIstanbul"
    This reverts commit 0e8885048d263fac7043efc088a3f36629bd92bd.

--- tasks/covertasks.js
+++ tasks/covertasks.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var del = require('del');
 var jeditor = require("gulp-json-editor");
 var istanbulReport = require('gulp-istanbul-report');
-var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 
 gulp.task('cover:clean', function (done) {
 	return del('coverage', done);
@@ -15,15 +14,6 @@ gulp.task('cover:enableconfig', () => {
 			return json; // must return JSON object.
 		}))
 		.pipe(gulp.dest("./out", { 'overwrite': true }));
-});
-
-gulp.task('remap-istanbul', () => {
-	return gulp.src('./coverage/coverage.json')
-		.pipe(remapIstanbul({
-			reports: {
-				'json': './coverage/coverage.json',
-			}
-		}));
 });
 
 gulp.task('cover:enable', gulp.series('cover:clean', 'cover:enableconfig'));
@@ -63,4 +53,4 @@ gulp.task('cover:combine-html', () => {
 });
 
 // for running on the ADO build system
-gulp.task('test:cover', gulp.series('cover:clean', 'cover:enableconfig', 'test', 'remap-istanbul'));
+gulp.task('test:cover', gulp.series('cover:clean', 'cover:enableconfig', 'test'));
