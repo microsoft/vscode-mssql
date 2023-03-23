afd42ef0107b0dde412d6289886c9b8913bc1065
Christopher Suh <chris.s.suh@gmail.com>
2023-03-23 16:42:22 -0700

    fix localized constants message

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1205,8 +1205,8 @@ export default class MainController implements vscode.Disposable {
 			// Prompt to reload VS Code when below settings are updated.
 			if (e.affectsConfiguration(Constants.azureAuthLibrary)
 				|| e.affectsConfiguration(Constants.enableSqlAuthenticationProvider)) {
-				if ((vscode.workspace.getConfiguration(Constants.azureAuthLibrary).get('authenticationLibrary') === 'ADAL') {
-					void vscode.window.showInformationMessage(LocalizedConstants.deprecatedOption);
+				if ((vscode.workspace.getConfiguration(Constants.azureAuthLibrary).get('authenticationLibrary') === 'ADAL')) {
+					void vscode.window.showInformationMessage(LocalizedConstants.deprecatedMessage);
 				}
 				await this.displayReloadMessage();
 			}
