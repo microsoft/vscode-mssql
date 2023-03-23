8e762abdb6bfeabe733b16e6adad6afeb4401110
Christopher Suh <chris.s.suh@gmail.com>
2023-03-23 16:34:33 -0700

    wip

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1218,6 +1218,9 @@ export default class MainController implements vscode.Disposable {
 			// Prompt to reload VS Code when below settings are updated.
 			if (e.affectsConfiguration(Constants.azureAuthLibrary)
 				|| e.affectsConfiguration(Constants.enableSqlAuthenticationProvider)) {
+				if ((vscode.workspace.getConfiguration(Constants.azureAuthLibrary).get('authenticationLibrary') === 'ADAL') {
+					void vscode.window.showInformationMessage(LocalizedConstants.deprecatedOption);
+				}
 				await this.displayReloadMessage();
 			}
 		}
