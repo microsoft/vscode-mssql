ca9bfb0c331a308d839f13bb560fbd0ddd38a56b
Christopher Suh <chris.s.suh@gmail.com>
2023-03-23 17:27:21 -0700

    fix constants

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1203,9 +1203,9 @@ export default class MainController implements vscode.Disposable {
 			}
 
 			// Prompt to reload VS Code when below settings are updated.
-			if (e.affectsConfiguration(Constants.azureAuthLibrary)
+			if (e.affectsConfiguration(Constants.mssqlAzureAuthLibrary)
 				|| e.affectsConfiguration(Constants.enableSqlAuthenticationProvider)) {
-				if ((vscode.workspace.getConfiguration(Constants.azureAuthLibrary).get('authenticationLibrary') === 'ADAL')) {
+				if ((vscode.workspace.getConfiguration(Constants.extensionName).get(Constants.azureAuthLibrary) === 'ADAL')) {
 					void vscode.window.showInformationMessage(LocalizedConstants.deprecatedMessage);
 				}
 				await this.displayReloadMessage();
