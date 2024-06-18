e489fe30b4cf7f2692023cc3fc472618a1948c12
Aasim <aaskhan@microsoft.com>
2024-06-18 14:44:57 -0700

    Update eslint configuration and add eslint-plugin-jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import tseslint from 'typescript-eslint';
 import notice from "eslint-plugin-notice";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
   {
@@ -11,7 +12,8 @@ export default [
       parser: tseslint.parser
     },
     plugins: {
-      notice
+      notice,
+      jsdoc
     },
     rules: {
       "notice/notice": [
@@ -29,7 +31,41 @@ export default [
           }
         },
 
-      ]
-    }
+      ],
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "constructor-super": "warn",
+      "curly": "off",
+      "eqeqeq": "warn",
+      "no-buffer-constructor": "warn",
+      "no-caller": "warn",
+      "no-debugger": "warn",
+      "no-duplicate-case": "warn",
+      "no-duplicate-imports": "off",
+      "no-eval": "warn",
+      "no-async-promise-executor": "off",
+      "no-extra-semi": "warn",
+      "no-new-wrappers": "warn",
+      "no-redeclare": "off",
+      "no-sparse-arrays": "warn",
+      "no-throw-literal": "off",
+      "no-unsafe-finally": "warn",
+      "no-unused-labels": "warn",
+      "no-restricted-globals": [
+        "warn",
+        "name",
+        "length",
+        "event",
+        "closed",
+        "external",
+        "status",
+        "origin",
+        "orientation",
+        "context"
+      ], // non-complete list of globals that are easy to access unintentionally
+      "no-var": "off",
+      "semi": "off",
+      "jsdoc/no-types": "warn",
+    },
   }
 ];
