c10f61b9cc566df0279aa228478932cb7994b9a2
Aasim <aaskhan@microsoft.com>
2024-06-18 14:26:31 -0700

    chore: Update eslint configuration and remove unused files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import notice from "eslint-plugin-notice";
 export default [
   {
     files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['src/prompts/**/*.ts'],  // Ignore prompts files as they are copied from other repos
     languageOptions: {
       parser: tseslint.parser
     },
@@ -20,11 +21,11 @@ export default [
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 `,
           onNonMatchingHeader: 'prepend',
           messages: {
-            whenFailedToMatch: "Couldn't find copyright statement",
-            whenOutsideTolerance: "The copyright statement isn't in the right format"
+            whenFailedToMatch: "Missing or incorrectly formatted copyright statement.",
           }
         },
 
