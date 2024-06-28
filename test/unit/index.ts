ddd08d85083d332b46f5feac487cd9d49996cf48
Lewis Sanchez <87730006+lewis-sanchez@users.noreply.github.com>
2024-06-28 12:58:31 -0700

    Move tests into dedicated subfolder (#17913)
    * Move tests and fix coverage report issue
    
    * Clean up debugging console logs
    
    * Correct folder name to unit

---
+++ test/unit/index.ts
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//
+// PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
+//
+// This file is providing the test runner to use when running extension tests.
+// By default the test runner in use is Mocha based.
+//
+// You can provide your own test runner if you want to override it by exporting
+// a function run(testRoot: string, clb: (error:Error) => void) that the extension
+// host can call to run the tests. The test runner is expected to use console.log
+// to report the results back to the caller. When the tests are finished, return
+// a possible error to the callback or null if none.
+
+import * as testRunner from './istanbultestrunner';
+
+// You can directly control Mocha options by uncommenting the following lines
+// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+testRunner.configure(
+	// Mocha Options
+	{
+		ui: 'tdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+		reporter: 'pm-mocha-jenkins-reporter',
+		reporterOptions: {
+			junit_report_name: 'Extension Tests',
+			junit_report_path: __dirname + '../../test-reports/extension_tests.xml',
+			junit_report_stack: 1
+		},
+		useColors: true // colored output from test results
+	},
+	// Coverage configuration options
+	{
+		coverConfig: '../../../coverconfig.json'
+	});
+
+module.exports = testRunner;
---
+++ test/unit/initialization.test.ts
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import * as Extension from '../../src/extension';
+import ConnectionManager from '../../src/controllers/connectionManager';
+import MainController from '../../src/controllers/mainController';
+
+function ensureExtensionIsActive(): Promise<any> {
+	return new Promise((resolve, reject) => {
+		waitForExtensionToBeActive(resolve);
+	});
+}
+
+function waitForExtensionToBeActive(resolve): void {
+	if (typeof (vscode.extensions.getExtension('ms-mssql.mssql')) === 'undefined' ||
+		!vscode.extensions.getExtension('ms-mssql.mssql').isActive) {
+		// tslint:disable-next-line no-invalid-this Bind to the mocha context so it resolves properly
+		setTimeout(waitForExtensionToBeActive.bind(this, resolve), 50);
+	} else {
+		resolve();
+	}
+}
+
+suite('Initialization Tests', () => {
+
+	test('Connection manager is initialized properly', (done) => {
+		// Wait for the extension to activate
+		ensureExtensionIsActive().then(async () => {
+			// Verify that the connection manager was initialized properly
+			let controller: MainController = await Extension.getController();
+			let connectionManager: ConnectionManager = controller.connectionManager;
+			assert.notStrictEqual(undefined, connectionManager.client);
+			done();
+		});
+	});
+});
