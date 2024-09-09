127fa7fa8668d6cbc73813540ae267eec37ce07c
Benjin Dubishar <benjin.dubishar@gmail.com>
2024-09-09 14:33:27 -0700

    Fixing some connection string bugs (#18030)
    * Fixing bug where connection string with integrated auth crashes connection dialog
    
    * added displayName for MRU list to avoid setting profileName unexpectedly
    
    * PR feedback

--- src/connectionconfig/connectionDialogWebviewController.ts
+++ src/connectionconfig/connectionDialogWebviewController.ts
@@ -81,7 +81,7 @@ export class ConnectionDialogWebviewController extends ReactWebviewPanelControll
 
 	private async loadRecentConnections() {
 		const recentConnections = this._mainController.connectionManager.connectionStore.loadAllConnections(true).map(c => c.connectionCreds);
-		const dialogConnections = [];
+		const dialogConnections: IConnectionDialogProfile[] = [];
 		for (let i = 0; i < recentConnections.length; i++) {
 			dialogConnections.push(await this.initializeConnectionForDialog(recentConnections[i]));
 		}
@@ -105,28 +105,41 @@ export class ConnectionDialogWebviewController extends ReactWebviewPanelControll
 		this.state.connectionProfile = emptyConnection;
 	}
 
-	private async initializeConnectionForDialog(connection: IConnectionInfo) {
+	private async initializeConnectionForDialog(connection: IConnectionInfo): Promise<IConnectionDialogProfile> {
 		// Load the password if it's saved
 		const isConnectionStringConnection = connection.connectionString !== undefined && connection.connectionString !== '';
-		const password = await this._mainController.connectionManager.connectionStore.lookupPassword(connection, isConnectionStringConnection);
 		if (!isConnectionStringConnection) {
+			const password = await this._mainController.connectionManager.connectionStore.lookupPassword(connection, isConnectionStringConnection);
 			connection.password = password;
 		} else {
-			connection.connectionString = '';
-			// extract password from connection string it starts after 'Password=' and ends before ';'
-			const passwordIndex = password.indexOf('Password=') === -1 ? password.indexOf('password=') : password.indexOf('Password=');
-			if (passwordIndex !== -1) {
-				const passwordStart = passwordIndex + 'Password='.length;
-				const passwordEnd = password.indexOf(';', passwordStart);
-				if (passwordEnd !== -1) {
-					connection.password = password.substring(passwordStart, passwordEnd);
+			// If the connection is a connection string connection with SQL Auth:
+			//   * the full connection string is stored as the "password" in the credential store
+			//   * we need to extract the password from the connection string
+			// If the connection is a connection string connection with a different auth type, then there's nothing in the credential store.
+
+			const connectionString = await this._mainController.connectionManager.connectionStore.lookupPassword(connection, isConnectionStringConnection);
+
+			if (connectionString) {
+				const passwordIndex = connectionString.toLowerCase().indexOf('password=');
+
+				if (passwordIndex !== -1) {
+					// extract password from connection string; found between 'Password=' and the next ';'
+					const passwordStart = passwordIndex + 'password='.length;
+					const passwordEnd = connectionString.indexOf(';', passwordStart);
+					if (passwordEnd !== -1) {
+						connection.password = connectionString.substring(passwordStart, passwordEnd);
+					}
+
+					// clear the connection string from the IConnectionDialogProfile so that the ugly connection string key
+					// that's used to look up the actual connection string (with password) isn't displayed
+					connection.connectionString = '';
 				}
 			}
-
 		}
+
 		const dialogConnection = connection as IConnectionDialogProfile;
-		// Set the profile name
-		dialogConnection.profileName = dialogConnection.profileName ?? getConnectionDisplayName(connection);
+		// Set the display name
+		dialogConnection.displayName = dialogConnection.profileName ? dialogConnection.profileName : getConnectionDisplayName(connection);
 		return dialogConnection;
 	}
 
--- src/sharedInterfaces/connectionDialog.ts
+++ src/sharedInterfaces/connectionDialog.ts
@@ -66,9 +66,9 @@ export interface IConnectionDialogProfile extends vscodeMssql.IConnectionInfo {
 	savePassword?: boolean;
 	emptyPasswordInput?: boolean;
 	azureAuthType?: vscodeMssql.AzureAuthType;
+    /** display name for the MRU pane; should be set to the profileName if available, otherwise generated from connection details */
+    displayName?: string;
 }
-
-
 
 export interface ConnectionDialogContextProps extends FormContextProps<ConnectionDialogWebviewState, IConnectionDialogProfile> {
 	theme: Theme;
