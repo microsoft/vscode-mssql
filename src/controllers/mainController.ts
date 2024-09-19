0ef46360c2d2d9ed5abaed931102e2a0a8d0640c
Lewis Sanchez <lewissanchez@microsoft.com>
2024-09-19 12:19:05 -0700

    Last lint fix

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -1,5 +1,4 @@
-﻿/* eslint-disable prettier/prettier */
-/*---------------------------------------------------------------------------------------------
+﻿/*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
