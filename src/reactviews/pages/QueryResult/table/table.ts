0045a7adc04eaa415636ec905bbc403c35747bef
Christopher Suh <chris.s.suh@gmail.com>
2024-09-27 12:44:35 -0700

    cleanup

--- src/reactviews/pages/QueryResult/table/table.ts
+++ src/reactviews/pages/QueryResult/table/table.ts
@@ -15,6 +15,7 @@ import * as DOM from "./dom";
 import { IDisposableDataProvider } from "./dataProvider";
 import { CellSelectionModel } from "./plugins/cellSelectionModel.plugin";
 import { mixin } from "./objects";
+import { HeaderFilter } from "./plugins/headerFilter.plugin";
 // import { MouseWheelSupport } from './plugins/mousewheelTableScroll.plugin';
 
 function getDefaultOptions<T extends Slick.SlickData>(): Slick.GridOptions<T> {
@@ -72,7 +73,6 @@ export class Table<T extends Slick.SlickData> implements IThemable {
         // this._tableContainer.className = //TODO: class name for styles
         let gridParent = document.getElementById("grid-parent");
         if (gridParent) {
-            // this._tableContainer.style.width = `calc(100% - 36px)`;
             this._tableContainer.style.width = `${(gridParent?.clientWidth - ACTIONBAR_WIDTH).toString()}px`;
             const height = gridParent?.clientHeight - 5;
             this._tableContainer.style.height = `${height.toString()}px`;
@@ -85,6 +85,7 @@ export class Table<T extends Slick.SlickData> implements IThemable {
             [],
             newOptions,
         );
+        this.registerPlugin(new HeaderFilter());
 
         if (configuration && configuration.columns) {
             this.columns = configuration.columns;
