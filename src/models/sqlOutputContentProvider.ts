c3a1463128594e4e32c59f618543c8993fd1d8ad
laurennat <laurennathan@vt.edu>
2024-10-15 17:31:35 -0700

    Porting hid execution plan button under rich feature flag (#18219) and Query Plan Bugs (#18215) (#18221)
    * Laurennat/query plan bugs (#18215)
    
    * some bug fixes
    
    * fixed bug that appeared after auto-switch
    
    * fixed spacing in find nodes
    
    * made filter box bigger
    
    * hid execution plan button under rich feature flag (#18219)

--- src/models/sqlOutputContentProvider.ts
+++ src/models/sqlOutputContentProvider.ts
@@ -55,6 +55,7 @@ export class SqlOutputContentProvider {
     >();
     private _panels = new Map<string, WebviewPanelController>();
     private _queryResultWebviewController: QueryResultWebviewController;
+    private _executionPlanOptions: ExecutionPlanOptions = {};
 
     // CONSTRUCTOR /////////////////////////////////////////////////////////
     constructor(
@@ -248,9 +249,15 @@ export class SqlOutputContentProvider {
                 await this.createWebviewController(uri, title, queryRunner);
             }
         } else {
+            if (executionPlanOptions) {
+                this._executionPlanOptions = executionPlanOptions;
+            } else {
+                this._executionPlanOptions = {};
+            }
             this._queryResultWebviewController.addQueryResultState(
                 uri,
-                executionPlanOptions?.includeEstimatedExecutionPlanXml ?? false,
+                this._executionPlanOptions?.includeEstimatedExecutionPlanXml ??
+                    false,
             );
         }
         if (queryRunner) {
@@ -388,7 +395,11 @@ export class SqlOutputContentProvider {
                 if (!this.isRichExperiencesEnabled) {
                     this._panels.get(uri).proxy.sendEvent("start", panelUri);
                 } else {
-                    this._queryResultWebviewController.addQueryResultState(uri);
+                    this._queryResultWebviewController.addQueryResultState(
+                        uri,
+                        this._executionPlanOptions
+                            ?.includeEstimatedExecutionPlanXml ?? false,
+                    );
                     await vscode.commands.executeCommand("queryResult.focus");
                     this._queryResultWebviewController.getQueryResultState(
                         uri,
