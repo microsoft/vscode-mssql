8c6ce4366fae70517a4c91d0eae1658093c82ede
Aasim Khan <aasimkhan30@gmail.com>
2024-10-23 10:34:59 -0700

    Fixing test coverage by adding more report types and updating to nyc from istanbul

---
+++ test/unit/runTest.ts
@@ -0,0 +1,32 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from "path";
+
+import { runTests } from "@vscode/test-electron";
+
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
+
+        // The path to test runner
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, "./index");
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: [],
+        });
+    } catch (err) {
+        console.error("Failed to run tests", err);
+        process.exit(1);
+    }
+}
+
+void main();
