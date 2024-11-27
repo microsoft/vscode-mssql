a9e01a79fe0ef4819c641931139091ae08aee499
Aasim Khan <aasimkhan30@gmail.com>
2024-11-26 19:35:33 -0800

    Fixing typings for azdataGraph lib

--- typings/azdataGraph.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'azdataGraph/dist/build' {
-	const bind: any;
-	export default bind;
-	export const mxClient: any;
-	export function azdataQueryPlan(queryPlanConfiguration: any): any;
-  }
