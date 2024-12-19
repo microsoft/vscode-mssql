c2e18b1eeaa7171bdff613409295bb5ca0c58c2b
Lewis Sanchez <lewissanchez@microsoft.com>
2024-12-19 14:02:37 -0800

    Add EditData RPC registration hook

--- src/editData/editDataWebViewController.ts
+++ src/editData/editDataWebViewController.ts
@@ -10,12 +10,26 @@ import {
     EditDataReducers,
     EditDataWebViewState,
 } from "../sharedInterfaces/editData";
+import ConnectionManager from "../controllers/connectionManager";
+import { ScriptingService } from "../scripting/scriptingService";
+import UntitledSqlDocumentService from "../controllers/untitledSqlDocumentService";
+import { TreeNodeInfo } from "../objectExplorer/treeNodeInfo";
+import { ObjectExplorerUtils } from "../objectExplorer/objectExplorerUtils";
+import { Deferred } from "../protocol";
+import { ScriptOperation } from "../models/contracts/scripting/scriptingRequest";
 
 export class EditDataWebViewController extends ReactWebviewPanelController<
     EditDataWebViewState,
     EditDataReducers
 > {
-    constructor(context: vscode.ExtensionContext, data?: EditDataWebViewState) {
+    constructor(
+        context: vscode.ExtensionContext,
+        private node: TreeNodeInfo,
+        private readonly connectionManager: ConnectionManager,
+        private readonly scriptingService: ScriptingService,
+        private readonly untitledSqlDocumentService: UntitledSqlDocumentService,
+        data?: EditDataWebViewState,
+    ) {
         super(context, "editData", data ?? {}, {
             title: vscode.l10n.t("Edit Data (Preview)"),
             viewColumn: vscode.ViewColumn.Active,
@@ -32,5 +46,42 @@ export class EditDataWebViewController extends ReactWebviewPanelController<
                 ),
             },
         });
+
+        void this.initialize();
     }
+
+    private async initialize() {
+        const nodeUri = ObjectExplorerUtils.getNodeUri(this.node);
+        let connectionCreds = Object.assign({}, this.node.connectionInfo);
+        const databaseName = ObjectExplorerUtils.getDatabaseName(this.node);
+
+        // if not connected or different database then make a new connection
+        if (
+            !this.connectionManager.isConnected(nodeUri) ||
+            connectionCreds.database !== databaseName
+        ) {
+            connectionCreds.database = databaseName;
+            if (!this.connectionManager.isConnecting(nodeUri)) {
+                const promise = new Deferred<boolean>();
+                await this.connectionManager.connect(
+                    nodeUri,
+                    connectionCreds,
+                    promise,
+                );
+                await promise;
+            }
+        }
+
+        const selectStatement = await this.scriptingService.script(
+            this.node,
+            nodeUri,
+            ScriptOperation.Select,
+        );
+        const editor =
+            await this.untitledSqlDocumentService.newQuery(selectStatement);
+
+        this.registerRpcHandlers();
+    }
+
+    private registerRpcHandlers() {}
 }
