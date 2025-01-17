042b80f1e3af5fb400d549eadf982d41aa595f67
Lauren Nathan <laurennathan@microsoft.com>
2025-01-17 13:30:04 -0800

    fixed tooltip bug in new query pane

--- src/reactviews/pages/QueryResult/commandBar.tsx
+++ src/reactviews/pages/QueryResult/commandBar.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Button, makeStyles } from "@fluentui/react-components";
+import { Button, makeStyles, Tooltip } from "@fluentui/react-components";
 import { useContext, useState } from "react";
 import { QueryResultContext } from "./queryResultStateProvider";
 import { useVscodeWebview } from "../../common/vscodeWebviewProvider";
@@ -98,48 +98,63 @@ const CommandBar = (props: CommandBarProps) => {
                 ></Button>
             )}
 
-            <Button
-                appearance="subtle"
-                onClick={(_event) => {
-                    saveResults("csv");
-                }}
-                icon={
-                    <img
-                        className={classes.buttonImg}
-                        src={saveAsCsvIcon(context.theme)}
-                    />
-                }
-                className="codicon saveCsv"
-                title={locConstants.queryResult.saveAsCsv}
-            />
-            <Button
-                appearance="subtle"
-                onClick={(_event) => {
-                    saveResults("json");
-                }}
-                icon={
-                    <img
-                        className={classes.buttonImg}
-                        src={saveAsJsonIcon(context.theme)}
-                    />
-                }
-                className="codicon saveJson"
-                title={locConstants.queryResult.saveAsJson}
-            />
-            <Button
-                appearance="subtle"
-                onClick={(_event) => {
-                    saveResults("excel");
-                }}
-                icon={
-                    <img
-                        className={classes.buttonImg}
-                        src={saveAsExcelIcon(context.theme)}
-                    />
-                }
-                className="codicon saveExcel"
-                title={locConstants.queryResult.saveAsExcel}
-            />
+            <Tooltip
+                content={locConstants.queryResult.saveAsCsv}
+                relationship="label"
+            >
+                <Button
+                    appearance="subtle"
+                    onClick={(_event) => {
+                        saveResults("csv");
+                    }}
+                    icon={
+                        <img
+                            className={classes.buttonImg}
+                            src={saveAsCsvIcon(context.theme)}
+                        />
+                    }
+                    className="codicon saveCsv"
+                    title={locConstants.queryResult.saveAsCsv}
+                />
+            </Tooltip>
+            <Tooltip
+                content={locConstants.queryResult.saveAsJson}
+                relationship="label"
+            >
+                <Button
+                    appearance="subtle"
+                    onClick={(_event) => {
+                        saveResults("json");
+                    }}
+                    icon={
+                        <img
+                            className={classes.buttonImg}
+                            src={saveAsJsonIcon(context.theme)}
+                        />
+                    }
+                    className="codicon saveJson"
+                    title={locConstants.queryResult.saveAsJson}
+                />
+            </Tooltip>
+            <Tooltip
+                content={locConstants.queryResult.saveAsExcel}
+                relationship="label"
+            >
+                <Button
+                    appearance="subtle"
+                    onClick={(_event) => {
+                        saveResults("excel");
+                    }}
+                    icon={
+                        <img
+                            className={classes.buttonImg}
+                            src={saveAsExcelIcon(context.theme)}
+                        />
+                    }
+                    className="codicon saveExcel"
+                    title={locConstants.queryResult.saveAsExcel}
+                />
+            </Tooltip>
         </div>
     );
 };
