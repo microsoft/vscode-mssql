9f7f33143c9aa82c9618824ecb7a5d8c0f76ecac
Aasim Khan <aasimkhan30@gmail.com>
2025-02-06 22:12:45 -0800

    remove log statements

--- src/reactviews/pages/SchemaDesigner/schemaDesignerEntityEditor.tsx
+++ src/reactviews/pages/SchemaDesigner/schemaDesignerEntityEditor.tsx
@@ -3,15 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { useRef } from "react";
 import { IEntity } from "../../../sharedInterfaces/schemaDesigner";
 
 export const SchemaDesignerEntityEditor = (props: { entity: IEntity }) => {
-    const renderCount = useRef(0);
-    renderCount.current += 1;
-    console.log(
-        `SchemaDesignerEntityEditor render count: ${renderCount.current}`,
-    );
     if (!props.entity) {
         return undefined;
     }
