b37aafc318e4f55045fb3a3426384a75a70f31e9
Lewis Sanchez <lewissanchez@microsoft.com>
2025-02-11 20:56:25 -0800

    Improve selection panel layout

--- src/reactviews/pages/SchemaCompare/components/SelectionPanel.tsx
+++ src/reactviews/pages/SchemaCompare/components/SelectionPanel.tsx
@@ -4,20 +4,31 @@
  *--------------------------------------------------------------------------------------------*/
 
 import {
+    Label,
     Input,
     Button,
     makeStyles,
     shorthands,
+    useId,
     type InputProps,
 } from "@fluentui/react-components";
 
 import { ArrowSwapFilled, AddFilled } from "@fluentui/react-icons";
 
 const useStyles = makeStyles({
-    container: {
+    topAndSideMargin: {
+        ...shorthands.margin("32px", "32px", "0"),
+    },
+
+    positionItemsHorizontally: {
         display: "flex",
         flexDirection: "row",
-        ...shorthands.margin("32px", "32px", "0"),
+    },
+
+    positionItemsVertically: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "2px",
     },
 
     inputWidth: {
@@ -26,39 +37,86 @@ const useStyles = makeStyles({
         minwidth: "300px",
     },
 
-    horizonalMargin: {
+    swapButton: {
+        height: "40px",
+        position: "relative",
+        top: "20px",
         ...shorthands.margin("0", "32px"),
     },
 });
 
-const SelectionPanel: React.FC<InputProps> = (props: InputProps) => {
+interface Props extends InputProps {}
+
+const SelectionPanel: React.FC<Props> = (props: Props) => {
+    const sourceId = useId("source");
+    const targetId = useId("target");
     const classes = useStyles();
 
     return (
-        <div className={classes.container}>
-            <Input
-                className={classes.inputWidth}
-                {...props}
-                value="C:\DatabaseProjects\SampleProj\SampleProj.sqlproj"
-                readOnly
-            />
+        <>
+            <div
+                className={[
+                    classes.positionItemsHorizontally,
+                    classes.topAndSideMargin,
+                ].join(" ")}
+            >
+                <div
+                    className={[
+                        classes.positionItemsVertically,
+                        classes.inputWidth,
+                    ].join(" ")}
+                >
+                    <Label
+                        htmlFor={sourceId}
+                        size={props.size}
+                        disabled={props.disabled}
+                    >
+                        Source
+                    </Label>
+                    <div className={classes.positionItemsHorizontally}>
+                        <Input
+                            id={sourceId}
+                            className={classes.inputWidth}
+                            {...props}
+                            value="C:\DatabaseProjects\SampleProj\SampleProj.sqlproj"
+                            readOnly
+                        />
+                        <Button size="large" icon={<AddFilled />} />
+                    </div>
+                </div>
 
-            <Button size="large" icon={<AddFilled />} />
-            <Button
-                className={classes.horizonalMargin}
-                size="large"
-                icon={<ArrowSwapFilled />}
-            />
+                <Button
+                    className={classes.swapButton}
+                    size="large"
+                    icon={<ArrowSwapFilled />}
+                />
 
-            <Input
-                className={classes.inputWidth}
-                {...props}
-                value="C:\DatabaseProjects\SampleProj2\SampleProj2.sqlproj"
-                readOnly
-            />
-
-            <Button size="large" icon={<AddFilled />} />
-        </div>
+                <div
+                    className={[
+                        classes.positionItemsVertically,
+                        classes.inputWidth,
+                    ].join(" ")}
+                >
+                    <Label
+                        htmlFor={targetId}
+                        size={props.size}
+                        disabled={props.disabled}
+                    >
+                        Target
+                    </Label>
+                    <div className={classes.positionItemsHorizontally}>
+                        <Input
+                            id={targetId}
+                            className={classes.inputWidth}
+                            {...props}
+                            value="C:\DatabaseProjects\SampleProj\SampleProj.sqlproj"
+                            readOnly
+                        />
+                        <Button size="large" icon={<AddFilled />} />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
