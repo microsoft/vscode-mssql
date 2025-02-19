0fc18f5f0a4df323ba079c0bbc3284ed53ee0ed9
Christopher Suh (from Dev Box) <chsuh@microsoft.com>
2025-02-19 10:16:02 -0800

    remove refreshKey, which was causing multiple grid rendering issue

--- src/reactviews/pages/QueryResult/resultGrid.tsx
+++ src/reactviews/pages/QueryResult/resultGrid.tsx
@@ -10,7 +10,6 @@ import {
     useEffect,
     useImperativeHandle,
     useRef,
-    useState,
 } from "react";
 import "../../media/slickgrid.css";
 import { ACTIONBAR_WIDTH_PX, range, Table } from "./table/table";
@@ -81,7 +80,6 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
 
         const context = useContext(QueryResultContext);
         const gridContainerRef = useRef<HTMLDivElement>(null);
-        const [refreshkey, setRefreshKey] = useState(0);
         const refreshGrid = () => {
             if (gridContainerRef.current) {
                 while (gridContainerRef.current.firstChild) {
@@ -90,7 +88,6 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
                     );
                 }
             }
-            setRefreshKey((prev) => prev + 1);
         };
         const resizeGrid = (width: number, height: number) => {
             let gridParent: HTMLElement | null;
@@ -315,7 +312,7 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
                     ),
                 );
             }
-        }, [refreshkey]);
+        }, []);
 
         useImperativeHandle(ref, () => ({
             refreshGrid,
