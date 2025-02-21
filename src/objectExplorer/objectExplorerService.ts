c7e31f7f6e2c860d003e192b97670ba67cc24d6f
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-02-20 22:42:02 -0800

    Added handling for instance when failure to expand node

--- src/objectExplorer/objectExplorerService.ts
+++ src/objectExplorer/objectExplorerService.ts
@@ -360,7 +360,12 @@ export class ObjectExplorerService {
     private handleExpandSessionNotification(): NotificationHandler<ExpandResponse> {
         const self = this;
         const handler = (result: ExpandResponse) => {
-            if (result && result.nodes) {
+            if (!result) {
+                return undefined;
+            }
+
+            if (result.nodes && !result.errorMessage) {
+                // successfully received children from SQL Tools Service
                 const credentials =
                     self._sessionIdToConnectionCredentialsMap.get(
                         result.sessionId,
@@ -397,6 +402,40 @@ export class ObjectExplorerService {
                     ) {
                         let promise = self._expandParamsToPromiseMap.get(key);
                         promise.resolve(children);
+                        self._expandParamsToPromiseMap.delete(key);
+                        self._expandParamsToTreeNodeInfoMap.delete(key);
+                        return;
+                    }
+                }
+            } else {
+                // failure to expand node; display error
+
+                if (result.errorMessage) {
+                    self._connectionManager.vscodeWrapper.showErrorMessage(
+                        result.errorMessage,
+                    );
+                }
+
+                const expandParams: ExpandParams = {
+                    sessionId: result.sessionId,
+                    nodePath: result.nodePath,
+                };
+                const parentNode = self.getParentFromExpandParams(expandParams);
+
+                const errorNode = new vscode.TreeItem(
+                    LocalizedConstants.ObjectExplorer.ErrorLoadingRefreshToTryAgain,
+                    TreeItemCollapsibleState.None,
+                );
+
+                self._treeNodeToChildrenMap.set(parentNode, [errorNode]);
+
+                for (let key of self._expandParamsToPromiseMap.keys()) {
+                    if (
+                        key.sessionId === expandParams.sessionId &&
+                        key.nodePath === expandParams.nodePath
+                    ) {
+                        let promise = self._expandParamsToPromiseMap.get(key);
+                        promise.resolve([errorNode as TreeNodeInfo]);
                         self._expandParamsToPromiseMap.delete(key);
                         self._expandParamsToTreeNodeInfoMap.delete(key);
                         return;
