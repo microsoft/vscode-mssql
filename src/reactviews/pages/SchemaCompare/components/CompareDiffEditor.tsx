cef8571bccb489d641e49c44d1eddd302946f94f
Lewis Sanchez <lewissanchez@microsoft.com>
2025-02-25 13:45:46 -0800

    Clean up compare diff editor component

--- src/reactviews/pages/SchemaCompare/components/CompareDiffEditor.tsx
+++ src/reactviews/pages/SchemaCompare/components/CompareDiffEditor.tsx
@@ -12,31 +12,13 @@ const CompareDiffEditor = () => {
     const modified =
         "CREATE TABLE [dbo].[Address] (\r\n [AddressID] INT NOT NULL PRIMARY KEY CLUSTERED ([AddressID] ASC),\r\n [PersonID] INT NULL,\r\n [Street] VARCHAR (255) NULL,\r\n [City] VARCHAR (255) NULL,\r\n [State] VARCHAR (255) NULL,\r\n [ZipCode] VARCHAR (10) NULL,\r\n FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Person] ([PersonID])\r\n);\r\nGO";
 
-    const handleEditorDidMount = (editor: any, _: any) => {
-        const originalModel = editor.getOriginalEditor().getModel();
-        const modifiedModel = editor.getModifiedEditor().getModel();
-
-        const invertDiffs = () => {
-            const originalValue = originalModel.getValue();
-            const modifiedValue = modifiedModel.getValue();
-
-            // Swap the values
-            originalModel.setValue(modifiedValue);
-            modifiedModel.setValue(originalValue);
-        };
-
-        // Call the function to invert the diffs
-        invertDiffs();
-    };
-
     return (
         <div style={{ height: "60vh" }}>
             <DiffEditor
                 height="60vh"
                 language="sql"
-                original={original}
-                modified={modified}
-                onMount={handleEditorDidMount}
+                original={modified}
+                modified={original}
                 options={{
                     renderSideBySide: true,
                     renderOverviewRuler: true,
