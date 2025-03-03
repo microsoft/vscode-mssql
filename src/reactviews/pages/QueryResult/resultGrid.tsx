5e9c3f159baa02d23f813c31030b23e441a533eb
Christopher Suh <chris.s.suh@gmail.com>
2025-03-03 10:55:48 -0800

    fix grid issue

--- src/reactviews/pages/QueryResult/resultGrid.tsx
+++ src/reactviews/pages/QueryResult/resultGrid.tsx
@@ -10,6 +10,7 @@ import {
     useEffect,
     useImperativeHandle,
     useRef,
+    useState,
 } from "react";
 import "../../media/slickgrid.css";
 import { ACTIONBAR_WIDTH_PX, range, Table } from "./table/table";
@@ -79,6 +80,7 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
             return undefined;
         }
         const gridContainerRef = useRef<HTMLDivElement>(null);
+        const [refreshKey, setRefreshKey] = useState(0);
         if (!props.gridParentRef) {
             return undefined;
         }
@@ -96,7 +98,8 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
             console.log("table: " + table);
             if (!table) {
                 context.log("resizeGrid - table is not initialized");
-                return;
+                refreshGrid();
+                setRefreshKey(refreshKey + 1);
             }
             let gridParent: HTMLElement | null;
             if (!props.resultSetSummary) {
@@ -138,7 +141,7 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
             }
         };
 
-        useEffect(() => {
+        const createTable = () => {
             console.log("useEffect create table");
             const filter = async () => {
                 let hasNewFilters = await table.setupFilterState();
@@ -318,7 +321,7 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
                     ),
                 );
             }
-        }, []);
+        };
 
         useImperativeHandle(ref, () => ({
             refreshGrid,
@@ -326,6 +329,10 @@ const ResultGrid = forwardRef<ResultGridHandle, ResultGridProps>(
             hideGrid,
             showGrid,
         }));
+
+        useEffect(() => {
+            createTable();
+        }, [refreshKey]);
 
         return <div id="gridContainter" ref={gridContainerRef}></div>;
     },
