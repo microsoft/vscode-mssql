42277c475e15ba8e6bcad5ed239f4e6bab8ae11f
Lauren Nathan <laurennathan@microsoft.com>
2025-03-26 13:26:12 -0700

    launch with new ui disabled in tests

--- test/e2e/utils/launchVscodeWithMsSqlExt.ts
+++ test/e2e/utils/launchVscodeWithMsSqlExt.ts
@@ -9,7 +9,7 @@ import * as path from "path";
 import { ElectronApplication, Page } from "@playwright/test";
 import { getVsCodeVersionName } from "./envConfigReader";
 
-export async function launchVsCodeWithMssqlExtension(): Promise<{
+export async function launchVsCodeWithMssqlExtension(oldUi?: boolean): Promise<{
     electronApp: ElectronApplication;
     page: Page;
 }> {
@@ -18,6 +18,11 @@ export async function launchVsCodeWithMssqlExtension(): Promise<{
         await downloadAndUnzipVSCode(vsCodeVersionName);
 
     const mssqlExtensionPath = path.resolve(__dirname, "../../../");
+
+    const settingsOption = oldUi
+        ? `--user-data-dir=${path.join(process.cwd(), "test", "resources", "launchDir")}`
+        : "";
+
     const electronApp = await electron.launch({
         executablePath: vsCodeExecutablePath,
         args: [
@@ -30,6 +35,7 @@ export async function launchVsCodeWithMssqlExtension(): Promise<{
             "--profile-temp", // "debug in a clean environment"
             "--skip-release-notes",
             "--skip-welcome",
+            settingsOption,
         ],
     });
 
