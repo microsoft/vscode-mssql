86f82af934e9ee2b8b09f525a8a3f8d65acda378
Christopher Suh <chsuh@microsoft.com>
2025-03-28 15:59:18 -0700

    Fix console errors (#19027)
    * fix console errors
    
    * remove commented code
    
    * fix lint

--- src/reactviews/pages/QueryResult/table/table.ts
+++ src/reactviews/pages/QueryResult/table/table.ts
@@ -197,7 +197,6 @@ export class Table<T extends Slick.SlickData> implements IThemable {
         this.setSelectionModel(this.selectionModel);
         this.mapMouseEvent(this._grid.onContextMenu);
         this.mapMouseEvent(this._grid.onClick);
-        this.mapMouseEvent(this._grid.onHeaderClick);
         this.mapMouseEvent(this._grid.onDblClick);
         this._grid.onColumnsResized.subscribe(() => console.log("oncolumnresize"));
         this.style(styles);
@@ -267,19 +266,17 @@ export class Table<T extends Slick.SlickData> implements IThemable {
         slickEvent.subscribe((e: Slick.EventData) => {
             const originalEvent = (e as JQuery.TriggeredEvent).originalEvent;
             const cell = this._grid.getCellFromEvent(originalEvent!);
-            const anchor =
-                originalEvent instanceof MouseEvent
-                    ? { x: originalEvent.x, y: originalEvent.y }
-                    : (originalEvent!.srcElement as HTMLElement);
-            console.log("anchor: ", anchor);
-            console.log("cell: ", cell);
-            this.handleLinkClick(cell);
-            // emitter.fire({ anchor, cell });
+            if (cell) {
+                this.handleLinkClick(cell);
+            }
         });
     }
 
     private handleLinkClick(cell: Slick.Cell): void {
         const columnInfo = this.resultSetSummary.columnInfo[cell.cell - 1];
+        if (!columnInfo) {
+            return;
+        }
         if (columnInfo.isXml || columnInfo.isJson) {
             this.linkHandler(
                 this.getCellValue(cell.row, cell.cell),
