0a86573025ede49f9d1c32a886d0bc926bd2fd80
Aasim Khan <aaskhan@microsoft.com>
2025-03-31 13:58:17 -0700

    Fixing node centering after a new table is added.  (#19043)

--- src/reactviews/pages/SchemaDesigner/schemaDesignerStateProvider.tsx
+++ src/reactviews/pages/SchemaDesigner/schemaDesignerStateProvider.tsx
@@ -35,6 +35,7 @@ export interface SchemaDesignerContextProps
     deleteTable: (table: SchemaDesigner.Table) => Promise<boolean>;
     deleteSelectedNodes: () => void;
     getTableWithForeignKeys: (tableId: string) => SchemaDesigner.Table | undefined;
+    setCenter: (nodeId: string, shouldZoomIn?: boolean) => void;
 }
 
 const SchemaDesignerContext = createContext<SchemaDesignerContextProps>(
@@ -168,13 +169,7 @@ const SchemaDesignerStateProvider: React.FC<SchemaDesignerProviderProps> = ({ ch
             reactFlow.addNodes(nodeWithPosition);
             reactFlow.addEdges(edgesForNewTable);
             requestAnimationFrame(async () => {
-                await reactFlow.setCenter(
-                    nodeWithPosition.position.x,
-                    nodeWithPosition.position.y,
-                    {
-                        duration: 500,
-                    },
-                );
+                setCenter(nodeWithPosition.id, true);
             });
 
             eventBus.emit("getScript");
@@ -277,6 +272,20 @@ const SchemaDesignerStateProvider: React.FC<SchemaDesignerProviderProps> = ({ ch
                 nodes: [],
                 edges: selectedEdges,
             });
+        }
+    };
+
+    const setCenter = (nodeId: string, shouldZoomIn: boolean = false) => {
+        const node = reactFlow.getNode(nodeId) as Node<SchemaDesigner.Table>;
+        if (node) {
+            void reactFlow.setCenter(
+                node.position.x + flowUtils.getTableWidth() / 2,
+                node.position.y + flowUtils.getTableHeight(node.data) / 2,
+                {
+                    zoom: shouldZoomIn ? 1 : reactFlow.getZoom(),
+                    duration: 500,
+                },
+            );
         }
     };
 
