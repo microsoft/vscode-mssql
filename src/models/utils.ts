929f23cd8349aaf7c840f81149266e50c293f965
Aasim Khan <aaskhan@microsoft.com>
2025-04-14 16:15:23 -0700

    Using Ids to match is same connection profile. (#19142)
    * Using Ids to match is same connection profile.
    
    * Fixing casting

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -261,6 +261,10 @@ export function isSameProfile(
     currentProfile: IConnectionProfile,
     expectedProfile: IConnectionProfile,
 ): boolean {
+    if (currentProfile.id && expectedProfile.id) {
+        // If connection profile has an id, use that to compare connections
+        return currentProfile.id === expectedProfile.id;
+    }
     if (currentProfile === undefined) {
         return false;
     }
@@ -308,6 +312,13 @@ export function isSameConnectionInfo(
     conn: IConnectionInfo,
     expectedConn: IConnectionInfo,
 ): boolean {
+    // If connection info has an id, use that to compare connections
+    const connId = (conn as IConnectionProfile).id;
+    const expectedConnId = (expectedConn as IConnectionProfile).id;
+    if (connId && expectedConnId) {
+        return connId === expectedConnId;
+    }
+    // If no id, compare the connection string or other properties
     return conn.connectionString || expectedConn.connectionString
         ? conn.connectionString === expectedConn.connectionString
         : // Azure MFA connections
