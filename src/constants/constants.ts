fc6577f5ddfd5af28a03f347ad0549c6028fdd08
Lewis Sanchez <lewissanchez@microsoft.com>
2025-04-16 13:30:46 -0700

    Open schema compare from cmd palette

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -45,6 +45,7 @@ export const cmdPauseQueryHistory = "mssql.pauseQueryHistoryCapture";
 export const cmdCommandPaletteQueryHistory = "mssql.commandPaletteQueryHistory";
 export const cmdNewQuery = "mssql.newQuery";
 export const cmdSchemaCompare = "mssql.schemaCompare";
+export const cmdSchemaCompareOpenFromCommandPalette = "mssql.schemaCompareOpenFromCommandPalette";
 export const cmdManageConnectionProfiles = "mssql.manageProfiles";
 export const cmdClearPooledConnections = "mssql.clearPooledConnections";
 export const cmdRebuildIntelliSenseCache = "mssql.rebuildIntelliSenseCache";
