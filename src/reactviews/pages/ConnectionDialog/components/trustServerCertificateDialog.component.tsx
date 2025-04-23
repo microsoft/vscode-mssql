2351917354d81da1d8a9a3c38687b2d3901edc62
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-04-23 13:10:10 -0700

    Respect ENTER and ESC keys in Trust Server Cert dialog

--- src/reactviews/pages/ConnectionDialog/components/trustServerCertificateDialog.component.tsx
+++ src/reactviews/pages/ConnectionDialog/components/trustServerCertificateDialog.component.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { ConnectionDialogContext } from "../connectionDialogStateProvider";
 import {
     Button,
@@ -27,14 +27,32 @@ export const TrustServerCertificateDialog = ({
     dialogProps: TrustServerCertDialogProps;
 }) => {
     const context = useContext(ConnectionDialogContext)!;
+    // eslint-disable-next-line no-restricted-syntax -- Ref needs to be null, not undefined
+    const trustCertButtonRef = useRef<HTMLButtonElement | null>(null);
 
     if (context.state.dialog?.type !== "trustServerCert") {
         return undefined;
     }
 
+    useEffect(() => {
+        // Focus the "Trust Server Certificate" button when the dialog opens
+        if (trustCertButtonRef.current) {
+            trustCertButtonRef.current.focus();
+        }
+    }, []);
+
+    const handleCancel = () => {
+        context.closeDialog();
+    };
+
     return (
         <Dialog open={dialogProps.type === "trustServerCert"}>
-            <DialogSurface>
+            <DialogSurface
+                onKeyDown={(e: React.KeyboardEvent) => {
+                    if (e.key === "Escape") {
+                        handleCancel();
+                    }
+                }}>
                 <DialogBody>
                     <DialogTitle>{locConstants.connectionDialog.connectionErrorTitle}</DialogTitle>
                     <DialogContent>
@@ -53,25 +71,20 @@ export const TrustServerCertificateDialog = ({
                     </DialogContent>
                     <DialogActions>
                         <Button
+                            ref={trustCertButtonRef}
                             appearance="primary"
                             onClick={() => {
                                 context.closeDialog();
-
                                 context.formAction({
                                     propertyName: "trustServerCertificate",
                                     value: true,
                                     isAction: false,
                                 });
-
                                 context.connect();
                             }}>
                             {locConstants.connectionDialog.enableTrustServerCertificateButton}
                         </Button>
-                        <Button
-                            appearance="secondary"
-                            onClick={() => {
-                                context.closeDialog();
-                            }}>
+                        <Button appearance="secondary" onClick={handleCancel}>
                             {locConstants.common.cancel}
                         </Button>
                     </DialogActions>
