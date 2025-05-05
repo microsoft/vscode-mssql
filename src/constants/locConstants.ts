c8f2624b3875184510a309ac781693b7187cadb5
Aasim Khan <aasimkhan30@gmail.com>
2025-05-04 21:06:48 -0700

    Add handling for missing account display name and tenant ID in multipleMatchingTokensError

--- src/constants/locConstants.ts
+++ src/constants/locConstants.ts
@@ -630,7 +630,12 @@ export class ConnectionDialog {
             comment: ["{0} is the connection name"],
         });
     };
-    public static multipleMatchingTokensError(accountDisplayName: string, tenantId: string) {
+    public static multipleMatchingTokensError(accountDisplayName?: string, tenantId?: string) {
+        if (!accountDisplayName || !tenantId) {
+            return l10n.t(
+                "Authentication error for account. Resolving this requires clearing your token cache, which will sign you out of all connected accounts.",
+            );
+        }
         return l10n.t({
             message:
                 "Authentication error for account '{0}' (tenant '{1}'). Resolving this requires clearing your token cache, which will sign you out of all connected accounts.",
