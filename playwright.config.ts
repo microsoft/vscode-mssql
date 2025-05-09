e83a96f7d99cc684cb4e82d543c1b6c1c4c25cd9
Lauren Nathan <laurennathan@microsoft.com>
2025-05-09 11:45:11 -0700

    change retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
     fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    retries: 0,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
