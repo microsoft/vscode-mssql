723ed30272e6e1c674cfd43c33514b9eb8c815a5
Lauren Nathan <laurennathan@microsoft.com>
2025-05-14 13:50:32 -0700

    fix unit test

--- src/controllers/sharedExecutionPlanUtils.ts
+++ src/controllers/sharedExecutionPlanUtils.ts
@@ -115,7 +115,7 @@ export async function createExecutionPlanGraphs(
                 e,
                 true, // includeErrorMessage
             );
-            return;
+            return state;
         }
     }
 
