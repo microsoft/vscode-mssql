6b5458cfe8efef3f33befa357f7062d6a402b3fc
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-05-28 13:20:32 -0700

    Comments

--- src/objectExplorer/objectExplorerService.ts
+++ src/objectExplorer/objectExplorerService.ts
@@ -520,7 +520,10 @@ export class ObjectExplorerService {
         connectionInfo?: IConnectionInfo,
     ): Promise<CreateSessionResult | undefined> {
         if (!this._rootTreeNodeArray) {
-            await this.getRootNodes(); // ensure root nodes are loaded
+            // Ensure root nodes are loaded.
+            // This is needed when connection attempts are made before OE has been activated
+            // e.g. User clicks connect button from Editor before ever viewing the OE panel
+            await this.getRootNodes();
         }
 
         const createSessionActivity = startActivity(
