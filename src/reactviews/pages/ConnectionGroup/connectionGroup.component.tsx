d3b0fbc95bf1f67ef57a921a3d19171e0ac976fa
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-06-05 13:37:24 -0700

    adding color picker

--- src/reactviews/pages/ConnectionGroup/connectionGroup.component.tsx
+++ src/reactviews/pages/ConnectionGroup/connectionGroup.component.tsx
@@ -16,14 +16,59 @@ import {
     Textarea,
     MessageBar,
     Field,
+    makeStyles,
+    Popover,
+    PopoverTrigger,
+    PopoverSurface,
 } from "@fluentui/react-components";
-
+import {
+    ColorArea,
+    ColorPicker,
+    ColorPickerProps,
+    ColorSlider,
+} from "@fluentui/react-color-picker";
+import { TinyColor } from "@ctrl/tinycolor";
 import { locConstants as Loc } from "../../common/locConstants";
 import {
     ConnectionGroupSpec,
     ConnectionGroupState,
 } from "../../../sharedInterfaces/connectionGroup";
 import { useState } from "react";
+
+const useStyles = makeStyles({
+    previewColor: {
+        width: "80px",
+        borderRadius: "4px",
+        border: "1px solid #ccc",
+        alignSelf: "stretch",
+        "@media (forced-colors: active)": {
+            forcedColorAdjust: "none",
+        },
+    },
+    row: {
+        display: "flex",
+        gap: "10px",
+    },
+    sliders: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    colorContainer: {
+        display: "flex",
+        alignItems: "center",
+        gap: "15px",
+    },
+});
+
+/** Generates a random Hex color */
+function getRandomColor(): string {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 export const ConnectionGroupDialog = ({
     state,
@@ -35,10 +80,19 @@ export const ConnectionGroupDialog = ({
     closeDialog: () => void;
 }) => {
     const formStyles = useFormStyles();
+    const styles = useStyles();
+
+    const intialHsvColor = new TinyColor(state.color || getRandomColor()).toHsv();
 
     const [groupName, setGroupName] = useState(state.existingGroupName || "");
     const [description, setDescription] = useState(state.description || "");
-    const [color, setColor] = useState(state.color || "");
+    const [color, setColor] = useState(intialHsvColor);
+    const [pickerColor, setPickerColor] = useState(intialHsvColor);
+    const [popoverOpen, setPopoverOpen] = useState(false);
+
+    const handleChange: ColorPickerProps["onColorChange"] = (_, data) => {
+        setColor({ ...data.color, a: 1 });
+    };
 
     return (
         <Dialog open={true /* standalone dialog always open*/}>
@@ -46,7 +100,7 @@ export const ConnectionGroupDialog = ({
                 <DialogBody>
                     <DialogTitle>
                         {state.existingGroupName
-                            ? `Edit Connection Group - ${state.existingGroupName}`
+                            ? `Edit Connection Group: ${state.existingGroupName}`
                             : "Create New Connection Group"}
                     </DialogTitle>
                     <DialogContent>
@@ -59,7 +113,7 @@ export const ConnectionGroupDialog = ({
                             </>
                         )}
 
-                        <Field className={formStyles.formComponentDiv} label="Name">
+                        <Field className={formStyles.formComponentDiv} label="Name" required>
                             <Input
                                 value={groupName}
                                 onChange={(_e, data) => {
@@ -81,14 +135,65 @@ export const ConnectionGroupDialog = ({
                         </Field>
 
                         <Field className={formStyles.formComponentDiv} label="Color">
-                            <Input
-                                type="text"
-                                value={color}
-                                onChange={(_e, data) => {
-                                    setColor(data.value);
-                                }}
-                                placeholder="Enter color (e.g., #FF5733)"
-                            />
+                            <div className={styles.colorContainer}>
+                                <div
+                                    className={styles.previewColor}
+                                    style={{
+                                        backgroundColor: new TinyColor(pickerColor).toRgbString(),
+                                    }}
+                                    onClick={() => {
+                                        setPopoverOpen(true);
+                                    }}
+                                />
+                                <Popover
+                                    open={popoverOpen}
+                                    trapFocus
+                                    onOpenChange={(_, data) => setPopoverOpen(data.open)}>
+                                    <PopoverTrigger disableButtonEnhancement>
+                                        <Button style={{ minWidth: "120px" }}>Choose color</Button>
+                                    </PopoverTrigger>
+
+                                    <PopoverSurface>
+                                        <ColorPicker
+                                            color={new TinyColor(color).toHsv()}
+                                            onColorChange={handleChange}>
+                                            <ColorArea
+                                                inputX={{ "aria-label": "Saturation" }}
+                                                inputY={{ "aria-label": "Brightness" }}
+                                            />
+                                            <div className={styles.row}>
+                                                <div className={styles.sliders}>
+                                                    <ColorSlider aria-label="Hue" />
+                                                </div>
+                                                <div
+                                                    className={styles.previewColor}
+                                                    style={{
+                                                        backgroundColor: new TinyColor(
+                                                            color,
+                                                        ).toRgbString(),
+                                                    }}
+                                                />
+                                            </div>
+                                        </ColorPicker>
+                                        <div className={styles.row}>
+                                            <Button
+                                                appearance="primary"
+                                                onClick={() => {
+                                                    setPickerColor(color);
+                                                    setPopoverOpen(false);
+                                                }}>
+                                                Ok
+                                            </Button>
+                                            <Button
+                                                onClick={() => {
+                                                    setPopoverOpen(false);
+                                                }}>
+                                                Cancel
+                                            </Button>
+                                        </div>
+                                    </PopoverSurface>
+                                </Popover>
+                            </div>
                         </Field>
                     </DialogContent>
                     <DialogActions>
@@ -99,11 +204,13 @@ export const ConnectionGroupDialog = ({
                                 saveConnectionGroup({
                                     name: groupName,
                                     description: description || undefined,
-                                    color: color || undefined,
+                                    color:
+                                        new TinyColor(color).toHexString(false /* allow3Char */) ||
+                                        undefined,
                                 });
                             }}
-                            disabled={false}>
-                            Create Connection Group
+                            disabled={groupName.length === 0}>
+                            Save Connection Group
                         </Button>
                         <Button
                             appearance="secondary"
