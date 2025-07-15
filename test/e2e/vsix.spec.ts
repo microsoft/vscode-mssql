80704465c81d92eef3864e6e4f8576a3bc168822
Aasim Khan <aaskhan@microsoft.com>
2025-07-15 13:22:43 -0700

    Adding vsix based smoke tests (#19765)
    * Enhance VS Code launch utility and update settings for connection groups
    
    * Fixing args
    
    * Running PR smoke tests on vsix
    
    * temp test
    
    * Add log message for missing VSIX path in launchVsCodeWithMssqlExtension
    
    * Export BUILT_VSIX_PATH in build-and-test workflow and update vsixPath assignment in launchVsCodeWithMssqlExtension
    
    * Update BUILT_VSIX_PATH export to search in the current directory for smoke tests
    
    * Fixing build
    
    * fix selectors
    
    * Refactor SQL Server tab navigation to check selection state before clicking
    
    * temp test modification
    
    * Refactor launchVsCodeWithMssqlExtension to include settings option when installing VSIX extension
    
    * Reverting test related changes
    
    * reverting gitignore changes
    
    * reverting gitignore
    
    * cleaning up launch file
    
    * Refactor writeCoverage to ensure directory creation using promises
    
    * Refactor writeCoverage to use fs.promises for directory creation
    
    * Add VSIX based tests for testing package breaks
    
    * Refactor launchVsCodeWithMssqlExtension to use options object for configuration
    
    * Forcing package break
    
    * reverting package break
    
    * Enhance documentation for mssqlExtensionLaunchConfig and update VSIX test descriptions
    
    * Refactor test names for clarity in VSIX based tests

---
+++ test/e2e/vsix.spec.ts
@@ -0,0 +1,50 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ElectronApplication, Page } from "@playwright/test";
+import { launchVsCodeWithMssqlExtension } from "./utils/launchVscodeWithMsSqlExt";
+import { addConnectionButton } from "./utils/commonSelectors";
+import { test, expect } from "./baseFixtures";
+import { screenshotOnFailure } from "./utils/screenshotOnError";
+import { getWebviewByTitle } from "./utils/testHelpers";
+
+/**
+ * Running a few launch specific tests to ensure the VSIX package is working correctly.
+ * Since code-coverage is not supported for VSIX based tests, we are converting all tests to use the VSIX package.
+ */
+test.describe("MSSQL Extension - VSIX Based tests", async () => {
+    let vsCodeApp: ElectronApplication;
+    let vsCodePage: Page;
+
+    test.beforeEach(async () => {
+        // Launch vscode with the VSIX package
+        const { electronApp, page } = await launchVsCodeWithMssqlExtension({
+            useVsix: true,
+        });
+        vsCodeApp = electronApp;
+        vsCodePage = page;
+    });
+
+    // Test if extension activates correctly
+    test("Extension activates correctly", async () => {
+        expect(vsCodeApp).not.toBeNull();
+    });
+
+    // Test if the webview loads correctly
+    test("MSSQL add connection webview is loaded correctly", async () => {
+        const addButton = await vsCodePage.locator(addConnectionButton);
+        await addButton.click();
+        const connectionDialog = await getWebviewByTitle(vsCodePage, "Connection Dialog");
+        await connectionDialog.locator("#connectButton").isVisible();
+    });
+
+    test.afterEach(async ({}, testInfo) => {
+        await screenshotOnFailure(vsCodePage, testInfo);
+    });
+
+    test.afterEach(async () => {
+        await vsCodeApp.close();
+    });
+});
