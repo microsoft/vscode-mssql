bb8f5ed349796c425696160b4c701074415a0f24
Benjin Dubishar <benjin.dubishar@gmail.com>
2025-07-16 11:36:58 -0700

    Fixing firewall rule dialog account selection reset upon submission (#19782)
    * checking array dependencies correctly in react effect trigger
    
    * preserving selection

--- src/reactviews/pages/AddFirewallRule/addFirewallRule.component.tsx
+++ src/reactviews/pages/AddFirewallRule/addFirewallRule.component.tsx
@@ -87,6 +87,13 @@ export const AddFirewallRuleDialog = ({
     // Update selected tenant when state.tenants changes
     useEffect(() => {
         if (state.accounts.length > 0) {
+            if (
+                selectedAccountId &&
+                state.accounts.find((account) => account.accountId === selectedAccountId)
+            ) {
+                return; // Currently-selected account is still valid; no need to reset selections
+            }
+
             const accountId = state.accounts[0].accountId;
 
             setSelectedAccountId(accountId);
@@ -98,7 +105,7 @@ export const AddFirewallRuleDialog = ({
                 setTenantDisplayText(formatTenant(tenant));
             }
         }
-    }, [state.accounts, state.tenants]);
+    }, [JSON.stringify(state.accounts), JSON.stringify(state.tenants)]);
 
     useEffect(() => {
         if (state.clientIp) {
