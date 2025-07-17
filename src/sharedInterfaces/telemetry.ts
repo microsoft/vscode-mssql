e0860f8186610dac41f680c1b9897dfe2ca3e631
Lewis Sanchez <87730006+lewis-sanchez@users.noreply.github.com>
2025-07-17 11:32:42 -0700

    Add logging to Schema Compare (#19795)
    * Add logging to schema compare controller
    
    * Add conn to logs for troubleshooting
    
    * Remove old comment to avoid confusion
    
    * Move extension ID to const
    
    * Use getErrorMessage to get error messages
    
    * Add error telemetry
    
    * Remove unnecessary stringify
    
    * use getErrorMessage in previously missed spots

--- src/sharedInterfaces/telemetry.ts
+++ src/sharedInterfaces/telemetry.ts
@@ -30,6 +30,9 @@ export enum TelemetryViews {
 }
 
 export enum TelemetryActions {
+    GetDatabaseProjectScriptFiles = "GetDatabaseProjectScriptFiles",
+    GetDatabaseProjectSchemaProvider = "GetDatabaseProjectSchemaProvider",
+    ListingDatabasesForActiveServer = "ListingDatabasesForActiveServer",
     Compare = "Compare",
     Switch = "Switch",
     OpenScmp = "OpenScmp",
