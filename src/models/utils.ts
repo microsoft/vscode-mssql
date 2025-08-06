ec726eeed83a609e7c5c1867ceda95c9deffcf53
Aasim Khan <aaskhan@microsoft.com>
2025-08-06 12:01:44 -0700

    fix import style

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -5,7 +5,7 @@
 
 import * as os from "os";
 import * as path from "path";
-const findRemoveSync = require("find-remove");
+import findRemoveSync from "find-remove";
 import * as vscode from "vscode";
 import * as Constants from "../constants/constants";
 import { IAzureSignInQuickPickItem, IConnectionProfile, AuthenticationTypes } from "./interfaces";
