c8eb3e163507bf708c5a632e3de0545e9ebf4089
Aasim Khan <aasimkhan30@gmail.com>
2025-08-11 00:18:43 -0700

    Not copying connections for sql files opened from internal extension code.

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -907,7 +907,7 @@ export default class MainController implements vscode.Disposable {
         // Handle case where SQL file is the 1st opened document
         const activeTextEditor = this._vscodeWrapper.activeTextEditor;
         if (activeTextEditor && this._vscodeWrapper.isEditingSqlFile) {
-            this.onDidOpenTextDocument(activeTextEditor.document);
+            await this.onDidOpenTextDocument(activeTextEditor.document);
         }
         await this.sanitizeConnectionProfiles();
         await this.loadTokenCache();
@@ -2553,14 +2553,21 @@ export default class MainController implements vscode.Disposable {
      * Called by VS Code when a text document is opened. Checks if a SQL file was opened
      * to enable features of our extension for the document.
      */
-    public onDidOpenTextDocument(doc: vscode.TextDocument): void {
+    public async onDidOpenTextDocument(doc: vscode.TextDocument): Promise<void> {
         if (this._connectionMgr === undefined) {
             // Avoid processing events before initialization is complete
             return;
         }
         this._connectionMgr.onDidOpenTextDocument(doc);
 
-        if (this._previousActiveDocument && doc.languageId === Constants.languageId) {
+        const isOpenedFromUntitledService =
+            await this._untitledSqlDocumentService.isUriOpenedFromService(doc.uri.toString(true));
+
+        if (
+            this._previousActiveDocument &&
+            doc.languageId === Constants.languageId &&
+            !isOpenedFromUntitledService
+        ) {
             void this._connectionMgr.copyConnectionToFile(
                 this._previousActiveDocument.uri.toString(true),
                 doc.uri.toString(true),
--- src/controllers/untitledSqlDocumentService.ts
+++ src/controllers/untitledSqlDocumentService.ts
@@ -10,13 +10,31 @@ import VscodeWrapper from "./vscodeWrapper";
  * Service for creating untitled documents for SQL query
  */
 export default class UntitledSqlDocumentService {
+    public openedUris: Set<string> = new Set();
+
+    public isOpening = false;
+
     constructor(private vscodeWrapper: VscodeWrapper) {}
+
+    public isUriOpenedFromService(uri: string): Promise<boolean> {
+        return new Promise((resolve) => {
+            const checkUri = () => {
+                if (!this.isOpening && this.openedUris.has(uri)) {
+                    resolve(true);
+                } else {
+                    setTimeout(checkUri, 100);
+                }
+            };
+            checkUri();
+        });
+    }
 
     /**
      * Creates new untitled document for SQL query and opens in new editor tab
      * with optional content
      */
     public async newQuery(content?: string): Promise<vscode.TextEditor> {
+        this.isOpening = true;
         // Open an untitled document. So the  file doesn't have to exist in disk
         let doc = await this.vscodeWrapper.openMsSqlTextDocument(content);
         // Show the new untitled document in the editor's first tab and change the focus to it.
@@ -25,6 +43,8 @@ export default class UntitledSqlDocumentService {
             preserveFocus: false,
             preview: false,
         });
+        this.openedUris.add(editor.document.uri.toString());
+        this.isOpening = false;
         return editor;
     }
 }
