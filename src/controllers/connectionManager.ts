779870445c9ab428e0de93ffcd8381e77a47e955
Allan Cascante <acascante@microsoft.com>
2025-08-12 19:03:27 -0600

    pr feedback

--- src/controllers/connectionManager.ts
+++ src/controllers/connectionManager.ts
@@ -578,18 +578,7 @@ export default class ConnectionManager {
             let connection = self.getConnectionInfo(fileUri);
             connection.connecting = false;
 
-            let mruConnection: IConnectionInfo = <any>{};
-
-            const saveDefaultDatabase = !connection?.credentials?.database; // connection used empty database for the original connection
-
             if (Utils.isNotEmpty(result.connectionId)) {
-                // Use the original connection information to save the MRU connection.
-                // for connections that a database is not provided, the database information will be updated
-                // to the default database name, if we use the new information as the MRU connection,
-                // the connection information will be different from the saved connections (saved connection's database property is empty).
-                // When deleting the saved connection, we won't be able to find its corresponding recent connection,
-                // and the saved connection credentials will become orphaned.
-                mruConnection = Utils.deepClone(connection.credentials);
                 // Convert to credentials if it's a connection string based connection
                 if (connection.credentials.connectionString) {
                     connection.credentials = await this.populateCredentialsFromConnectionString(
@@ -611,7 +600,6 @@ export default class ConnectionManager {
                 );
                 if (result.connectionSummary && result.connectionSummary.databaseName) {
                     newCredentials.database = result.connectionSummary.databaseName;
-                    mruConnection.database = result.connectionSummary.databaseName;
                 }
                 self.handleConnectionSuccess(fileUri, connection, newCredentials, result);
                 const promise = self._uriToConnectionPromiseMap.get(result.ownerUri);
@@ -625,7 +613,6 @@ export default class ConnectionManager {
                     self._uriToConnectionCompleteParamsMap.delete(result.ownerUri);
                 }
             } else {
-                mruConnection = undefined;
                 const promise = self._uriToConnectionPromiseMap.get(result.ownerUri);
                 if (promise) {
                     if (result.errorMessage) {
@@ -645,7 +632,7 @@ export default class ConnectionManager {
                 await self.handleConnectionErrors(fileUri, connection, result);
             }
 
-            await self.tryAddMruConnection(connection, mruConnection, saveDefaultDatabase);
+            await self.tryAddMruConnection(connection);
         };
     }
 
@@ -837,18 +824,14 @@ export default class ConnectionManager {
         return updatedConn;
     }
 
-    private async tryAddMruConnection(
-        connection: ConnectionInfo,
-        newConnection: IConnectionInfo,
-        saveAsDefaultDatabase: boolean = false,
-    ): Promise<void> {
-        if (newConnection) {
-            let connectionToSave: IConnectionInfo = Object.assign({}, newConnection);
+    /**
+     * Tries to add a connection to the list of most recently used connections. It saves the original credentials used to create the connection.
+     * @param connection The original connection returned from the connect operation. It could be null if the connection operation was not successful.
+     */
+    private async tryAddMruConnection(connection: ConnectionInfo): Promise<void> {
+        if (connection?.credentials) {
+            let connectionToSave: IConnectionInfo = Object.assign({}, connection.credentials);
             try {
-                // if the connection was originally for an empty database, the database should be empty when being saved to match the original connection
-                if (saveAsDefaultDatabase) {
-                    connectionToSave = { ...connectionToSave, database: undefined };
-                }
                 await this._connectionStore.addRecentlyUsed(connectionToSave);
                 connection.connectHandler(true);
             } catch (err) {
