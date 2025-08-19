b088da4db01695e5d00044d1a9170dba58ef10bc
Aasim Khan <aasimkhan30@gmail.com>
2025-08-19 13:16:43 -0700

    Switching webview compilation to tsc.

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,8 @@ try {
   execSync('yarn build:prepare', { stdio: 'inherit' });
   execSync('yarn build:extension', { stdio: 'inherit' });
   execSync(`yarn build:extension-bundle ${prodArg}`, { stdio: 'inherit' });
-  execSync(`yarn build:webviews ${prodArg}`, { stdio: 'inherit' });
+  execSync(`yarn build:webviews`, { stdio: 'inherit' });
+  execSync(`yarn build:webviews-bundle ${prodArg}`, { stdio: 'inherit' });
 } catch (error) {
   process.exit(1);
 }
