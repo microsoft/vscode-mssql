683638b26b7cc285f618dd3d1a9413959477a869
Benjin Dubishar <benjin.dubishar@gmail.com>
2025-08-25 11:34:52 -0700

    Dev/benjin/add fabric helper support (#20020)
    * Adding helper class for accessing Fabric APIs
    
    * moving http methods to helper class

--- src/sharedInterfaces/webview.ts
+++ src/sharedInterfaces/webview.ts
@@ -15,6 +15,11 @@ export enum ApiStatus {
     Error = "error",
 }
 
+export interface Status {
+    status: ApiStatus;
+    message?: string;
+}
+
 export interface WebviewTelemetryActionEvent {
     /**
      * The view in which the event occurred.
