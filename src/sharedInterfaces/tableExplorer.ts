4dad82c546f380d72c60c835e93a71b56254d15a
Lewis Sanchez <lewissanchez@microsoft.com>
2025-09-10 16:58:46 -0700

    Adds table explorer contracts

---
+++ src/sharedInterfaces/tableExplorer.ts
@@ -0,0 +1,167 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IConnectionProfile } from "../models/interfaces";
+
+export interface IEditSessionOperationParams {
+    ownerUri: string;
+}
+
+export interface EditInitializeFiltering {
+    LimitResults?: number | undefined;
+}
+
+export enum EditRowState {
+    clean = 0,
+    dirtyInsert = 1,
+    dirtyDelete = 2,
+    dirtyUpdate = 3,
+}
+
+export interface EditRow {
+    cells: DbCellValue[];
+    id: number;
+    isDirty: boolean;
+    state: EditRowState;
+}
+
+export interface DbCellValue {
+    displayValue: string;
+    isNull: boolean;
+    invariantCultureDisplayValue: string;
+}
+
+export interface IEditRowOperationParams extends IEditSessionOperationParams {
+    rowId: number;
+}
+
+export interface EditCell extends DbCellValue {
+    isDirty: boolean;
+}
+export interface EditCellResult {
+    cell: EditCell;
+    isRowDirty: boolean;
+}
+
+export interface EditReferencedTableInfo {
+    schemaName: string;
+    tableName: string;
+    fullyQualifiedName: string;
+    foreignKeyName: string;
+    sourceColumns: string[];
+    ReferencedColumns: string[];
+}
+
+//#region edit/initialize
+
+export interface EditInitializeParams extends IEditSessionOperationParams {
+    filters: EditInitializeFiltering;
+    objectName: string;
+    schemaName: string;
+    objectType: string;
+    queryString: string;
+}
+
+export interface EditInitializeResult {}
+
+//#endregion
+
+//#region edit/sessionReady Event
+
+export interface EditSessionReadyParams {
+    ownerUri: string;
+    success: boolean;
+    message: string;
+}
+
+//#endregion
+
+//#region edit/subset
+
+export interface EditSubsetParams extends IEditSessionOperationParams {
+    rowStartIndex: number;
+    rowCount: number;
+}
+
+export interface EditSubsetResult {
+    rowCount: number;
+    subset: EditRow[];
+}
+
+//#endregion
+
+//#region edit/commit
+
+export interface EditCommitParams extends IEditSessionOperationParams {}
+
+export interface EditCommitResult {}
+
+//#endregion
+
+//#region edit/createRow
+
+export interface EditCreateRowParams extends IEditSessionOperationParams {}
+
+export interface EditCreateRowResult {
+    defaultValues: string[];
+    newRowId: number;
+}
+
+//#endregion
+
+//#region edit/deleteRow
+
+export interface EditDeleteRowParams extends IEditRowOperationParams {}
+
+export interface EditDeleteRowResult {}
+
+//#endregion
+
+//#region edit/revertRow
+
+export interface EditRevertRowParams extends IEditRowOperationParams {}
+
+export interface EditRevertRowResult {}
+
+//#endregion
+
+//#region edit/updateCell
+
+export interface EditUpdateCellParams extends IEditRowOperationParams {
+    columnId: number;
+    newValue: string;
+}
+
+export interface EditUpdateCellResult extends EditCellResult {}
+
+//#endregion
+
+//#region edit/revertCell
+
+export interface EditRevertCellParams extends IEditRowOperationParams {
+    columnId: number;
+}
+
+export interface EditRevertCellResult extends EditCellResult {}
+
+//#endregion
+
+//#region edit/dispose
+
+export interface EditDisposeParams extends IEditSessionOperationParams {}
+
+export interface EditDisposeResult {}
+
+//#endregion
+
+//#region edit/script
+
+export interface EditScriptParams extends IEditSessionOperationParams {}
+
+export interface EditScriptResult {
+    script: string;
+}
+
+//#endregion
