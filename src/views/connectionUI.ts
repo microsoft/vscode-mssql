1e5ae3cc78eec45e331412a2fd925bfd6f32b190
Aasim Khan <aasimkhan@gmail.com>
2025-09-25 21:24:48 -0700

    remove legacy firewall rule creation.

--- src/views/connectionUI.ts
+++ src/views/connectionUI.ts
@@ -4,9 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from "vscode";
-import { IAccount, IConnectionInfo, ITenant } from "vscode-mssql";
+import { IAccount, IConnectionInfo } from "vscode-mssql";
 import { AccountStore } from "../azure/accountStore";
-import providerSettings from "../azure/providerSettings";
 import * as constants from "../constants/constants";
 import * as LocalizedConstants from "../constants/locConstants";
 import ConnectionManager from "../controllers/connectionManager";
@@ -14,7 +13,6 @@ import VscodeWrapper from "../controllers/vscodeWrapper";
 import { ConnectionCredentials } from "../models/connectionCredentials";
 import { ConnectionProfile } from "../models/connectionProfile";
 import { ConnectionStore } from "../models/connectionStore";
-import { ICreateFirewallRuleParams } from "../models/contracts/firewall/firewallRequest";
 import {
     CredentialsQuickPickItemType,
     IConnectionCredentialsQuickPickItem,
@@ -25,9 +23,6 @@ import { Timer } from "../models/utils";
 import { ObjectExplorerUtils } from "../objectExplorer/objectExplorerUtils";
 import { INameValueChoice, IPrompter, IQuestion, QuestionTypes } from "../prompts/question";
 import { CancelError } from "../utils/utils";
-import { ConnectionCompleteParams } from "../models/contracts/connection";
-import { AddFirewallRuleWebviewController } from "../controllers/addFirewallRuleWebviewController";
-import { SessionCreatedParameters } from "../models/contracts/objectExplorer/createSessionRequest";
 import { CREATE_NEW_GROUP_ID, IConnectionGroup } from "../sharedInterfaces/connectionGroup";
 import { FormItemOptions } from "../sharedInterfaces/form";
 
@@ -660,166 +655,6 @@ export class ConnectionUI {
         } else {
             // user cancelled the prompt - throw error so that we know user cancelled
             throw new CancelError();
-        }
-    }
-
-    private async promptForFirewallRuleCreation(
-        startIpAddress: string,
-        server: string,
-    ): Promise<ICreateFirewallRuleParams | undefined> {
-        function padTo2Digits(num: number): string {
-            return num.toString().padStart(2, "0");
-        }
-
-        // format as "YYYY-MM-DD_hh-mm-ss" (default Azure rulename format)
-        function formatDate(date: Date): string {
-            return (
-                [
-                    date.getFullYear(),
-                    padTo2Digits(date.getMonth() + 1),
-                    padTo2Digits(date.getDate()),
-                ].join("-") +
-                "_" +
-                [
-                    padTo2Digits(date.getHours()),
-                    padTo2Digits(date.getMinutes()),
-                    padTo2Digits(date.getSeconds()),
-                ].join("-")
-            );
-        }
-
-        let azureAccountChoices: INameValueChoice[] = await ConnectionProfile.getAccountChoices(
-            this._accountStore,
-        );
-        let tenantChoices: INameValueChoice[] = [];
-        let defaultFirewallRuleName = `ClientIPAddress_${formatDate(new Date())}`;
-
-        let accountAnswer: IAccount;
-        let tenantIdAnswer: string;
-        let firewallRuleNameAnswer: string;
-
-        let questions: IQuestion[] = [
-            {
-                type: QuestionTypes.input,
-                name: LocalizedConstants.startIpAddressPrompt,
-                message: LocalizedConstants.startIpAddressPrompt,
-                placeHolder: startIpAddress,
-                default: startIpAddress,
-                validate: (value: string) => {
-                    if (!Number.parseFloat(value) || !value.match(constants.ipAddressRegex)) {
-                        return LocalizedConstants.msgInvalidIpAddress;
-                    }
-                },
-            },
-            {
-                type: QuestionTypes.input,
-                name: LocalizedConstants.endIpAddressPrompt,
-                message: LocalizedConstants.endIpAddressPrompt,
-                placeHolder: startIpAddress,
-                validate: (value: string) => {
-                    if (
-                        !Number.parseFloat(value) ||
-                        !value.match(constants.ipAddressRegex) ||
-                        Number.parseFloat(value) > Number.parseFloat(startIpAddress)
-                    ) {
-                        return LocalizedConstants.msgInvalidIpAddress;
-                    }
-                },
-                default: startIpAddress,
-            },
-            {
-                type: QuestionTypes.input,
-                name: LocalizedConstants.firewallRuleNamePrompt,
-                message: LocalizedConstants.firewallRuleNamePrompt,
-                placeHolder: defaultFirewallRuleName,
-                validate: (value: string) => {
-                    if (!value.match(constants.ruleNameRegex)) {
-                        return LocalizedConstants.msgInvalidRuleName;
-                    }
-                    firewallRuleNameAnswer = value;
-                },
-                default: defaultFirewallRuleName,
-            },
-            {
-                type: QuestionTypes.expand,
-                name: LocalizedConstants.aad,
-                message: LocalizedConstants.azureChooseAccount,
-                choices: azureAccountChoices,
-                onAnswered: async (value: IAccount) => {
-                    accountAnswer = value;
-                    let account = value;
-                    tenantChoices.push(
-                        ...account?.properties?.tenants!.map((t) => ({
-                            name: t.displayName,
-                            value: t,
-                        })),
-                    );
-                    if (tenantChoices.length === 1) {
-                        tenantIdAnswer = tenantChoices[0].value.id;
-                    }
-                },
-            },
-            {
-                type: QuestionTypes.expand,
-                name: LocalizedConstants.tenant,
-                message: LocalizedConstants.azureChooseTenant,
-                choices: tenantChoices,
-                shouldPrompt: () => tenantChoices.length > 1,
-                onAnswered: (value: ITenant) => {
-                    tenantIdAnswer = value.id;
-                },
-            },
-        ];
-
-        // Prompt and return the value if the user confirmed
-        let answers = await this._prompter.prompt(questions);
-        if (answers) {
-            let result: ICreateFirewallRuleParams = {
-                account: accountAnswer,
-                startIpAddress: answers[LocalizedConstants.startIpAddressPrompt]
-                    ? (answers[LocalizedConstants.startIpAddressPrompt] as string)
-                    : startIpAddress,
-                endIpAddress: answers[LocalizedConstants.endIpAddressPrompt]
-                    ? (answers[LocalizedConstants.endIpAddressPrompt] as string)
-                    : startIpAddress,
-                firewallRuleName: firewallRuleNameAnswer,
-                serverName: server,
-                securityTokenMappings:
-                    await this.connectionManager.accountService.createSecurityTokenMapping(
-                        accountAnswer,
-                        tenantIdAnswer,
-                    ),
-            };
-            return result;
-        } else {
-            return undefined;
-        }
-    }
-
-    private async createFirewallRule(serverName: string, ipAddress: string): Promise<boolean> {
-        let result = await this._vscodeWrapper.showInformationMessage(
-            LocalizedConstants.msgPromptRetryFirewallRuleSignedIn(ipAddress, serverName),
-            LocalizedConstants.createFirewallRuleLabel,
-        );
-        if (result === LocalizedConstants.createFirewallRuleLabel) {
-            const firewallService = this.connectionManager.firewallService;
-            let params = await this.promptForFirewallRuleCreation(ipAddress, serverName);
-            if (params) {
-                let firewallResult = await firewallService.createFirewallRule(params);
-                if (firewallResult.result) {
-                    this._vscodeWrapper.showInformationMessage(
-                        LocalizedConstants.msgPromptFirewallRuleCreated,
-                    );
-                    return true;
-                } else {
-                    Utils.showErrorMsg(firewallResult.errorMessage);
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return false;
         }
     }
 
