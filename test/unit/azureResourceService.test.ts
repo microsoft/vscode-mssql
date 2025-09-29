7697f8b7477480f54fbc83056460cc149f4a561c
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-09-29 14:01:43 -0700

    converted azureResourceService.test.ts

--- test/unit/azureResourceService.test.ts
+++ test/unit/azureResourceService.test.ts
@@ -3,9 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as assert from "assert";
 import * as sinon from "sinon";
-import { AzureAuthType, IAccount } from "../../src/models/contracts/azure";
+import sinonChai from "sinon-chai";
+import { expect } from "chai";
+import * as chai from "chai";
 import {
     SubscriptionClient,
     Subscription,
@@ -15,168 +16,127 @@ import {
 import { PagedAsyncIterableIterator } from "@azure/core-paging";
 import { ResourceGroup, ResourceGroups, ResourceManagementClient } from "@azure/arm-resources";
 import { AzureResourceController } from "../../src/azure/azureResourceController";
-import { AzureAccountService } from "../../src/services/azureAccountService";
 import allSettings from "../../src/azure/providerSettings";
+import { AzureAuthType, IAccount } from "../../src/models/contracts/azure";
 import { IAzureAccountSession } from "vscode-mssql";
 
-export interface ITestContext {
-    azureAccountService: sinon.SinonStubbedInstance<AzureAccountService>;
-    accounts: IAccount[];
-    session: IAzureAccountSession;
-    subscriptionClient: sinon.SinonStubbedInstance<SubscriptionClient>;
-    subscriptions: Subscription[];
-    locations: Location[];
-    groups: ResourceGroup[];
-}
+chai.use(sinonChai);
 
-export function createContext(): ITestContext {
-    const accounts = [
-        {
-            key: undefined!,
-            displayInfo: undefined!,
-            properties: {
-                tenants: [
-                    {
-                        id: "",
-                        displayName: "",
-                    },
-                ],
-                azureAuthType: AzureAuthType.AuthCodeGrant,
-                isMsAccount: false,
-                owningTenant: {
+const mockAccounts: IAccount[] = [
+    {
+        key: undefined!,
+        displayInfo: undefined!,
+        properties: {
+            tenants: [
+                {
                     id: "",
                     displayName: "",
                 },
-                providerSettings: allSettings,
+            ],
+            azureAuthType: AzureAuthType.AuthCodeGrant,
+            isMsAccount: false,
+            owningTenant: {
+                id: "",
+                displayName: "",
             },
-            isStale: false,
-            isSignedIn: true,
+            providerSettings: allSettings,
         },
-    ];
-    const subscriptions: Subscription[] = [{ subscriptionId: "id1" }, { subscriptionId: "id2" }];
-    const locations: Location[] = [{ id: "id1" }, { id: "id2" }];
-    const groups: ResourceGroup[] = [
-        { id: "id1", location: "l1" },
-        { id: "id2", location: "l2" },
-    ];
-    const session0: IAzureAccountSession = {
-        account: accounts[0],
-        subscription: subscriptions[0],
-        tenantId: "tenantId",
-        token: {
-            key: "",
-            token: "",
-            tokenType: "",
-        },
-    };
-    const session1: IAzureAccountSession = {
-        account: accounts[0],
-        subscription: subscriptions[1],
-        tenantId: "tenantId",
-        token: {
-            key: "",
-            token: "",
-            tokenType: "",
-        },
-    };
+        isStale: false,
+        isSignedIn: true,
+    },
+];
 
-    const azureAccountService = sinon.createStubInstance(AzureAccountService);
-    azureAccountService.getAccounts.resolves(accounts);
-    azureAccountService.addAccount.resolves(accounts[0]);
-    azureAccountService.getAccountSecurityToken.resolves({
+const mockSubscriptions: Subscription[] = [{ subscriptionId: "id1" }, { subscriptionId: "id2" }];
+const mockLocations: Location[] = [{ id: "id1" }, { id: "id2" }];
+const mockGroups: ResourceGroup[] = [
+    { id: "id1", location: "l1" },
+    { id: "id2", location: "l2" },
+];
+
+const primarySession: IAzureAccountSession = {
+    account: mockAccounts[0],
+    subscription: mockSubscriptions[0],
+    tenantId: "tenantId",
+    token: {
         key: "",
         token: "",
         tokenType: "",
-    });
-    azureAccountService.getAccountSessions.resolves([session0, session1]);
+    },
+};
 
-    const subscriptionClient = sinon.createStubInstance(SubscriptionClient);
+function createPagedIterator<T>(items: T[]): PagedAsyncIterableIterator<T> {
+    let index = 0;
+    const maxLength = items.length;
 
     return {
-        groups: groups,
-        locations: locations,
-        subscriptions: subscriptions,
-        subscriptionClient,
-        session: session0,
-        accounts: accounts,
-        azureAccountService,
+        next: async () => {
+            if (index < maxLength) {
+                return {
+                    done: false,
+                    value: items[index++],
+                };
+            }
+
+            return { done: true, value: undefined };
+        },
+        byPage: () => undefined!,
+        [Symbol.asyncIterator]: undefined!,
     };
 }
 
 suite("Azure SQL client", function (): void {
+    let sandbox: sinon.SinonSandbox;
+
+    setup(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
     test("Should return locations successfully", async function (): Promise<void> {
-        const testContext = createContext();
-        const azureSqlClient = new AzureResourceController(() => testContext.subscriptionClient);
-
-        let index = 0;
-        let maxLength = testContext.locations.length;
-        const pages: PagedAsyncIterableIterator<Location> = {
-            next: () => {
-                if (index < maxLength) {
-                    return Promise.resolve({
-                        done: false,
-                        value: testContext.locations[index++],
-                    });
-                } else {
-                    return Promise.resolve({ done: true, value: undefined });
-                }
-            },
-            byPage: () => undefined!,
-            [Symbol.asyncIterator]: undefined!,
-        };
+        const pages = createPagedIterator(mockLocations);
         const subscriptions: Subscriptions = {
-            listLocations: () => pages,
-            list: () => undefined!,
-            get: () => undefined!,
+            listLocations: sandbox.stub().returns(pages),
+            list: sandbox.stub().returns(undefined),
+            get: sandbox.stub().returns(undefined),
         };
+        const subscriptionClient = {
+            subscriptions,
+        } as unknown as SubscriptionClient;
+        const azureSqlClient = new AzureResourceController(() => subscriptionClient);
 
-        Object.defineProperty(testContext.subscriptionClient, "subscriptions", {
-            get: () => subscriptions,
-        });
+        const result = await azureSqlClient.getLocations(primarySession);
 
-        const result = await azureSqlClient.getLocations(testContext.session);
-        assert.deepStrictEqual(result.length, testContext.locations.length);
+        expect(subscriptions.listLocations).to.have.been.calledOnceWithExactly(
+            primarySession.subscription?.subscriptionId,
+        );
+        expect(result).to.have.lengthOf(mockLocations.length);
     });
 
     test("Should return resource groups successfully", async function (): Promise<void> {
-        const testContext = createContext();
-        const groupClient = sinon.createStubInstance(ResourceManagementClient);
-        const azureSqlClient = new AzureResourceController(undefined, () => groupClient);
-
-        let index = 0;
-        let maxLength = testContext.groups.length;
-        const pages: PagedAsyncIterableIterator<ResourceGroup> = {
-            next: () => {
-                if (index < maxLength) {
-                    return Promise.resolve({
-                        done: false,
-                        value: testContext.groups[index++],
-                    });
-                } else {
-                    return Promise.resolve({ done: true, value: undefined });
-                }
-            },
-            byPage: () => undefined!,
-            [Symbol.asyncIterator]: undefined!,
-        };
+        const pages = createPagedIterator(mockGroups);
         const resourceGroups: ResourceGroups = {
-            list: () => pages,
-            get: () => undefined!,
-            beginDelete: undefined!,
-            beginDeleteAndWait: undefined!,
-            beginExportTemplate: undefined!,
-            beginExportTemplateAndWait: undefined!,
-            checkExistence: undefined!,
-            createOrUpdate: undefined!,
-            update: undefined!,
+            list: sandbox.stub().returns(pages),
+            get: sandbox.stub().returns(undefined),
+            beginDelete: sandbox.stub(),
+            beginDeleteAndWait: sandbox.stub(),
+            beginExportTemplate: sandbox.stub(),
+            beginExportTemplateAndWait: sandbox.stub(),
+            checkExistence: sandbox.stub(),
+            createOrUpdate: sandbox.stub(),
+            update: sandbox.stub(),
         };
+        const resourceClient = {
+            resourceGroups,
+        } as unknown as ResourceManagementClient;
+        const azureSqlClient = new AzureResourceController(undefined, () => resourceClient);
 
-        Object.defineProperty(groupClient, "resourceGroups", {
-            get: () => resourceGroups,
-        });
+        const result = await azureSqlClient.getResourceGroups(primarySession);
 
-        const result = await azureSqlClient.getResourceGroups(testContext.session);
-        assert.deepStrictEqual(result.length, testContext.groups.length);
-        assert.deepStrictEqual(result[0].location, testContext.groups[0].location);
+        expect(resourceGroups.list).to.have.been.calledOnceWithExactly();
+        expect(result).to.have.lengthOf(mockGroups.length);
+        expect(result[0].location).to.equal(mockGroups[0].location);
     });
 });
