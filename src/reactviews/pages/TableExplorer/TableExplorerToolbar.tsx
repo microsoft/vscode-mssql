4440292fade4b6f58ff06e40842bd38a47da79ff
Lewis Sanchez <lewissanchez@microsoft.com>
2025-10-14 17:00:20 -0700

    Replace Dropdown with Combobox for row count input

--- src/reactviews/pages/TableExplorer/TableExplorerToolbar.tsx
+++ src/reactviews/pages/TableExplorer/TableExplorerToolbar.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Toolbar, ToolbarButton, Dropdown, Option } from "@fluentui/react-components";
+import { Toolbar, ToolbarButton, Combobox, Option } from "@fluentui/react-components";
 import { SaveRegular, AddRegular } from "@fluentui/react-icons";
 import { locConstants as loc } from "../../common/locConstants";
 import { useTableExplorerContext } from "./TableExplorerStateProvider";
@@ -29,13 +29,38 @@ export const TableExplorerToolbar: React.FC<TableExplorerToolbarProps> = ({ onSa
         context.createRow();
     };
 
-    const handleDropdownChange = (_event: any, data: any) => {
-        const newValue = data.optionValue;
+    const handleComboboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // Handle when user types or selects an option
+        const newValue = event.target.value;
+        setSelectedValue(newValue);
+    };
+
+    const handleComboboxSelect = (_event: any, data: any) => {
+        // Handle when user selects from dropdown
+        const newValue = data.optionValue || data.value;
         setSelectedValue(newValue);
         const newRowCount = parseInt(newValue, 10);
-        // Call the loadSubset reducer with the new row count
-        // This loads data from the database, but doesn't change pagination size
-        context.loadSubset(newRowCount);
+        if (!isNaN(newRowCount) && newRowCount > 0) {
+            context.loadSubset(newRowCount);
+        }
+    };
+
+    const handleComboboxBlur = () => {
+        // Handle when user finishes typing (loses focus)
+        const newRowCount = parseInt(selectedValue, 10);
+        if (!isNaN(newRowCount) && newRowCount > 0) {
+            context.loadSubset(newRowCount);
+        }
+    };
+
+    const handleComboboxKeyDown = (event: React.KeyboardEvent) => {
+        // Handle when user presses Enter
+        if (event.key === "Enter") {
+            const newRowCount = parseInt(selectedValue, 10);
+            if (!isNaN(newRowCount) && newRowCount > 0) {
+                context.loadSubset(newRowCount);
+            }
+        }
     };
 
     return (
@@ -54,15 +79,19 @@ export const TableExplorerToolbar: React.FC<TableExplorerToolbarProps> = ({ onSa
                 onClick={handleAddRow}>
                 {loc.tableExplorer.addRow}
             </ToolbarButton>
-            <Dropdown
+            <Combobox
                 value={selectedValue}
-                selectedOptions={[selectedValue]}
-                onOptionSelect={handleDropdownChange}
+                freeform
+                onChange={handleComboboxChange}
+                onOptionSelect={handleComboboxSelect}
+                onBlur={handleComboboxBlur}
+                onKeyDown={handleComboboxKeyDown}
+                placeholder="Enter row count"
                 style={{ minWidth: "100px" }}>
                 <Option value="10">10</Option>
                 <Option value="100">100</Option>
                 <Option value="1000">1000</Option>
-            </Dropdown>
+            </Combobox>
         </Toolbar>
     );
 };
