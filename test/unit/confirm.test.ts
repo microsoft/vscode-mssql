daa35df009d43874c93e0d5238f737ee97828fb7
Benjin Dubishar <benjin.dubishar@gmail.com>
2025-10-15 13:38:05 -0700

    Converting TypeMoq tests to sinon (#20274)
    * converted reactWebviewPanelController tests
    
    * converted userSurvey tests
    
    * test: replace TypeMoq with Sinon in azureResourceService tests
    
    * converted azureResourceService.test.ts
    
    * converted queryNotificationHandler.test.ts
    
    * converted confirm.test.ts
    
    * Converted prompts.test.ts
    
    * converting mssqlProtocolHandler.test.ts
    
    * converted server.test.ts
    
    * converted scriptingService.test.ts
    
    * converted extConfig.test.ts
    
    * converted executionPlanWebviewController.test.ts
    
    * converted metadataService.test.ts
    
    * converted list.test.ts
    
    * converted adapter.test.ts
    
    * converted firewallService.test.ts
    
    * converted databaseObjectSearchService.test.ts
    
    * cleaned up some tests
    
    * cleaned up list.test.ts
    
    * fixed up adapter.test.ts
    
    * cleaned up firewallService.test.ts
    
    * added test/unit AGENTS.md
    
    * Adding AGENTS.md for unit test folder
    
    * converted tableDesigner.test.ts
    
    * swapping to correct assertions
    
    * reverting AGENTS and README
    
    * fixing import for test
    
    * PR feedback
    
    * cleanup
    
    * reverting root AGENTS.md

--- test/unit/confirm.test.ts
+++ test/unit/confirm.test.ts
@@ -3,42 +3,51 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as TypeMoq from "typemoq";
+import * as sinon from "sinon";
+import sinonChai from "sinon-chai";
+import { expect } from "chai";
+import * as chai from "chai";
 import VscodeWrapper from "../../src/controllers/vscodeWrapper";
 import * as LocalizedConstants from "../../src/constants/locConstants";
 import ConfirmPrompt from "../../src/prompts/confirm";
 
-// @cssuh 10/22 - commented this test because it was throwing some random undefined errors
-suite.skip("Test Confirm Prompt", () => {
-    test("Test Confirm prompt with simple question", () => {
-        let question = {
-            name: "test",
-        };
-        let vscodeWrapper = TypeMoq.Mock.ofType(VscodeWrapper, TypeMoq.MockBehavior.Loose);
-        vscodeWrapper
-            .setup((v) => v.showQuickPickStrings(TypeMoq.It.isAny(), TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve(LocalizedConstants.msgYes));
-        let confirm = new ConfirmPrompt(question, vscodeWrapper.object);
-        confirm.render();
-        vscodeWrapper.verify(
-            (v) => v.showQuickPickStrings(TypeMoq.It.isAny(), TypeMoq.It.isAny()),
-            TypeMoq.Times.once(),
-        );
+chai.use(sinonChai);
+
+suite("Test Confirm Prompt", () => {
+    let sandbox: sinon.SinonSandbox;
+
+    setup(() => {
+        sandbox = sinon.createSandbox();
     });
 
-    test("Test Checkbox prompt with error", () => {
-        let question = {
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    test("Test Confirm prompt with simple question", async () => {
+        const question = {
             name: "test",
         };
-        let vscodeWrapper = TypeMoq.Mock.ofType(VscodeWrapper, TypeMoq.MockBehavior.Loose);
-        vscodeWrapper
-            .setup((v) => v.showQuickPickStrings(TypeMoq.It.isAny(), TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve(undefined));
-        let confirm = new ConfirmPrompt(question, vscodeWrapper.object);
-        confirm.render();
-        vscodeWrapper.verify(
-            (v) => v.showQuickPickStrings(TypeMoq.It.isAny(), TypeMoq.It.isAny()),
-            TypeMoq.Times.once(),
-        );
+        const vscodeWrapper = sandbox.createStubInstance(VscodeWrapper);
+        vscodeWrapper.showQuickPickStrings.resolves(LocalizedConstants.msgYes);
+
+        const confirm = new ConfirmPrompt(question, vscodeWrapper);
+        await confirm.render();
+
+        expect(vscodeWrapper.showQuickPickStrings).to.have.been.calledOnce;
+    });
+
+    test("Test Checkbox prompt with error", async () => {
+        const question = {
+            name: "test",
+        };
+        const vscodeWrapper = sandbox.createStubInstance(VscodeWrapper);
+        vscodeWrapper.showQuickPickStrings.resolves(undefined);
+
+        const confirm = new ConfirmPrompt(question, vscodeWrapper);
+
+        await confirm.render().catch(() => undefined);
+
+        expect(vscodeWrapper.showQuickPickStrings).to.have.been.calledOnce;
     });
 });
