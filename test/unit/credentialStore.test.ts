f3663d07302c052a407339c0b33dc68899864946
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-10-17 16:29:42 -0700

    converted test/unit/credentialStore.test.ts

--- test/unit/credentialStore.test.ts
+++ test/unit/credentialStore.test.ts
@@ -3,62 +3,119 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as sinon from "sinon";
+import sinonChai from "sinon-chai";
+import { expect } from "chai";
+import * as chai from "chai";
 import * as vscode from "vscode";
-import * as TypeMoq from "typemoq";
 import * as Contracts from "../../src/models/contracts";
 import SqlToolsServiceClient from "../../src/languageservice/serviceclient";
 import { CredentialStore } from "../../src/credentialstore/credentialstore";
 import { ICredentialStore } from "../../src/credentialstore/icredentialstore";
 import VscodeWrapper from "../../src/controllers/vscodeWrapper";
 
+chai.use(sinonChai);
+
 suite("Credential Store Tests", () => {
-    let client: TypeMoq.IMock<SqlToolsServiceClient>;
+    let sandbox: sinon.SinonSandbox;
+    let client: sinon.SinonStubbedInstance<SqlToolsServiceClient>;
     let credentialStore: ICredentialStore;
-    let mockContext: TypeMoq.IMock<vscode.ExtensionContext>;
+    let secretStorage: {
+        get: sinon.SinonStub<[string], Promise<string | undefined>>;
+        store: sinon.SinonStub<[string, string], Promise<void>>;
+        delete: sinon.SinonStub<[string], Promise<void>>;
+    };
+    let context: vscode.ExtensionContext;
+    let vscodeWrapper: sinon.SinonStubbedInstance<VscodeWrapper>;
+    let saveRequestStub: sinon.SinonStub;
+    let readRequestStub: sinon.SinonStub;
+    let deleteRequestStub: sinon.SinonStub;
+
+    const credentialId = "test_credential";
 
     setup(() => {
-        client = TypeMoq.Mock.ofType(SqlToolsServiceClient, TypeMoq.MockBehavior.Loose);
-        mockContext = TypeMoq.Mock.ofType<vscode.ExtensionContext>();
-        client
-            .setup((c) => c.sendRequest(Contracts.SaveCredentialRequest.type, TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve(true));
-        client
-            .setup((c) => c.sendRequest(Contracts.ReadCredentialRequest.type, TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve(undefined));
-        client
-            .setup((c) => c.sendRequest(Contracts.DeleteCredentialRequest.type, TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve(undefined));
+        sandbox = sinon.createSandbox();
+
+        client = sandbox.createStubInstance(SqlToolsServiceClient);
+
+        secretStorage = {
+            get: sandbox.stub<[string], Promise<string | undefined>>(),
+            store: sandbox.stub<[string, string], Promise<void>>(),
+            delete: sandbox.stub<[string], Promise<void>>(),
+        };
+
+        secretStorage.get.resolves(undefined);
+        secretStorage.store.resolves();
+        secretStorage.delete.resolves();
+
+        context = {
+            secrets: secretStorage as unknown as vscode.SecretStorage,
+        } as vscode.ExtensionContext;
+
+        saveRequestStub = client.sendRequest
+            .withArgs(Contracts.SaveCredentialRequest.type, sinon.match.any)
+            .resolves(true);
+        readRequestStub = client.sendRequest
+            .withArgs(Contracts.ReadCredentialRequest.type, sinon.match.any)
+            .resolves(undefined);
+        deleteRequestStub = client.sendRequest
+            .withArgs(Contracts.DeleteCredentialRequest.type, sinon.match.any)
+            .resolves(undefined);
+
+        vscodeWrapper = sandbox.createStubInstance(VscodeWrapper);
+
         credentialStore = new CredentialStore(
-            mockContext.object,
-            TypeMoq.Mock.ofType<VscodeWrapper>().object,
-            client.object,
+            context,
+            vscodeWrapper as unknown as VscodeWrapper,
+            client as unknown as SqlToolsServiceClient,
         );
     });
 
-    test("Read credential should send a ReadCredentialRequest", () => {
-        void credentialStore.readCredential("test_credential").then(() => {
-            client.verify(
-                (c) => c.sendRequest(Contracts.ReadCredentialRequest.type, TypeMoq.It.isAny()),
-                TypeMoq.Times.once(),
-            );
-        });
+    teardown(() => {
+        sandbox.restore();
     });
 
-    test("Save credential should send a SaveCredentialRequest", () => {
-        void credentialStore.saveCredential("test_credential", "test_password").then(() => {
-            client.verify(
-                (c) => c.sendRequest(Contracts.SaveCredentialRequest.type, TypeMoq.It.isAny()),
-                TypeMoq.Times.once(),
-            );
-        });
+    test("Read credential should send a ReadCredentialRequest when secret storage misses", async () => {
+        const serverCredential: Contracts.Credential = new Contracts.Credential();
+        serverCredential.credentialId = credentialId;
+        serverCredential.password = "test_password";
+
+        readRequestStub.resolves(serverCredential);
+
+        await credentialStore.readCredential(credentialId);
+
+        expect(secretStorage.get).to.have.been.calledOnceWithExactly(credentialId);
+        expect(readRequestStub).to.have.been.calledOnceWithExactly(
+            Contracts.ReadCredentialRequest.type,
+            sinon.match.has("credentialId", credentialId),
+        );
+        expect(secretStorage.store).to.have.been.calledOnceWithExactly(
+            credentialId,
+            serverCredential.password,
+        );
+        expect(deleteRequestStub).to.have.been.calledOnceWithExactly(
+            Contracts.DeleteCredentialRequest.type,
+            sinon.match.has("credentialId", credentialId),
+        );
     });
 
-    test("Delete credential should send a DeleteCredentialRequest", () => {
-        void credentialStore.deleteCredential("test_credential").then(() => {
-            client.verify(
-                (c) => c.sendRequest(Contracts.DeleteCredentialRequest.type, TypeMoq.It.isAny()),
-                TypeMoq.Times.once(),
-            );
-        });
+    test("Save credential should store in secret storage", async () => {
+        await credentialStore.saveCredential(credentialId, "test_password");
+
+        expect(secretStorage.store).to.have.been.calledOnceWithExactly(
+            credentialId,
+            "test_password",
+        );
+        expect(saveRequestStub).to.not.have.been.called;
+    });
+
+    test("Delete credential should remove from secret storage and request delete", async () => {
+        await credentialStore.deleteCredential(credentialId);
+
+        expect(secretStorage.delete).to.have.been.calledOnceWithExactly(credentialId);
+        expect(deleteRequestStub).to.have.been.calledWithExactly(
+            Contracts.DeleteCredentialRequest.type,
+            sinon.match.has("credentialId", credentialId),
+        );
     });
 });
