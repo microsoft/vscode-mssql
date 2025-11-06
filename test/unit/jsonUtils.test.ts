558740e6836c4b257bcd1797b4715f454d1b9a56
allancascante <allan.cascante@gmail.com>
2025-11-06 17:28:41 -0600

    reverting lint exclude rule

--- test/unit/jsonUtils.test.ts
+++ test/unit/jsonUtils.test.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// eslint-disable-next-line custom-eslint-rules/banned-imports
 import { isJson } from "../../src/reactviews/common/jsonUtils";
 import { expect } from "@playwright/test";
 
