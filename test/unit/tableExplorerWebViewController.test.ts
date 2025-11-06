189579d8532d966d5720d6a695cca137fc4de689
Lewis Sanchez <lewissanchez@microsoft.com>
2025-11-05 22:55:48 -0800

    Remove unused import

--- test/unit/tableExplorerWebViewController.test.ts
+++ test/unit/tableExplorerWebViewController.test.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as assert from "assert";
 import { expect } from "chai";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
