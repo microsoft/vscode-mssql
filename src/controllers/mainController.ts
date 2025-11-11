1303b6de93e04ec41f6f95a633a368a7fb20a9c5
Sai Avishkar Sreerama <74571829+ssreerama@users.noreply.github.com>
2025-11-10 20:18:17 -0600

    Publish Dialog - Deploy project to local container backend functionality (#20508)
    * inital code implementation
    
    * publish successful, refactoring and message displaying needed
    
    * testing with vscode messages
    
    * messages fixed
    
    * adding tests
    
    * form message adjustment and sqlcmd table UI fixes
    
    * error msg fix
    
    * adding vscode process message notificaiotns while operation in progress
    
    * sqlcmd table ui fix
    
    * undo launch updates, only for local
    
    * adding negative test to improve code coverage for failure scenarios
    
    * removing unused declaration
    
    * removing unused leftover state
    
    * addressing comments
    
    * loc
    
    * moving to loc

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -2586,6 +2586,7 @@ export default class MainController implements vscode.Disposable {
             this._vscodeWrapper,
             this.connectionManager,
             projectFilePath,
+            this,
             this.sqlProjectsService,
             this.dacFxService,
             deploymentOptions.defaultDeploymentOptions,
