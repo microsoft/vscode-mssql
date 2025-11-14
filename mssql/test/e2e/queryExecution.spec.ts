51945b37caa43eb951c8a5618a893a44622c3329
Benjin Dubishar <benjin.dubishar@gmail.com>
2025-11-13 20:03:22 -0800

    e2e test updates

--- mssql/test/e2e/queryExecution.spec.ts
+++ mssql/test/e2e/queryExecution.spec.ts
@@ -35,10 +35,7 @@ test.describe("MSSQL Extension - Query Execution", async () => {
     let profileName: string;
 
     test.beforeAll(async () => {
-        // Launch with new UI off
-        const { electronApp, page } = await launchVsCodeWithMssqlExtension({
-            useNewUI: true,
-        });
+        const { electronApp, page } = await launchVsCodeWithMssqlExtension();
         vsCodeApp = electronApp;
         vsCodePage = page;
 
