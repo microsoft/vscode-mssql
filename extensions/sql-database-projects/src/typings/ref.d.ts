9d34d85269e14021d7644e3898875b3ac9dbbfd6
Aasim Khan <aasimkhan@gmail.com>
2025-11-18 21:46:48 -0800

    Fix launch and sql-proj compilations

--- extensions/sql-database-projects/src/typings/ref.d.ts
+++ extensions/sql-database-projects/src/typings/ref.d.ts
@@ -4,12 +4,12 @@
  *--------------------------------------------------------------------------------------------*/
 
 // /// <reference path='../../../../src/vscode-dts/vscode.d.ts'/>
-/// <reference path='../../../typings/azdata.d.ts'/>
-/// <reference path='../../../typings/azdata.proposed.d.ts'/>
-/// <reference path='../../../typings/dataworkspace.d.ts'/>
-/// <reference path='../../../typings/mssql.d.ts'/>
+/// <reference path='../../../../typings/azdata.d.ts'/>
+/// <reference path='../../../../typings/azdata.proposed.d.ts'/>
+/// <reference path='../../../../typings/dataworkspace.d.ts'/>
+/// <reference path='../../../../typings/mssql.d.ts'/>
 /// TODO: switch to this direct reference:
 // /// <reference path='../../../mssql/typings/vscode-mssql.d.ts'/>
 // TODO: remove this typings file (ported from ADS)
-/// <reference path='../../../typings/vscode-mssql.d.ts'/>
+/// <reference path='../../../../typings/vscode-mssql.d.ts'/>
 /// <reference types='@types/node'/>
