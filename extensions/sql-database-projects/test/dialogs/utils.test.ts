026f6ef13fbcca98e1a27189f4e1a49621c570cd
Benjin Dubishar <benjin.dubishar@gmail.com>
2025-11-20 16:10:06 -0800

    Enabling SqlProj tests in github pipeline (#20648)
    * Moving tests to top-level folder
    
    * updating package.json to point to new location
    
    * bringing in test updates from other branch
    
    * updating github pipeline
    
    * updating string

---
+++ extensions/sql-database-projects/test/dialogs/utils.test.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import should = require('should/as-function');
+import * as constants from '../../src/common/constants';
+import { SqlTargetPlatform } from 'sqldbproj';
+import { getDefaultDockerImageWithTag, getDockerBaseImage } from '../../src/dialogs/utils';
+
+suite('Tests to verify dialog utils functions', function (): void {
+	test('getDefaultDockerImageWithTag should return correct image', () => {
+		const sqlServerImageInfo = getDockerBaseImage(constants.targetPlatformToVersion.get(SqlTargetPlatform.sqlServer2022)!);
+
+		should(getDefaultDockerImageWithTag('160', 'mcr.microsoft.com/mssql/server', sqlServerImageInfo)).equals(`${sqlServerImageInfo?.name}:2022-latest`, 'Unexpected docker image returned for target platform SQL Server 2022 and SQL Server base image');
+		should(getDefaultDockerImageWithTag('150', 'mcr.microsoft.com/mssql/server', sqlServerImageInfo)).equals(`${sqlServerImageInfo?.name}:2019-latest`, 'Unexpected docker image returned for target platform SQL Server 2019 and SQL Server base image');
+		should(getDefaultDockerImageWithTag('140', 'mcr.microsoft.com/mssql/server', sqlServerImageInfo)).equals(`${sqlServerImageInfo?.name}:2017-latest`, 'Unexpected docker image returned for target platform SQL Server 2017 and SQL Server base image');
+		should(getDefaultDockerImageWithTag('130', 'mcr.microsoft.com/mssql/server', sqlServerImageInfo)).equals(`${sqlServerImageInfo?.name}`, 'Unexpected docker image returned for target platform SQL Server 2016 and SQL Server base image');
+
+		// different display names are returned when a project's target platform is Azure, but currently the Azure full image points to mcr.microsoft.com/mssql/server
+		const azureFullImageInfo = getDockerBaseImage(constants.targetPlatformToVersion.get(SqlTargetPlatform.sqlAzure)!);
+
+		should(getDefaultDockerImageWithTag('AzureV12', 'mcr.microsoft.com/mssql/server', azureFullImageInfo)).equals(`${azureFullImageInfo?.name}`, 'Unexpected docker image returned for target platform Azure and Azure full base image');
+	});
+});
+
+
