e998d5faaeda1596eddf2b62349fc17d0af57966
Aasim Khan <aasimkhan@gmail.com>
2025-12-01 15:33:05 -0800

    Updating sts to get generate script fix in schema designer

--- extensions/mssql/src/configurations/config.ts
+++ extensions/mssql/src/configurations/config.ts
@@ -7,7 +7,7 @@ export const config = {
     service: {
         downloadUrl:
             "https://github.com/Microsoft/sqltoolsservice/releases/download/{#version#}/microsoft.sqltools.servicelayer-{#fileName#}",
-        version: "5.0.20251124.1",
+        version: "5.0.20251201.1",
         downloadFileNames: {
             Windows_64: "win-x64-net8.0.zip",
             Windows_ARM64: "win-arm64-net8.0.zip",
