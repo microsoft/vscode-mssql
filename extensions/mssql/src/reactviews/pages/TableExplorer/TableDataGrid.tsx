d67c99c5ec88646784bb79825c6ed9941c2874e8
Lewis Sanchez <lewissanchez@microsoft.com>
2025-12-04 16:35:48 -0800

    Code review changes

--- extensions/mssql/src/reactviews/pages/TableExplorer/TableDataGrid.tsx
+++ extensions/mssql/src/reactviews/pages/TableExplorer/TableDataGrid.tsx
@@ -26,6 +26,7 @@ import { EditSubsetResult } from "../../../sharedInterfaces/tableExplorer";
 import { ColorThemeKind } from "../../../sharedInterfaces/webview";
 import { locConstants as loc } from "../../common/locConstants";
 import TableExplorerCustomPager from "./TableExplorerCustomPager";
+import { slickGridLocales } from "./commonGridOptions";
 import "@slickgrid-universal/common/dist/styles/css/slickgrid-theme-default.css";
 import "./TableDataGrid.css";
 
@@ -138,17 +139,8 @@ export const TableDataGrid = forwardRef<TableDataGridRef, TableDataGridProps>(
                 }
             },
             getSelectedRowIds: () => {
-                if (reactGridRef.current?.slickGrid) {
-                    const selectedRows = reactGridRef.current.slickGrid.getSelectedRows();
-                    const dataView = reactGridRef.current.dataView;
-                    if (dataView) {
-                        return selectedRows
-                            .map((rowIdx: number) => {
-                                const item = dataView.getItem(rowIdx);
-                                return item?.id;
-                            })
-                            .filter((id: number | undefined) => id !== undefined);
-                    }
+                if (reactGridRef.current?.dataView) {
+                    return reactGridRef.current.dataView.getAllSelectedIds() as number[];
                 }
                 return [];
             },
@@ -366,63 +358,7 @@ export const TableDataGrid = forwardRef<TableDataGridRef, TableDataGridProps>(
                         forceFitColumns: false, // Allow horizontal scrolling for many columns
 
                         // Localization for grid UI
-                        locales: {
-                            TEXT_CONTAINS: loc.tableExplorer.filterContains,
-                            TEXT_NOT_CONTAINS: loc.tableExplorer.filterNotContains,
-                            TEXT_EQUALS: loc.tableExplorer.filterEquals,
-                            TEXT_EQUAL_TO: loc.tableExplorer.equalTo,
-                            TEXT_NOT_EQUAL_TO: loc.tableExplorer.filterNotEqualTo,
-                            TEXT_STARTS_WITH: loc.tableExplorer.filterStartsWith,
-                            TEXT_ENDS_WITH: loc.tableExplorer.filterEndsWith,
-                            TEXT_GREATER_THAN: loc.tableExplorer.greaterThan,
-                            TEXT_GREATER_THAN_OR_EQUAL_TO: loc.tableExplorer.greaterThanOrEqualTo,
-                            TEXT_LESS_THAN: loc.tableExplorer.lessThan,
-                            TEXT_LESS_THAN_OR_EQUAL_TO: loc.tableExplorer.lessThanOrEqualTo,
-                            TEXT_ALL_SELECTED: loc.tableExplorer.allSelected,
-                            TEXT_CANCEL: loc.tableExplorer.cancel,
-                            TEXT_CLEAR_ALL_FILTERS: loc.tableExplorer.clearAllFilters,
-                            TEXT_CLEAR_ALL_GROUPING: loc.tableExplorer.clearAllGrouping,
-                            TEXT_CLEAR_ALL_SORTING: loc.tableExplorer.clearAllSorting,
-                            TEXT_CLEAR_PINNING: loc.tableExplorer.clearPinning,
-                            TEXT_COLLAPSE_ALL_GROUPS: loc.tableExplorer.collapseAllGroups,
-                            TEXT_COLUMNS: loc.tableExplorer.columns,
-                            TEXT_COLUMN_RESIZE_BY_CONTENT: loc.tableExplorer.columnResizeByContent,
-                            TEXT_COMMANDS: loc.tableExplorer.commands,
-                            TEXT_COPY: loc.tableExplorer.copy,
-                            TEXT_EXPAND_ALL_GROUPS: loc.tableExplorer.expandAllGroups,
-                            TEXT_EXPORT_TO_CSV: loc.tableExplorer.exportToCsv,
-                            TEXT_EXPORT_TO_EXCEL: loc.tableExplorer.exportToExcel,
-                            TEXT_EXPORT_TO_TEXT_FORMAT: loc.tableExplorer.exportToTextFormat,
-                            TEXT_EXPORT_TO_TAB_DELIMITED: loc.tableExplorer.exportToTabDelimited,
-                            TEXT_FILTER_SHORTCUTS: loc.tableExplorer.filterShortcuts,
-                            TEXT_FORCE_FIT_COLUMNS: loc.tableExplorer.forceFitColumns,
-                            TEXT_FREEZE_COLUMNS: loc.tableExplorer.freezeColumns,
-                            TEXT_GROUP_BY: loc.tableExplorer.groupBy,
-                            TEXT_HIDE_COLUMN: loc.tableExplorer.hideColumn,
-                            TEXT_ITEMS: loc.tableExplorer.items,
-                            TEXT_ITEMS_PER_PAGE: loc.tableExplorer.itemsPerPage,
-                            TEXT_ITEMS_SELECTED: loc.tableExplorer.itemsSelected,
-                            TEXT_LOADING: loc.tableExplorer.loading,
-                            TEXT_NO_ELEMENTS_FOUND: loc.tableExplorer.noElementsFound,
-                            TEXT_NO_MATCHES_FOUND: loc.tableExplorer.noMatchesFound,
-                            TEXT_OF: loc.tableExplorer.of,
-                            TEXT_OK: loc.tableExplorer.ok,
-                            TEXT_OPTIONS: loc.tableExplorer.options,
-                            TEXT_PAGE: loc.tableExplorer.page,
-                            TEXT_REFRESH_DATASET: loc.tableExplorer.refreshDataset,
-                            TEXT_REMOVE_FILTER: loc.tableExplorer.removeFilter,
-                            TEXT_REMOVE_SORT: loc.tableExplorer.removeSort,
-                            TEXT_SAVE: loc.tableExplorer.save,
-                            TEXT_SELECT_ALL: loc.tableExplorer.selectAll,
-                            TEXT_SORT_ASCENDING: loc.tableExplorer.sortAscending,
-                            TEXT_SORT_DESCENDING: loc.tableExplorer.sortDescending,
-                            TEXT_SYNCHRONOUS_RESIZE: loc.tableExplorer.synchronousResize,
-                            TEXT_TOGGLE_DARK_MODE: loc.tableExplorer.toggleDarkMode,
-                            TEXT_TOGGLE_FILTER_ROW: loc.tableExplorer.toggleFilterRow,
-                            TEXT_TOGGLE_PRE_HEADER_ROW: loc.tableExplorer.togglePreHeaderRow,
-                            TEXT_UNFREEZE_COLUMNS: loc.tableExplorer.unfreezeColumns,
-                            TEXT_X_OF_Y_SELECTED: loc.tableExplorer.xOfYSelected,
-                        },
+                        locales: slickGridLocales,
 
                         // Column operations
                         enableColumnReorder: true, // Allow column reordering via drag-and-drop
@@ -733,15 +669,10 @@ export const TableDataGrid = forwardRef<TableDataGridRef, TableDataGridProps>(
         }
 
         // Handle row selection changes
-        function handleSelectedRowsChanged(_e: any, args: any) {
+        function handleSelectedRowsChanged(_e: any, _args: any) {
             if (onSelectedRowsChanged && reactGridRef.current?.dataView) {
-                const selectedRowIndices = args.rows || [];
-                const selectedRowIds = selectedRowIndices
-                    .map((rowIdx: number) => {
-                        const item = reactGridRef.current?.dataView?.getItem(rowIdx);
-                        return item?.id;
-                    })
-                    .filter((id: number | undefined) => id !== undefined);
+                const selectedRowIds =
+                    reactGridRef.current.dataView.getAllSelectedIds() as number[];
                 onSelectedRowsChanged(selectedRowIds);
             }
         }
---
+++ extensions/mssql/src/reactviews/pages/TableExplorer/commonGridOptions.ts
@@ -0,0 +1,68 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { locConstants as loc } from "../../common/locConstants";
+
+/**
+ * Common locales configuration for SlickGrid components.
+ * This can be reused across multiple grid implementations.
+ */
+export const slickGridLocales = {
+    TEXT_CONTAINS: loc.tableExplorer.filterContains,
+    TEXT_NOT_CONTAINS: loc.tableExplorer.filterNotContains,
+    TEXT_EQUALS: loc.tableExplorer.filterEquals,
+    TEXT_EQUAL_TO: loc.tableExplorer.equalTo,
+    TEXT_NOT_EQUAL_TO: loc.tableExplorer.filterNotEqualTo,
+    TEXT_STARTS_WITH: loc.tableExplorer.filterStartsWith,
+    TEXT_ENDS_WITH: loc.tableExplorer.filterEndsWith,
+    TEXT_GREATER_THAN: loc.tableExplorer.greaterThan,
+    TEXT_GREATER_THAN_OR_EQUAL_TO: loc.tableExplorer.greaterThanOrEqualTo,
+    TEXT_LESS_THAN: loc.tableExplorer.lessThan,
+    TEXT_LESS_THAN_OR_EQUAL_TO: loc.tableExplorer.lessThanOrEqualTo,
+    TEXT_ALL_SELECTED: loc.tableExplorer.allSelected,
+    TEXT_CANCEL: loc.tableExplorer.cancel,
+    TEXT_CLEAR_ALL_FILTERS: loc.tableExplorer.clearAllFilters,
+    TEXT_CLEAR_ALL_GROUPING: loc.tableExplorer.clearAllGrouping,
+    TEXT_CLEAR_ALL_SORTING: loc.tableExplorer.clearAllSorting,
+    TEXT_CLEAR_PINNING: loc.tableExplorer.clearPinning,
+    TEXT_COLLAPSE_ALL_GROUPS: loc.tableExplorer.collapseAllGroups,
+    TEXT_COLUMNS: loc.tableExplorer.columns,
+    TEXT_COLUMN_RESIZE_BY_CONTENT: loc.tableExplorer.columnResizeByContent,
+    TEXT_COMMANDS: loc.tableExplorer.commands,
+    TEXT_COPY: loc.tableExplorer.copy,
+    TEXT_EXPAND_ALL_GROUPS: loc.tableExplorer.expandAllGroups,
+    TEXT_EXPORT_TO_CSV: loc.tableExplorer.exportToCsv,
+    TEXT_EXPORT_TO_EXCEL: loc.tableExplorer.exportToExcel,
+    TEXT_EXPORT_TO_TEXT_FORMAT: loc.tableExplorer.exportToTextFormat,
+    TEXT_EXPORT_TO_TAB_DELIMITED: loc.tableExplorer.exportToTabDelimited,
+    TEXT_FILTER_SHORTCUTS: loc.tableExplorer.filterShortcuts,
+    TEXT_FORCE_FIT_COLUMNS: loc.tableExplorer.forceFitColumns,
+    TEXT_FREEZE_COLUMNS: loc.tableExplorer.freezeColumns,
+    TEXT_GROUP_BY: loc.tableExplorer.groupBy,
+    TEXT_HIDE_COLUMN: loc.tableExplorer.hideColumn,
+    TEXT_ITEMS: loc.tableExplorer.items,
+    TEXT_ITEMS_PER_PAGE: loc.tableExplorer.itemsPerPage,
+    TEXT_ITEMS_SELECTED: loc.tableExplorer.itemsSelected,
+    TEXT_LOADING: loc.tableExplorer.loading,
+    TEXT_NO_ELEMENTS_FOUND: loc.tableExplorer.noElementsFound,
+    TEXT_NO_MATCHES_FOUND: loc.tableExplorer.noMatchesFound,
+    TEXT_OF: loc.tableExplorer.of,
+    TEXT_OK: loc.tableExplorer.ok,
+    TEXT_OPTIONS: loc.tableExplorer.options,
+    TEXT_PAGE: loc.tableExplorer.page,
+    TEXT_REFRESH_DATASET: loc.tableExplorer.refreshDataset,
+    TEXT_REMOVE_FILTER: loc.tableExplorer.removeFilter,
+    TEXT_REMOVE_SORT: loc.tableExplorer.removeSort,
+    TEXT_SAVE: loc.tableExplorer.save,
+    TEXT_SELECT_ALL: loc.tableExplorer.selectAll,
+    TEXT_SORT_ASCENDING: loc.tableExplorer.sortAscending,
+    TEXT_SORT_DESCENDING: loc.tableExplorer.sortDescending,
+    TEXT_SYNCHRONOUS_RESIZE: loc.tableExplorer.synchronousResize,
+    TEXT_TOGGLE_DARK_MODE: loc.tableExplorer.toggleDarkMode,
+    TEXT_TOGGLE_FILTER_ROW: loc.tableExplorer.toggleFilterRow,
+    TEXT_TOGGLE_PRE_HEADER_ROW: loc.tableExplorer.togglePreHeaderRow,
+    TEXT_UNFREEZE_COLUMNS: loc.tableExplorer.unfreezeColumns,
+    TEXT_X_OF_Y_SELECTED: loc.tableExplorer.xOfYSelected,
+};
