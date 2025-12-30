e7b8a4d31171214e34f5864f040722eb682ac9bd
Aasim Khan <aaskhan@microsoft.com>
2025-12-30 12:10:36 -0800

    Refactoring Result grid copy to fix issues.  (#20870)
    * Refactor copy results handling to improve progress indication and error management
    
    * Adding updated sts
    
    * Fix clipboard text handling in QueryRunner to use result content
    
    * Fixing sort and filter indexes for copying
    
    ---------
    
    Co-authored-by: Aasim Khan <aasimkhan@gmail.com>

--- extensions/mssql/src/configurations/config.ts
+++ extensions/mssql/src/configurations/config.ts
@@ -7,7 +7,7 @@ export const config = {
     service: {
         downloadUrl:
             "https://github.com/Microsoft/sqltoolsservice/releases/download/{#version#}/microsoft.sqltools.servicelayer-{#fileName#}",
-        version: "5.0.20251212.1",
+        version: "5.0.20251227.1",
         downloadFileNames: {
             Windows_64: "win-x64-net8.0.zip",
             Windows_ARM64: "win-arm64-net8.0.zip",
