c365d5a174485421cfebd605814302b738515f8b
laurenastrid1 <151488703+laurenastrid1@users.noreply.github.com>
2026-01-21 13:37:05 -0500

    Added Azure Blob Logic (#20915)
    * added azure blob logic
    
    * reverted null check in sql task service; fixed by pr #20914
    
    * added error handling for calls
    
    * azure blob service test
    
    * helper tests
    
    * pr comments
    
    * throw error instead of return it
    
    * fail error tests if it works
    
    ---------
    
    Co-authored-by: Lauren Nathan <laurennathan@microsoft.com>

--- extensions/mssql/test/unit/azureHelperStubs.ts
+++ extensions/mssql/test/unit/azureHelperStubs.ts
@@ -121,6 +121,16 @@ export const mockAzureResources = {
         location: "eastus2",
         kind: "StorageV2",
     } as GenericResourceExpanded,
+    storageAccount: {
+        name: "testStorageAccount",
+        location: "eastus2",
+        id: `/subscriptions/${mockSubscriptions[0].subscriptionId}/resourceGroups/DefaultResourceGroup/providers/Microsoft.Storage/storageAccounts/testStorageAccount`,
+        type: "Microsoft.Storage/storageAccounts",
+    },
+    blobContainer: {
+        name: "testContainer",
+        id: `/subscriptions/${mockSubscriptions[0].subscriptionId}/resourceGroups/DefaultResourceGroup/providers/Microsoft.Storage/storageAccounts/testStorageAccount/blobServices/default/containers/testContainer`,
+    },
 };
 
 export const mockSqlDbList = {
