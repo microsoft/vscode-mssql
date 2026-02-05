85a85eef3a58862a7674ad5b8d75908ed2963f7c
Lewis Sanchez <lewissanchez@microsoft.com>
2026-02-05 11:49:51 -0800

    Remove unused imports

--- extensions/mssql/src/reactviews/pages/GlobalSearch/GlobalSearchResultsTable.tsx
+++ extensions/mssql/src/reactviews/pages/GlobalSearch/GlobalSearchResultsTable.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import React, { useMemo, useCallback } from "react";
+import React from "react";
 import {
     makeStyles,
     shorthands,
