47b60020240fb1c8952f8afa9405fe2291455b80
Benjin Dubishar <benjin.dubishar@gmail.com>
2026-02-09 14:40:05 -0800

    Fixing engine edition not being sent with telemetry

--- extensions/mssql/src/controllers/connectionManager.ts
+++ extensions/mssql/src/controllers/connectionManager.ts
@@ -1527,10 +1527,14 @@ export default class ConnectionManager {
             ),
         );
         sendActionEvent(
-            TelemetryViews.ConnectionPrompt,
+            TelemetryViews.ConnectionManager,
             TelemetryActions.CreateConnectionResult,
-            undefined,
-            undefined,
+            {
+                connectedEngineEditionId: String(result.serverInfo.engineEditionId),
+            },
+            {
+                connectedEngineEditionId: result.serverInfo.engineEditionId,
+            },
             newCredentials as IConnectionProfile,
             result.serverInfo,
         );
