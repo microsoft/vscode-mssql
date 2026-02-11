684b40230bbd06810c26768832ee40c4ae8b3d4a
Lauren Nathan <laurennathan@microsoft.com>
2026-02-11 17:55:08 -0500

    include error messgae in reset state error telemetry

--- extensions/mssql/src/controllers/flatFileImportWebviewController.ts
+++ extensions/mssql/src/controllers/flatFileImportWebviewController.ts
@@ -286,7 +286,7 @@ export class FlatFileImportWebviewController extends FormWebviewController<
                         TelemetryViews.FlatFile,
                         TelemetryActions.ResetState,
                         new Error(errorMessage),
-                        false,
+                        true,
                     );
                     break;
             }
