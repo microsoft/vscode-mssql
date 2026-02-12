d09c0063f9a96a48a3ff7c2a6e3a79ba17be54b7
Sai Avishkar Sreerama <ssreerama@microsoft.com>
2026-02-12 11:43:10 -0600

    datasource and newProjectTool test

--- extensions/sql-database-projects/test/datasource.test.ts
+++ extensions/sql-database-projects/test/datasource.test.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import should = require("should/as-function");
+import { expect } from "chai";
 import * as baselines from "./baselines/baselines";
 import * as testUtils from "./testUtils";
 import * as sql from "../src/models/dataSources/sqlConnectionStringSource";
@@ -18,6 +18,7 @@ suite("Data Sources: DataSource operations", function (): void {
         await testUtils.deleteGeneratedTestFolder();
     });
 
+    // TODO: Test skipped because dataSources.load() has parsing logic commented out for version "0.0.0"
     test.skip("Should read DataSources from datasource.json", async function (): Promise<void> {
         const dataSourcePath = await testUtils.createTestDataSources(
             this.test,
@@ -25,41 +26,45 @@ suite("Data Sources: DataSource operations", function (): void {
         );
         const dataSourceList = await dataSources.load(dataSourcePath);
 
-        should(dataSourceList.length).equal(3);
+        expect(dataSourceList.length).to.equal(3);
 
-        should(dataSourceList[0].name).equal("Test Data Source 1");
-        should(dataSourceList[0].type).equal(sql.SqlConnectionDataSource.type);
-        should((dataSourceList[0] as sql.SqlConnectionDataSource).database).equal("testDb");
+        expect(dataSourceList[0].name).to.equal("Test Data Source 1");
+        expect(dataSourceList[0].type).to.equal(sql.SqlConnectionDataSource.type);
+        expect((dataSourceList[0] as sql.SqlConnectionDataSource).database).to.equal("testDb");
 
-        should(dataSourceList[1].name).equal("My Other Data Source");
-        should((dataSourceList[1] as sql.SqlConnectionDataSource).integratedSecurity).equal(false);
+        expect(dataSourceList[1].name).to.equal("My Other Data Source");
+        expect((dataSourceList[1] as sql.SqlConnectionDataSource).integratedSecurity).to.equal(
+            false,
+        );
 
-        should(dataSourceList[2].name).equal("AAD Interactive Data Source");
-        should((dataSourceList[2] as sql.SqlConnectionDataSource).integratedSecurity).equal(false);
-        should((dataSourceList[2] as sql.SqlConnectionDataSource).azureMFA).equal(true);
+        expect(dataSourceList[2].name).to.equal("AAD Interactive Data Source");
+        expect((dataSourceList[2] as sql.SqlConnectionDataSource).integratedSecurity).to.equal(
+            false,
+        );
+        expect((dataSourceList[2] as sql.SqlConnectionDataSource).azureMFA).to.equal(true);
     });
 
     test("Should be able to create sql data source from connection strings with and without ending semicolon", function (): void {
-        should.doesNotThrow(
+        expect(
             () =>
                 new sql.SqlConnectionDataSource(
                     "no ending semicolon",
                     "Data Source=(LOCAL);Initial Catalog=testdb;User id=sa;Password=PLACEHOLDER",
                 ),
-        );
-        should.doesNotThrow(
+        ).to.not.throw();
+        expect(
             () =>
                 new sql.SqlConnectionDataSource(
                     "ending in semicolon",
                     "Data Source=(LOCAL);Initial Catalog=testdb;User id=sa;Password=PLACEHOLDER;",
                 ),
-        );
-        should.throws(
+        ).to.not.throw();
+        expect(
             () =>
                 new sql.SqlConnectionDataSource(
                     "invalid extra equals sign",
                     "Data Source=(LOCAL);Initial Catalog=testdb=extra;User id=sa;Password=PLACEHOLDER",
                 ),
-        );
+        ).to.throw();
     });
 });
