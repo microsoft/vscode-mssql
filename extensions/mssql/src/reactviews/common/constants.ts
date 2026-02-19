a82e88f1fd75d20b836b9942fb899c657e28827c
Lauren Nathan <laurennathan@microsoft.com>
2026-02-19 17:06:12 -0500

    added editable target db for restore

--- extensions/mssql/src/reactviews/common/constants.ts
+++ extensions/mssql/src/reactviews/common/constants.ts
@@ -9,3 +9,7 @@ export const dataTierApplicationsDocumentationUrl =
     "https://learn.microsoft.com/en-us/sql/tools/sql-database-projects/concepts/data-tier-applications/overview?view=sql-server-ver17";
 
 export const url = "URL";
+
+export const keydown = "keydown";
+
+export const enter = "Enter";
