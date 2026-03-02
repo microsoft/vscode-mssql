3b2655fa12289a339ce338405d8486dda71fd385
laurenastrid1 <151488703+laurenastrid1@users.noreply.github.com>
2026-03-02 15:40:39 -0500

    Remove colons from default backup file name (#21413)
    * remove colons from default backup file name
    
    * fixed sep in form action
    
    ---------
    
    Co-authored-by: Lauren Nathan <laurennathan@microsoft.com>

--- extensions/mssql/test/unit/backupDatabaseWebviewController.test.ts
+++ extensions/mssql/test/unit/backupDatabaseWebviewController.test.ts
@@ -715,7 +715,7 @@ suite("BackupDatabaseWebviewController", () => {
         const backupName = controller["getDefaultBackupFileName"](
             mockInitialState.viewModel.model as BackupDatabaseViewModel,
         );
-        expect(backupName).to.include("testDatabase_");
+        expect(backupName).to.include("testDatabase-");
 
         mockInitialState.viewModel.model = {
             ...mockInitialState.viewModel.model,
@@ -724,7 +724,8 @@ suite("BackupDatabaseWebviewController", () => {
         const backupName2 = controller["getDefaultBackupFileName"](
             mockInitialState.viewModel.model as BackupDatabaseViewModel,
         );
-        expect(backupName2).to.include("testDatabase_Full_1");
+        expect(backupName2).to.include("testDatabase-Full-1");
+        expect(backupName2).to.not.include(":");
     });
 
     test("setMediaOptionsIfExistingFiles updates media options based on existing backup files", () => {
