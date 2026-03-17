fecd36c650538000962663a53e2361adf292036c
Benjin Dubishar <benjin.dubishar@gmail.com>
2026-03-17 14:38:14 -0700

    Adding "Test connection" and "Save without connecting" options to connection dialog (#21515)
    * Checkpoint
    
    * Actions working
    
    * testing/connecting buttons
    
    * some refinement
    
    * Adding disconnect to test connection step

--- extensions/mssql/src/reactviews/common/locConstants.ts
+++ extensions/mssql/src/reactviews/common/locConstants.ts
@@ -349,9 +349,14 @@ export class LocConstants {
             },
             databaseList: l10n.t("Database list"),
             connect: l10n.t("Connect"),
+            connectActions: l10n.t("Connection actions"),
             advancedConnectionSettings: l10n.t("Advanced Connection Settings"),
             advancedSettings: l10n.t("Advanced"),
-            testConnection: l10n.t("Test Connection"),
+            testConnection: l10n.t("Test connection"),
+            testConnectionSucceeded: l10n.t("Connection test succeeded"),
+            testing: l10n.t("Testing..."),
+            connecting: l10n.t("Connecting..."),
+            saveWithoutConnecting: l10n.t("Save without connecting"),
             connectToDatabase: l10n.t("Connect to Database"),
             connectTo: (profileName: string) =>
                 l10n.t({
