8cbee4d408f63b0597ae7267993a97ba9100fa4f
Aasim Khan <aasimkhan@gmail.com>
2026-03-16 22:45:06 -0700

    fix: update database icon and adjust layout spacing in ConnectionDialog

--- extensions/mssql/src/reactviews/pages/ConnectionDialog/connectionPage.tsx
+++ extensions/mssql/src/reactviews/pages/ConnectionDialog/connectionPage.tsx
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Button, Divider, makeStyles, shorthands } from "@fluentui/react-components";
-import { DatabaseRegular } from "@fluentui/react-icons";
+import { Database32Regular } from "@fluentui/react-icons";
 import { useState } from "react";
 import { ResizableBox } from "react-resizable";
 import { ConnectionsListContainer } from "./connectionsListContainer";
@@ -52,7 +52,7 @@ export const ConnectionPage = () => {
         <div className={classes.root}>
             <div className={classes.mainContainer}>
                 <DialogPageShell
-                    icon={<DatabaseRegular />}
+                    icon={<Database32Regular />}
                     title={locConstants.connectionDialog.connectToDatabase}
                     footerStart={
                         <Button
