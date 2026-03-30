8dd47e6d42ca40a8aabd6ce590db2267b3182653
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2026-03-30 14:46:27 -0700

    bumping STS

--- extensions/mssql/src/configurations/config.ts
+++ extensions/mssql/src/configurations/config.ts
@@ -7,14 +7,14 @@ export const config = {
     service: {
         downloadUrl:
             "https://github.com/Microsoft/sqltoolsservice/releases/download/{#version#}/microsoft.sqltools.servicelayer-{#fileName#}",
-        version: "5.0.20260325.3",
+        version: "6.0.20260330.1",
         downloadFileNames: {
-            Windows_64: "win-x64-net8.0.zip",
-            Windows_ARM64: "win-arm64-net8.0.zip",
-            OSX: "osx-x64-net8.0.tar.gz",
-            OSX_ARM64: "osx-arm64-net8.0.tar.gz",
-            Linux: "linux-x64-net8.0.tar.gz",
-            Linux_ARM64: "linux-arm64-net8.0.tar.gz",
+            Windows_64: "win-x64-net10.0.zip",
+            Windows_ARM64: "win-arm64-net10.0.zip",
+            OSX: "osx-x64-net10.0.tar.gz",
+            OSX_ARM64: "osx-arm64-net10.0.tar.gz",
+            Linux: "linux-x64-net10.0.tar.gz",
+            Linux_ARM64: "linux-arm64-net10.0.tar.gz",
         },
         installDir: "./sqltoolsservice/{#version#}/{#platform#}",
         executableFiles: [
