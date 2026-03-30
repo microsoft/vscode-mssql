6f4076c06cd7afa8036c8d3feeba792490942cf5
Sai Avishkar Sreerama <ssreerama@microsoft.com>
2026-03-30 11:40:38 -0500

    cleanup

--- extensions/sql-database-projects/src/typings/ref.d.ts
+++ extensions/sql-database-projects/src/typings/ref.d.ts
@@ -3,9 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// /// <reference path='../../../../src/vscode-dts/vscode.d.ts'/>
 /// <reference path='../../../data-workspace/src/dataworkspace.d.ts'/>
 /// <reference path='../../../mssql/typings/vscode-mssql.d.ts'/>
-// TODO: remove root typings/vscode-mssql.d.ts (ported from ADS)
-// /// <reference path='../../../../typings/vscode-mssql.d.ts'/>
 /// <reference types='@types/node'/>
