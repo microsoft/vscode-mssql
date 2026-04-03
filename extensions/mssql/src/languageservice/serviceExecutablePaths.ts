3bc94365dd6dc5ad3a42b2ffa6a778b3885c8ecc
Aasim Khan <aaskhan@microsoft.com>
2026-04-03 15:44:17 -0700

    Fixing logic for checking existing sts installation (#21808)

---
+++ extensions/mssql/src/languageservice/serviceExecutablePaths.ts
@@ -0,0 +1,46 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from "path";
+import { Runtime } from "../models/platform";
+
+/**
+ * The service executables required for a SQL Tools Service installation to be considered runnable.
+ */
+export const requiredServiceExecutablePrefixes = [
+    "MicrosoftSqlToolsServiceLayer",
+    "SqlToolsResourceProviderService",
+] as const;
+
+export type ServiceExecutablePrefix = (typeof requiredServiceExecutablePrefixes)[number];
+
+/**
+ * Returns the expected service executable file name for a runtime.
+ */
+export function getServiceExecutableFileName(
+    runtime: Runtime,
+    filePrefix: ServiceExecutablePrefix,
+): string {
+    if (runtime === Runtime.Portable) {
+        return `${filePrefix}.dll`;
+    }
+
+    if (runtime === Runtime.Windows_64 || runtime === Runtime.Windows_ARM64) {
+        return `${filePrefix}.exe`;
+    }
+
+    return filePrefix;
+}
+
+/**
+ * Returns the full path to a runtime-specific service executable in an install folder.
+ */
+export function getServiceExecutablePath(
+    folderPath: string,
+    runtime: Runtime,
+    filePrefix: ServiceExecutablePrefix,
+): string {
+    return path.join(folderPath, getServiceExecutableFileName(runtime, filePrefix));
+}
