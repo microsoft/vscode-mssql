fdab719be8000b1104db0064457ec7ec59af8624
Aasim Khan <aasimkhan@gmail.com>
2026-04-07 15:41:40 -0700

    Unused imports

--- extensions/mssql/test/unit/sqlTasksService.test.ts
+++ extensions/mssql/test/unit/sqlTasksService.test.ts
@@ -5,8 +5,6 @@
 
 import * as vscode from "vscode";
 import * as sinon from "sinon";
-import sinonChai from "sinon-chai";
-import * as chai from "chai";
 import { expect } from "chai";
 import {
     SqlTasksService,
@@ -21,8 +19,6 @@ import VscodeWrapper from "../../src/controllers/vscodeWrapper";
 import { TaskExecutionMode } from "../../src/enums";
 import * as telemetry from "../../src/telemetry/telemetry";
 import { stubLoggerGetter } from "./utils";
-
-chai.use(sinonChai);
 
 suite("SqlTasksService Tests", () => {
     let sandbox: sinon.SinonSandbox;
