413bd9acc17beeb9098334bc1ca5736d98d0dca1
Aasim Khan (from Dev Box) <aaskhan@microsoft.com>
2026-06-09 21:44:57 -0700

    Refactor Fluent Results Grid: add new files and remove obsolete command and keyboard handler

--- extensions/mssql/src/webviews/common/FluentResultGrid/FluentResultGridProvider.tsx
+++ extensions/mssql/src/webviews/common/FluentResultGrid/FluentResultGridProvider.tsx
@@ -3,9 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { FluentResultCommand } from "./fluentResultGridCommands";
-
-export interface FluentResultGridProps {
-    /** Commands contributed by the host for the command bar, menus, and keyboard. */
-    commands?: FluentResultCommand[];
-}
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
