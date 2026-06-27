6df0c3c9e373defff9c44b1becce5401722e38eb
Benjin Dubishar <benjin.dubishar@gmail.com>
2026-06-26 21:48:34 -0700

    Fixing test

--- extensions/mssql/test/unit/connectionManager.test.ts
+++ extensions/mssql/test/unit/connectionManager.test.ts
@@ -77,6 +77,7 @@ suite("ConnectionManager Tests", () => {
         });
 
         mockConnectionStore.readAllConnections.resolves([]);
+        mockConnectionStore.readAllConnectionGroups.resolves([]);
     });
 
     teardown(() => {
