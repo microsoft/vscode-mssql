bc550b9faec3db7bb175273233e1dfc4f2cbe2d4
Aasim Khan (from Dev Box) <aaskhan@microsoft.com>
2026-07-02 13:24:27 -0700

    Add provenance comments for toolkit DI files

--- packages/extension-toolkit/src/base/di/instantiationService.ts
+++ packages/extension-toolkit/src/base/di/instantiationService.ts
@@ -3,6 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+//!!! DO NOT modify directly. This file contains stable DI primitives copied from microsoft/vscode.
+// Source: https://github.com/microsoft/vscode/blob/1f01c15f70c50c8a6f6e9e17acca9d7cae9bbd5c/src/vs/platform/instantiation/common/instantiation.ts
+// Reference: https://github.com/microsoft/vscode-copilot-chat/blob/5863f5a7088958050792b5dccbe8b46c6e13eccc/src/util/vs/platform/instantiation/common/instantiation.ts
+// Extension-specific behavior should live outside src/base/di.
+
 import { isDisposable, type IDisposable } from "../lifecycle";
 import { ServiceCollection } from "./serviceCollection";
 import { ServiceDescriptor } from "./serviceDescriptor";
