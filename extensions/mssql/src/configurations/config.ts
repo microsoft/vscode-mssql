2361510a59953fe1e79c6113ad967498f548bff5
Hai Cao <hacao@microsoft.com>
2026-07-22 17:29:17 -0700

    Update SQL Tools Service to 6.0.20260722.6

--- extensions/mssql/src/configurations/config.ts
+++ extensions/mssql/src/configurations/config.ts
@@ -7,7 +7,7 @@ export const config = {
     service: {
         downloadUrl:
             "https://github.com/Microsoft/sqltoolsservice/releases/download/{#version#}/microsoft.sqltools.servicelayer-{#fileName#}",
-        version: "6.0.20260713.1",
+        version: "6.0.20260722.6",
         downloadFileNames: {
             Windows_64: "win-x64-net10.0.zip",
             Windows_ARM64: "win-arm64-net10.0.zip",
