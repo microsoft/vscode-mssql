634e52401dda743c4666bfbb727af28aef4787b0
Benjin Dubishar <benjin.dubishar@gmail.com>
2026-08-05 15:15:53 -0700

    Adding comment, loc

--- extensions/mssql/src/firewall/firewallService.ts
+++ extensions/mssql/src/firewall/firewallService.ts
@@ -23,6 +23,7 @@ export class FirewallService {
             return { result: false, ipAddress: "" };
         }
 
+        // Source: https://regexbox.com/regex-templates/ip
         const ipMatch = errorMessage.match(
             /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/,
         );
