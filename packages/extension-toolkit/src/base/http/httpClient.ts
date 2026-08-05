b2d0bbb3d6d9218dbca33b87d78b947157ddce8a
Aasim Khan <aaskhan@microsoft.com>
2026-08-05 13:44:55 -0700

    Format proxy SSL option

--- packages/extension-toolkit/src/base/http/httpClient.ts
+++ packages/extension-toolkit/src/base/http/httpClient.ts
@@ -346,9 +346,7 @@ export class HttpClient {
                 host: agentOptions.host,
                 port: Number(agentOptions.port),
                 ...(agentOptions.auth ? { proxyAuth: agentOptions.auth } : {}),
-                ...(isHttpsProxy
-                    ? { rejectUnauthorized: agentOptions.rejectUnauthorized }
-                    : {}),
+                ...(isHttpsProxy ? { rejectUnauthorized: agentOptions.rejectUnauthorized } : {}),
             },
         };
 
