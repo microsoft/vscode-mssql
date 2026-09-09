9ecb7d1cc304afb177344b850a859559cf601867
Frederico Almeida <frederico.almeida@gmail.com>
2026-09-09 12:18:18 +0100

    feat: expose per-batch query text from QueryRunner

--- extensions/mssql/src/controllers/queryRunner.ts
+++ extensions/mssql/src/controllers/queryRunner.ts
@@ -1524,6 +1524,23 @@ export default class QueryRunner {
         return undefined;
     }
 
+    public async getBatchQueryText(batchId: number): Promise<string | undefined> {
+        const batchSummary = this.batchSets[batchId];
+        if (!batchSummary) {
+            return undefined;
+        }
+        const doc = await vscode.workspace.openTextDocument(vscode.Uri.parse(this._ownerUri));
+        const selection = batchSummary.selection;
+        if (!selection) {
+            return doc.getText();
+        }
+        const range = new vscode.Range(
+            new vscode.Position(selection.startLine, selection.startColumn),
+            new vscode.Position(selection.endLine, selection.endColumn),
+        );
+        return doc.getText(range);
+    }
+
     public resetHasCompleted(): void {
         this._hasCompleted = false;
     }
